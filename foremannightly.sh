40c487505b01446d2ed5c870a220f10eb7358ed5
Sam Kottler <sam@kottlerdeveloment.com>
2012-08-15 13:52:46 -0400

    Use sudo for mock and ensure the mock result dir exists

--- foremannightly.sh
+++ foremannightly.sh
@@ -12,7 +12,7 @@ for distro in el5 el6 f16 f17; do
 
     #Build foreman for each arch and distro
     for arch in i386 x86_64; do
-        mock -r $distro-$arch ~/rpmbuild/SRPMS/foreman*
+        sudo mock -r $distro-$arch ~/rpmbuild/SRPMS/foreman*
         scp /var/lib/mock/$distro-$arch/result/*src.rpm account@shell.../$distro/source
         scp /var/lib/mock/$distro-$arch/result/*noarch.rpm account@shell.../$distro/$arch
     done
