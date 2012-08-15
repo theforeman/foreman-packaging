d3d66042e7bc38181d1f7e0f855ddb101c830ce0
Sam Kottler <sam@kottlerdeveloment.com>
2012-08-15 15:28:06 -0400

    Put the sudo in the right place

--- foremanbuild.sh
+++ foremanbuild.sh
@@ -14,8 +14,8 @@ for distro in el5 el6 f16 f17; do
    
     #Build foreman for all distros and archs 
     for arch in i386 x86_64; do
-        mock -r $distro-$arch ~/rpmbuild/SRPMS/foreman*
-        sudo mv /var/lib/mock/$distro-$arch/result/*src.rpm ~/foreman/$distro/source
+        sudo mock -r $distro-$arch ~/rpmbuild/SRPMS/foreman*
+        mv /var/lib/mock/$distro-$arch/result/*src.rpm ~/foreman/$distro/source
         mv /var/lib/mock/$distro-$arch/result/*noarch.rpm  ~/foreman/$distro/$arch
     done
     rm -f ~/rpmbuild/SRPMS/foreman*
