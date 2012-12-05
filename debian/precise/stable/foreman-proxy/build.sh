e13d2e09a1dc89dfaf8dcadd58e3443fb89403c6
Greg Sutcliffe <gsutclif@redhat.com>
2012-12-05 17:41:23 +0000

    Refactor the build.sh files so only the essential differences are in the leaf dirs

--- debian/precise/stable/foreman-proxy/build.sh
+++ debian/precise/stable/foreman-proxy/build.sh
@@ -1,55 +1,8 @@
-#!/bin/bash
-
-# Script to be cloned from git to build foreman-proxy (stable)
-# Assumes this repo/dir has been checked out already. Operates
-# from ./ and copies to $source/debian
-
-# Expected input
-# $1 - pbuilder base image, eg squeeze64
-# $2 - precreated temp dir to use
-
-# Stop on errors
-set -e
-
 PACKAGE_NAME='foreman-proxy'
-
-# Name of the pbuilder env to use, local for on-the-host
-if [ "$1" == "" ] ; then
-  PBUILDER="local"
-else
-  PBUILDER="$1"
-fi
-
-# We use readlink to get an absolute path to the Jenkins
-# checkout, but readlink expects the path to exist
-mkdir -p "../$PBUILDER"
-BUILD_DIR=`readlink -f ../$PBUILDER`
-TARGET="${BUILD_DIR}/${PACKAGE_NAME}"
 
 REPO='git://github.com/theforeman/smart-proxy.git'
 BRANCH='9ea6076283d744ba55163ad45e3bacd96a1add72'
 
-GIT='/usr/bin/git'
-
-# Copy in packaging to the build dir
-mkdir -p  "${BUILD_DIR}/debian"
-cp -r ./* "${BUILD_DIR}/debian/"
-
-# Clone source code
-cd "${BUILD_DIR}"
-$GIT clone "${REPO}" "${TARGET}"
-cd "${TARGET}"
-$GIT checkout "${BRANCH}"
-$GIT submodule init
-$GIT submodule update
-# Move packaging into source clone
-mv "${BUILD_DIR}/debian" "${TARGET}/"
-
-# Cleanup source
-rm -rf $(/usr/bin/find "${TARGET}" -name '.git*')
-
-if [ "$PBUILDER" != "local" ] ; then
-  # Execute build using the pbuilder image in $1
-  sudo pdebuild-$PBUILDER
-  sudo chown -R jenkins:jenkins "${BUILD_DIR}"
-fi
+function setup_build {
+  echo "stable: skipping setup for nightly"
+}
--- debian/squeeze/nightly/foreman/build.sh
+++ debian/squeeze/nightly/foreman/build.sh
@@ -1,53 +1,10 @@
-#!/bin/bash
-
-# Script to be cloned from git to build foreman-proxy (stable)
-# Assumes this repo/dir has been checked out already. Operates
-# from ./ and copies to $source/debian
-
-# Expected input
-# $1 - pbuilder base image, eg squeeze64
-# $2 - precreated temp dir to use
-
-# Stop on errors
-set -e
-
 PACKAGE_NAME='foreman'
-
-# Name of the pbuilder env to use, local for on-the-host
-if [ "$1" == "" ] ; then
-  PBUILDER="local"
-else
-  PBUILDER="$1"
-fi
-
-# We use readlink to get an absolute path to the Jenkins
-# checkout, but readlink expects the path to exist
-mkdir -p "../$PBUILDER"
-BUILD_DIR=`readlink -f ../$PBUILDER`
-TARGET="${BUILD_DIR}/${PACKAGE_NAME}"
 
 REPO='git://github.com/theforeman/foreman.git'
 BRANCH='develop'
 
-GIT='/usr/bin/git'
-
-# Copy in packaging to the build dir
-mkdir -p  "${BUILD_DIR}/debian"
-cp -r ./* "${BUILD_DIR}/debian/"
-
-# Clone source code
-cd "${BUILD_DIR}"
-$GIT clone "${REPO}" "${TARGET}"
-cd "${TARGET}"
-$GIT checkout "${BRANCH}"
-$GIT submodule init
-$GIT submodule update
-# Move packaging into source clone
-mv "${BUILD_DIR}/debian" "${TARGET}/"
-
-# Cleanup source
+function setup_build {
 LAST_COMMIT=$($GIT rev-list HEAD|/usr/bin/head -n 1)
-rm -rf $(/usr/bin/find "${TARGET}" -name '.git*')
 
 # Add 'nightly' to changelog
 
@@ -72,8 +29,4 @@ rm -f debian/changelog.tmp
 # Add 'nightly' to VERSION
 echo "`cat VERSION`-${LAST_COMMIT}" > VERSION
 
-if [ "$PBUILDER" != "local" ] ; then
-  # Execute build using the pbuilder image in $1
-  sudo pdebuild-$PBUILDER
-  sudo chown -R jenkins:jenkins "${BUILD_DIR}"
-fi
+}
