815fd468f54f6ebe9033a5a8c3c72589709e7dea
Greg Sutcliffe <gsutclif@redhat.com>
2012-12-11 14:40:38 +0000

    release 1.1 rc 1.1

--- debian/squeeze/rc/foreman-proxy/build.sh
+++ debian/squeeze/rc/foreman-proxy/build.sh
@@ -1,7 +1,7 @@
 PACKAGE_NAME='foreman-proxy'
 
 REPO='git://github.com/theforeman/smart-proxy.git'
-BRANCH='9a38704839f089942683842e6ebc369cb8502181'
+BRANCH='1.1RC1'
 
 function setup_build {
   echo "rc: skipping setup for nightly"
