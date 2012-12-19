9f76d167b5b59eab8207b12601dd3f9c72a0b255
Greg Sutcliffe <gsutclif@redhat.com>
2012-12-19 15:54:59 +0000

    Release 1.0.2 stable to fix CVE-2012-5648

--- debian/precise/stable/foreman/build.sh
+++ debian/precise/stable/foreman/build.sh
@@ -1,7 +1,7 @@
 PACKAGE_NAME='foreman'
 
 REPO='git://github.com/theforeman/foreman.git'
-BRANCH='1.0-stable'
+BRANCH='89d3f79b13c8a12dac5d1c9b71f6b42176102f7b'
 
 function setup_build {
   echo "stable: skipping setup for nightly"
