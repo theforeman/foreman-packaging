c8f44a52fb53d3acd365cbeb7cbaec06d3cacb1d
Dominic Cleal <dcleal@redhat.com>
2013-01-09 15:11:45 +0000

    Redirect cron output, messages and summaries from tasks to cron.log

--- debian/squeeze/nightly/foreman/foreman.cron.d
+++ debian/squeeze/nightly/foreman/foreman.cron.d
@@ -5,24 +5,24 @@ RAILS_ENV=production
 FOREMAN_HOME=/usr/share/foreman
 
 # Clean up the session entries in the database
-15 23 * * *     foreman    cd ${FOREMAN_HOME} && /usr/bin/rake db:sessions:clear
+15 23 * * *     foreman    cd ${FOREMAN_HOME} && /usr/bin/rake db:sessions:clear >/var/log/foreman/cron.log 2>&1
 
 # Send out daily summary
-0 7 * * *       foreman    cd ${FOREMAN_HOME} && /usr/bin/rake reports:summarize
+0 7 * * *       foreman    cd ${FOREMAN_HOME} && /usr/bin/rake reports:summarize >/var/log/foreman/cron.log 2>&1
 
 # Expire old reports
-30 7 * * *      foreman    cd ${FOREMAN_HOME} && /usr/bin/rake reports:expire
+30 7 * * *      foreman    cd ${FOREMAN_HOME} && /usr/bin/rake reports:expire >/var/log/foreman/cron.log 2>&1
 
 # Collects trends data
-*/30 * * * *    foreman    cd ${FOREMAN_HOME} && /usr/bin/rake trends:counter
+*/30 * * * *    foreman    cd ${FOREMAN_HOME} && /usr/bin/rake trends:counter >/var/log/foreman/cron.log 2>&1
 
 
 # Only use the following cronjob if you're using stored configs!
 # Populate hosts
-*/30 * * * *    foreman    cd ${FOREMAN_HOME} && /usr/bin/rake puppet:migrate:populate_hosts
+*/30 * * * *    foreman    cd ${FOREMAN_HOME} && /usr/bin/rake puppet:migrate:populate_hosts >/var/log/foreman/cron.log 2>&1
 
 
 # Only uncomment the following cronjob if you're not using stored configs!
 # Send facts to Foreman.
-#*/2 * * * *    puppet     ${FOREMAN_HOME}/extras/puppet/foreman/files/push_facts.rb
+#*/2 * * * *    puppet     ${FOREMAN_HOME}/extras/puppet/foreman/files/push_facts.rb >/var/log/foreman/cron.log 2>&1
 
