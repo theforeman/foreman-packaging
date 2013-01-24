bb44475a8322722240123baaadd5dbb05b317241
Greg Sutcliffe <gsutclif@redhat.com>
2013-01-24 16:25:08 +0000

    Debian: Bump to rc5 / rc3

--- debian/squeeze/rc/foreman-proxy/build.sh
+++ debian/squeeze/rc/foreman-proxy/build.sh
@@ -1,7 +1,7 @@
 PACKAGE_NAME='foreman-proxy'
 
 REPO='git://github.com/theforeman/smart-proxy.git'
-BRANCH='1.1RC2'
+BRANCH='22431e9072e3b2c60ed2c8c2ed28fd92cb2b49ca'
 
 function setup_build {
   echo "rc: skipping setup for nightly"
