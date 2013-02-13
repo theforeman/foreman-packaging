81b9d611c628c48ec5d8da62d694afd3064abe72
Greg Sutcliffe <gsutclif@redhat.com>
2013-02-13 16:47:13 +0000

    debian: 1.1.1-1 release

--- debian/squeeze/stable/foreman/build.sh
+++ debian/squeeze/stable/foreman/build.sh
@@ -1,7 +1,7 @@
 PACKAGE_NAME='foreman'
 
 REPO='git://github.com/theforeman/foreman.git'
-BRANCH='caed6b1d442c74ddf38fab4add03d3830b35fe43'
+BRANCH='e5f23ec91422c85347bf782d7e22381d30fec978'
 
 function setup_build {
   echo "stable: skipping setup for nightly"
