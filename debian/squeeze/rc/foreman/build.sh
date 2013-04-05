badd645a0cd481cbef1dd826040e2e5805ece172
Greg Sutcliffe <gsutclif@redhat.com>
2013-04-05 12:35:42 +0100

    Remove debian subdir, this is now in the debian branches

--- debian/squeeze/rc/foreman/build.sh
+++
@@ -1,8 +0,0 @@
-PACKAGE_NAME='foreman'
-
-REPO='git://github.com/theforeman/foreman.git'
-BRANCH='74c114333c368aed777ac06e1a258603173b2f0b'
-
-function setup_build {
-  echo "rc: skipping setup for nightly"
-}
