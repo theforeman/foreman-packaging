ea774e37d3921709f05b4566e10aeb21973e89b3
Greg Sutcliffe <gsutclif@redhat.com>
2013-05-20 13:13:03 +0100

    Revert build.sh changes from nightly

--- debian/wheezy/foreman/build.sh
+++ debian/wheezy/foreman/build.sh
@@ -1,32 +1,8 @@
 PACKAGE_NAME='foreman'
 
 REPO='git://github.com/theforeman/foreman.git'
-BRANCH='develop'
+BRANCH='74c114333c368aed777ac06e1a258603173b2f0b'
 
 function setup_build {
-LAST_COMMIT=$($GIT rev-list HEAD|/usr/bin/head -n 1)
-
-# Add 'nightly' to changelog
-
-DATE=$(date -R)
-UNIXTIME=$(date +%s)
-RELEASE="9999+debian1~nightlybuild${UNIXTIME}"
-MAINTAINER="Greg Sutcliffe <gsutclif@redhat.com>"
-
-mv debian/changelog debian/changelog.tmp
-
-echo "$PACKAGE_NAME ($RELEASE) UNRELEASED; urgency=low
-
-  * Automatically built package based on the state of
-    $REPO at commit $LAST_COMMIT
-
- -- $MAINTAINER  $DATE
-" > debian/changelog
-
-cat debian/changelog.tmp >> debian/changelog
-rm -f debian/changelog.tmp
-
-# Add 'nightly' to VERSION
-echo "`cat VERSION`-${LAST_COMMIT}" > VERSION
-
+  echo "rc: skipping setup for nightly"
 }
