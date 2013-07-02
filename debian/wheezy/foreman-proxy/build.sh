bd45ce92b19ac4fbbd3cc22e0e271778e1272c72
Greg Sutcliffe <greg.sutcliffe@gmail.com>
2013-07-02 10:34:52 +0100

    Release 1.2.0 foreman/foreman-proxy

--- debian/wheezy/foreman-proxy/build.sh
+++ debian/wheezy/foreman-proxy/build.sh
@@ -1,8 +1,8 @@
 PACKAGE_NAME='foreman-proxy'
 
 REPO='git://github.com/theforeman/smart-proxy.git'
-BRANCH='39eba0e7e9efbc26c07f40a3db2f81050a8fd0a0'
+BRANCH='e599316efc251efa8b2cbd7edef2ffe859ef79a8'
 
 function setup_build {
-  echo "rc: skipping setup for nightly"
+  echo "stable: skipping setup for nightly"
 }
