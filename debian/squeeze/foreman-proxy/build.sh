c78c6fac9503b12a9ff71e74733dc5b60c52066e
Greg Sutcliffe <gsutclif@redhat.com>
2013-07-04 17:57:53 +0100

    Update nightly build scripts from stable
    This reverts commit ea774e37d3921709f05b4566e10aeb21973e89b3.

--- debian/squeeze/foreman-proxy/build.sh
+++ debian/squeeze/foreman-proxy/build.sh
@@ -1,8 +1,29 @@
 PACKAGE_NAME='foreman-proxy'
 
 REPO='git://github.com/theforeman/smart-proxy.git'
-BRANCH='e599316efc251efa8b2cbd7edef2ffe859ef79a8'
+BRANCH='develop'
 
 function setup_build {
-  echo "stable: skipping setup for nightly"
+LAST_COMMIT=$($GIT rev-list HEAD|/usr/bin/head -n 1)
+
+# Add 'nightly' to changelog
+
+DATE=$(date -R)
+UNIXTIME=$(date +%s)
+RELEASE="9999+debian1~nightlybuild${UNIXTIME}"
+MAINTAINER="Greg Sutcliffe <gsutclif@redhat.com>"
+
+mv debian/changelog debian/changelog.tmp
+
+echo "$PACKAGE_NAME ($RELEASE) UNRELEASED; urgency=low
+
+  * Automatically built package based on the state of
+    $REPO at commit $LAST_COMMIT
+
+ -- $MAINTAINER  $DATE
+" > debian/changelog
+
+cat debian/changelog.tmp >> debian/changelog
+rm -f debian/changelog.tmp
+
 }
