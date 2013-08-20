481353c999a3f123b469038363d3627bc2b4f230
Dominic Cleal <dcleal@redhat.com>
2013-08-20 12:25:42 +0100

    Add plugin tags to SFK

--- rpms/pull_from_koji.sh
+++ rpms/pull_from_koji.sh
@@ -7,6 +7,22 @@
 
 koji=http://koji.katello.org
 here=$(dirname $0)
-sfk -h $koji -t foreman-nightly-rhel6 -t foreman-nightly-nonscl-rhel6 -o $here/epel-6 -e ^foreman $*
-sfk -h $koji -t foreman-nightly-fedora18 -o $here/fedora-18 -e ^foreman $*
-sfk -h $koji -t foreman-nightly-fedora19 -o $here/fedora-19 -e ^foreman $*
+
+sfk -h $koji \
+  -t foreman-nightly-rhel6 \
+  -t foreman-nightly-nonscl-rhel6 \
+  -t foreman-plugins-1.2-rhel6 \
+  -t foreman-plugins-nightly-rhel6 \
+  -o $here/epel-6 -e ^foreman $*
+
+sfk -h $koji \
+  -t foreman-nightly-fedora18 \
+  -t foreman-plugins-1.2-fedora18 \
+  -t foreman-plugins-nightly-fedora18 \
+  -o $here/fedora-18 -e ^foreman $*
+
+sfk -h $koji \
+  -t foreman-nightly-fedora19 \
+  -t foreman-plugins-1.2-fedora19 \
+  -t foreman-plugins-nightly-fedora19 \
+  -o $here/fedora-19 -e ^foreman $*
