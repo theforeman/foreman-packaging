b8da8dbb9f1affa9b098eccc9c2f3e9bf3d315cc
Lukas Zapletal <lzap+git@redhat.com>
2014-07-10 15:06:10 +0200

    fixes #6564 - Added -doc packages for all rubygems

---
+++ comps_doc.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+begin='<!--RGD-START-->'
+end='<!--RGD-END-->'
+
+TEMP=$(mktemp)
+trap "rm -f $TEMP" EXIT
+
+for FILE in comps/comps-*.xml
+do
+  for G in $(grep "%package doc" rubygem-*/*spec | awk -F/ '{print $1}'); do
+    grep "[>-]$G<" $FILE | sed 's/<\//-doc<\//'
+  done | sort -u > $TEMP
+  sed -i -e "/$begin/,/$end/{ /$begin/{p; r $TEMP
+    }; /$end/p; d }" $FILE
+done
