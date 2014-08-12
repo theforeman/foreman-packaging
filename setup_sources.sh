91a6bcf49b5dfee756ec350858e3e87f52ca9f36
Dominic Cleal <dcleal@redhat.com>
2014-08-12 13:43:10 +0100

    Ignore stderr in whereis existence check

--- setup_sources.sh
+++ setup_sources.sh
@@ -8,6 +8,6 @@ for spec in $dir/*.spec; do
   source0=$(spectool --list-files $spec | awk '{print $2}' | head -n1)
   sourcebase=$(basename "$source0")
   [ -h $d/$sourcebase ] || continue
-  git annex whereis "$d/$sourcebase" | grep -q " web" && continue
+  git annex whereis "$d/$sourcebase" 2>/dev/null | grep -q " web" && continue
   git annex addurl --file "$d/$sourcebase" "$source0"
 done
