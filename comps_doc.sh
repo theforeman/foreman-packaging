2b32310816a73fe33d0539153f9ffcb63bd94ebf
Dominic Cleal <dcleal@redhat.com>
2014-10-24 14:06:36 +0100

    Fix search for %package doc with more whitespace

--- comps_doc.sh
+++ comps_doc.sh
@@ -7,7 +7,7 @@ trap "rm -f $TEMP" EXIT
 
 for FILE in comps/comps-*.xml
 do
-  for G in $(grep "%package doc" rubygem-*/*spec | awk -F/ '{print $1}'); do
+  for G in $(egrep "%package\s+doc" rubygem-*/*spec | awk -F/ '{print $1}'); do
     grep "[>-]$G<" $FILE | sed 's/<\//-doc<\//'
   done | sort -u > $TEMP
   sed -i -e "/$begin/,/$end/{ /$begin/{p; r $TEMP
