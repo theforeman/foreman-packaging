fa10fa32b5e806df63a2ceb3477ef2fc6b04537e
Daniel Lobato Garcia <me@daniellobato.me>
2017-09-19 20:23:13 +0200

    Add --clone to copy-tags script

--- koji/copy-tags-commands.sh
+++ koji/copy-tags-commands.sh
@@ -14,7 +14,7 @@ NONSCL_SYSTEMS=""
 SCL_SYSTEMS="rhel7"
 
 clone() {
-  echo kkoji clone-tag --config $PRODUCT-$OLD-$SYSTEM $PRODUCT-$VERSION-$SYSTEM
+  echo kkoji clone-tag --config $PRODUCT-$OLD-$SYSTEM $PRODUCT-$VERSION-$SYSTEM --all
 
   echo kkoji add-tag --parent=$PRODUCT-$VERSION-$SYSTEM $PRODUCT-$VERSION-$SYSTEM-override
   echo kkoji add-tag --parent=$PRODUCT-$VERSION-$SYSTEM-override $PRODUCT-$VERSION-$SYSTEM-build
