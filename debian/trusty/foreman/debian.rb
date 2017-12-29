29f9b06403d8e02d921cd6c8fe0eb91fe7e41571
Michael Moll <kvedulv@kvedulv.de>
2017-12-29 03:22:08 +0100

    delete Ubuntu/trusty files

--- debian/trusty/foreman/debian.rb
+++
@@ -1,1 +0,0 @@
-gem 'concurrent-ruby', '= 1.0.3'
--- dependencies/trusty/rubyipmi/ruby-tests.rb
+++
@@ -1,3 +0,0 @@
-# there's a spec/ or a test/ directory in the upstream source, but
-# no test suite was defined in the Gem specification. It would be
-# a good idea to define it here so the package gets tested at build time.
