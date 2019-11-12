e34ad42152f86825e06ee228afe6b2f245799578
Evgeni Golov <evgeni@golov.de>
2019-11-12 15:32:05 +0100

    update for new naming of the pulp repo

--- add_pypi_package.sh
+++ add_pypi_package.sh
@@ -95,7 +95,7 @@ add_pypi_to_manifest() {
 		local section="plugin_nonscl_packages"
 	elif [[ $TITO_TAG == "katello-nightly-rhel7" ]] ; then
 		local section="katello_packages"
-	elif [[ $TITO_TAG == "katello-nightly-pulp3-rhel7" ]] ; then
+	elif [[ $TITO_TAG == "katello-nightly-pulpcore-el7" ]] ; then
 		local section="pulp_packages"
 	else
 		# TODO: client packages
