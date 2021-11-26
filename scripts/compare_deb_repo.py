5f45801a49b87e428353485ac8b4e185dbf013e5
Evgeni Golov <evgeni@golov.de>
2021-11-26 07:46:33 +0100

    make compare_deb_repo aware of stagingdeb

--- scripts/compare_deb_repo.py
+++ scripts/compare_deb_repo.py
@@ -14,9 +14,13 @@ PLUGINS = ['plugins']
 NIGHTLY_PACKAGES = ['foreman', 'foreman-installer', 'foreman-proxy']
 
 
-def get_repo_packages(dist, release='nightly', arch='amd64'):
+def get_repo_packages(dist, release='nightly', arch='amd64', staging=False):
     packages = defaultdict(lambda: '0')
-    remote_packages = requests.get(f'https://deb.theforeman.org/dists/{dist}/{release}/binary-{arch}/Packages')
+    if staging and dist != 'plugins':
+        repo_url = f'https://stagingdeb.theforeman.org/dists/{dist}/theforeman-{release}/binary-{arch}/Packages'
+    else:
+        repo_url = f'https://deb.theforeman.org/dists/{dist}/{release}/binary-{arch}/Packages'
+    remote_packages = requests.get(repo_url)
     for pkg in Packages.iter_paragraphs(remote_packages.text, use_apt_pkg=False):
         source = pkg.get('Source', pkg['Package'])
         version = pkg['Version']
@@ -53,10 +57,11 @@ def print_diff(dist, repo_packages, git_packages):
 def main():
     parser = argparse.ArgumentParser(description='compare git and debian repo')
     parser.add_argument('--release', default='nightly', help='release to compare for (default: %(default)s)')
+    parser.add_argument('--staging', action='store_true', help='check staging repository')
     args = parser.parse_args()
     for dist in DISTS + PLUGINS:
         packages = get_git_packages(dist, release=args.release)
-        repo_packages = get_repo_packages(dist, release=args.release)
+        repo_packages = get_repo_packages(dist, release=args.release, staging=args.staging)
         print_diff(dist, repo_packages, packages)
 
 
