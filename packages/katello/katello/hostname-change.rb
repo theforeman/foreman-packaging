theforeman/foreman-packaging

commit 7dccdc4816307acc463ffbf98dc2fb9c060ef233

Fixes #33743: Mention Puppet options separately in change hostname script

packages/katello/katello/hostname-change.rb
@@ -154,7 +154,10 @@ module KatelloUtilities
   On all #{@plural_proxy}, you will need to re-run the #{@options[:program]}-installer with this command:
 
   #{@options[:program]}-installer --foreman-proxy-foreman-base-url https://#{@new_hostname} \\
-                                  --foreman-proxy-trusted-hosts #{@new_hostname} \\
+                                  --foreman-proxy-trusted-hosts #{@new_hostname}
+
+  If Puppet is enabled on the #{proxy}, add the following to the installer command:
+
                                   --puppet-server-foreman-url https://#{@new_hostname}
 
   Short hostnames have not been updated, please update those manually.\n

packages/katello/katello/katello.spec
@@ -5,7 +5,7 @@
 %global confdir common
 %global prereleasesource master
 %global prerelease %{?prereleasesource:.}%{?prereleasesource}
-%global release 3
+%global release 4
 
 Name:       katello
 Version:    4.3.0
@@ -132,6 +132,9 @@ Provides a federation of katello services
 # the files section is empty, but without it no RPM will be generated
 
 %changelog
+* Wed Oct 20 2021 Eric D. Helms <ericdhelms@gmail.com> - 4.3.0-0.4.master
+- Mention Puppet options separately for change hostname
+
 * Mon Oct 18 2021 Eric D. Helms <ericdhelms@gmail.com> - 4.3.0-0.3.master
 - Remove dropped parameter in change-hostname
 - Fix broken branding caused by empty last_scenario

commit 2c46e6cbceb1cf7ef47fdb476620bf97a4afe881

Fixes #34098 - correctly print proxy type in Puppet message
Fixes: 7dccdc4816307acc463ffbf98dc2fb9c060ef233

packages/katello/katello/hostname-change.rb
@@ -156,7 +156,7 @@ module KatelloUtilities
   #{@options[:program]}-installer --foreman-proxy-foreman-base-url https://#{@new_hostname} \\
                                   --foreman-proxy-trusted-hosts #{@new_hostname}
 
-  If Puppet is enabled on the #{proxy}, add the following to the installer command:
+  If Puppet is enabled on the #{@proxy}, add the following to the installer command:
 
                                   --puppet-server-foreman-url https://#{@new_hostname}
 

packages/katello/katello/katello.spec
@@ -5,7 +5,7 @@
 %global confdir common
 %global prereleasesource master
 %global prerelease %{?prereleasesource:.}%{?prereleasesource}
-%global release 1
+%global release 2
 
 Name:       katello
 Version:    4.4.0
@@ -132,6 +132,9 @@ Provides a federation of katello services
 # the files section is empty, but without it no RPM will be generated
 
 %changelog
+* Tue Dec 07 2021 Evgeni Golov - 4.4.0-0.2.master
+- Fixes #34098 - correctly print proxy type in Puppet message
+
 * Wed Nov 17 2021 Chris Roberts <chrobert@redhat.com> - 4.4.0-0.1.master
 - 4.4.0 version bump
 