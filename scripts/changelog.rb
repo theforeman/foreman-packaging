theforeman/foreman-packaging

commit 4810c1de51c9c1b198e275abf2cc971348c241f2

Default to bumping release in changelog script

scripts/changelog.rb
@@ -9,6 +9,8 @@ author_name = ENV['GIT_AUTHOR_NAME'] || `git config --get user.name`.chomp
 author_email = ENV['DEBEMAIL'] || ENV['EMAIL'] || ENV['GIT_AUTHOR_EMAIL'] || `git config --get user.email`.chomp
 options[:author] = "#{author_name} <#{author_email}>" if !author_name.nil? && !author_email.nil?
 
+VERSION_REGEX = %r{\((?<version>.*)-(?<release>.*)\)}
+
 OptionParser.new do |opts|
   opts.banner = "Usage: changelog.rb -v [VERSION] [options] paths ..."
 
@@ -25,20 +27,28 @@ OptionParser.new do |opts|
   end
 end.parse!
 
-raise "missing --version" if options[:version].nil?
 raise "missing author" if options[:author].nil?
 raise "missing changelog file(s)" if ARGV.empty?
 ARGV.each { |f| raise "cannot find file #{f}" unless File.readable? f }
 
 options[:message] << "#{options[:version]} released" if options[:message].empty?
-options[:version] += '-1' unless options[:version].include?('-')
 
 ARGV.each do |path|
   control = File.read(File.join(File.dirname(path), 'control'))
   project = $1 if control =~ /Source:\s*(.+)/
 
+  version = options.fetch(:version, nil)
+
+  unless version
+    match = File.read(path).split("\n")[0].match(VERSION_REGEX)
+    current_version = match[:version]
+    current_release = match[:release]
+
+    version = "#{current_version}-#{current_release.to_i + 1}"
+  end
+
   logs = File.read(path)
-  entry = "#{project} (#{options[:version]}) stable; urgency=low\n\n"
+  entry = "#{project} (#{version}) stable; urgency=low\n\n"
   options[:message].each { |m| entry += "  * #{m}\n" }
   entry += "\n -- #{options[:author]}  #{Time.now.rfc2822}\n\n"
   File.open(path, 'w') { |f| f.write(entry + logs) }

commit a23a39b8a68b58e04ea94f3fef2c70ac26b828b5

automatically set revision to 1 when bumping packages
Fixes: 4810c1de51c9c1b198e275abf2cc971348c241f2

scripts/changelog.rb
@@ -47,6 +47,8 @@ ARGV.each do |path|
     version = "#{current_version}-#{current_release.to_i + 1}"
   end
 
+  version += '-1' unless version.include?('-')
+
   logs = File.read(path)
   entry = "#{project} (#{version}) stable; urgency=low\n\n"
   options[:message].each { |m| entry += "  * #{m}\n" }