d61794ac6d29d2818762f6a72e3a1fce0ff5aeb4
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2022-07-15 14:43:28 +0200

    Use modularity in repoclosure
    This removes the module_hotfixes and excludepkgs hack. To use a
    different modules.d directory, the --installroot parameter must be
    passed. This in turn requires the releasever to be set. This is actually
    also useful in determining which module(s) need to be enabled. Passing
    an installroot also uses a correct cache directory and further isolates
    dnf from the host system.
    
    Additionally a template is passed to mktemp for easier debugging.

--- repoclosure/repoclosure.sh
+++ repoclosure/repoclosure.sh
@@ -3,13 +3,43 @@
 set -e
 
 if [ $# -lt 2 ]; then
-	echo "Usage: $0 <yum.conf>"
+	echo "Usage: $0 <dnf.conf> <releasever>"
 	exit 1
 fi
 
 config=$1 ; shift
+releasever=$1 ; shift
 
-TEMPDIR=$(mktemp -d)
+TEMPDIR=$(mktemp -d -t repoclosure-XXXXXX)
 trap "rm -rf $TEMPDIR" EXIT
 
-dnf repoclosure -c $config "$@"
+if [[ $releasever == 8 ]] ; then
+	# Enable modularity on EL8
+	MODULESDIR=$TEMPDIR/etc/dnf/modules.d
+	mkdir -p $MODULESDIR
+	cat > $MODULESDIR/ruby.module <<-EOF
+	[ruby]
+	name=ruby
+	stream=2.7
+	profiles=
+	state=enabled
+	EOF
+
+	cat > $MODULESDIR/postgresql.module <<-EOF
+	[postgresql]
+	name=postgresql
+	stream=12
+	profiles=
+	state=enabled
+	EOF
+
+	cat > $MODULESDIR/foreman.module <<-EOF
+	[foreman]
+	name=foreman
+	stream=el$releasever
+	profiles=
+	state=enabled
+	EOF
+fi
+
+dnf --installroot $TEMPDIR --releasever $releasever repoclosure --config $config "$@"
