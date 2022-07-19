32bd74a4bcb77212223880a1789661308116dd1b
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2022-07-19 13:55:52 +0200

    Drop support for passing a custom repo URL to repoclosure
    This is unused and makes the script a lot easier.

--- repoclosure/repoclosure.sh
+++ repoclosure/repoclosure.sh
@@ -3,32 +3,16 @@
 set -e
 
 if [ $# -lt 2 ]; then
-  echo "Usage: $0 <yum.conf> <url>"
+  echo "Usage: $0 <yum.conf>"
   exit 1
 fi
 
-yumorig=$1
-url=$2
-shift; shift
+config=$1 ; shift
 
 TEMPDIR=$(mktemp -d)
 trap "rm -rf $TEMPDIR" EXIT
 
-# repo names must be unique, or yum will get confused between different OSes and URLs
-reponame=undertest-$(basename $yumorig .conf)-$(echo $url | cksum | sed 's/ /-/g')
-
-yumconf=$TEMPDIR/yum.conf
-cat $yumorig > $yumconf
-cat >> $yumconf << EOF
-
-[$reponame]
-name=$reponame
-gpgcheck=0
-baseurl=$url
-
-EOF
-
-repoclosure -c $yumconf -t -r $reponame $* 2>&1 | tee $TEMPDIR/repoclosure.log
+repoclosure -c $config -t $* 2>&1 | tee $TEMPDIR/repoclosure.log
 
 if tail -n1 $TEMPDIR/repoclosure.log | grep -q "Num Packages"; then
   exit 0
