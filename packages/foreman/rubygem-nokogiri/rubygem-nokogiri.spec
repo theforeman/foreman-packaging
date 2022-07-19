theforeman/foreman-packaging

commit d521dd04e9f4bd0b132aad86935c1fb9b6ce81bb

Update rubygem-nokogiri to 1.13.7

packages/foreman/rubygem-nokogiri/rubygem-nokogiri.spec
@@ -1,15 +1,11 @@
-# template: scl
-%{?scl:%scl_package rubygem-%{gem_name}}
-%{!?scl:%global pkg_name %{name}}
-
+# template: default
 %global gem_name nokogiri
 %global gem_require_name %{gem_name}
 
-Name: %{?scl_prefix}rubygem-%{gem_name}
-Version: 1.13.6
-Release: 2%{?dist}
+Name: rubygem-%{gem_name}
+Version: 1.13.7
+Release: 1%{?dist}
 Summary: Nokogiri (鋸) makes it easy and painless to work with XML and HTML from Ruby
-Group: Development/Languages
 License: MIT
 URL: https://nokogiri.org
 Source0: https://rubygems.org/gems/%{gem_name}-%{version}.gem
@@ -19,32 +15,22 @@ Source0: https://rubygems.org/gems/%{gem_name}-%{version}.gem
 Autoreq: 0
 
 # start specfile generated dependencies
-Requires: %{?scl_prefix_ruby}ruby(release)
-Requires: %{?scl_prefix_ruby}ruby >= 2.6.0
-Requires: %{?scl_prefix_ruby}ruby(rubygems)
-Requires: %{?scl_prefix}rubygem(mini_portile2) >= 2.8.0
-Requires: %{?scl_prefix}rubygem(mini_portile2) < 2.9
-BuildRequires: %{?scl_prefix_ruby}ruby(release)
-BuildRequires: %{?scl_prefix_ruby}ruby-devel >= 2.6.0
-BuildRequires: %{?scl_prefix_ruby}rubygems-devel
-BuildRequires: %{?scl_prefix}rubygem(mini_portile2) >= 2.8.0
-BuildRequires: %{?scl_prefix}rubygem(mini_portile2) < 2.9
-Provides: %{?scl_prefix}rubygem(%{gem_name}) = %{version}
+Requires: ruby >= 2.6.0
+BuildRequires: ruby-devel >= 2.6.0
+BuildRequires: rubygems-devel
+BuildRequires: rubygem(mini_portile2) >= 2.8.0
+BuildRequires: rubygem(mini_portile2) < 2.9
+# Compiler is required for build of gem binary extension.
+# https://fedoraproject.org/wiki/Packaging:C_and_C++#BuildRequires_and_Requires
+BuildRequires: gcc
 # end specfile generated dependencies
 BuildRequires: libxml2-devel
 BuildRequires: libxslt-devel
 
-%if 0%{?rhel} >= 8
 Requires: bundled(rubygem-racc) >= 1.4
 Requires: bundled(rubygem-racc) < 2
 BuildRequires: bundled(rubygem-racc) >= 1.4
 BuildRequires: bundled(rubygem-racc) < 2
-%else
-Requires: %{?scl_prefix}rubygem(racc) >= 1.4
-Requires: %{?scl_prefix}rubygem(racc) < 2
-BuildRequires: %{?scl_prefix}rubygem(racc) >= 1.4
-BuildRequires: %{?scl_prefix}rubygem(racc) < 2
-%endif
 
 %description
 Nokogiri (鋸) makes it easy and painless to work with XML and HTML from Ruby.
@@ -56,36 +42,23 @@ xerces (Java).
 
 
 %package doc
-Summary: Documentation for %{pkg_name}
-Group: Documentation
-Requires: %{?scl_prefix}%{pkg_name} = %{version}-%{release}
+Summary: Documentation for %{name}
+Requires: %{name} = %{version}-%{release}
 BuildArch: noarch
 
 %description doc
-Documentation for %{pkg_name}.
+Documentation for %{name}.
 
 %prep
-%{?scl:scl enable %{scl} - << \EOF}
-gem unpack %{SOURCE0}
-%{?scl:EOF}
-
-%setup -q -D -T -n  %{gem_name}-%{version}
-
-%{?scl:scl enable %{scl} - << \EOF}
-gem spec %{SOURCE0} -l --ruby > %{gem_name}.gemspec
-%{?scl:EOF}
+%setup -q -n  %{gem_name}-%{version}
 
 %build
 # Create the gem as gem install only works on a gem file
-%{?scl:scl enable %{scl} - << \EOF}
-gem build %{gem_name}.gemspec
-%{?scl:EOF}
+gem build ../%{gem_name}-%{version}.gemspec
 
 # %%gem_install compiles any C extensions and installs the gem into ./%%gem_dir
 # by default, so that we can move it into the buildroot in %%install
-%{?scl:scl enable %{scl} - << \EOF}
 %gem_install
-%{?scl:EOF}
 
 %install
 mkdir -p %{buildroot}%{gem_dir}
@@ -94,7 +67,7 @@ cp -a .%{gem_dir}/* \
 
 mkdir -p %{buildroot}%{gem_extdir_mri}/%{gem_name}
 cp -a .%{gem_extdir_mri}/gem.build_complete %{buildroot}%{gem_extdir_mri}/
-cp -a .%{gem_extdir_mri}/%{gem_name}/*.so %{buildroot}%{gem_extdir_mri}/%{gem_name}/
+cp -a .%{gem_extdir_mri}/%{gem_name}/*.so %{buildroot}%{gem_extdir_mri}/%{gem_name}
 
 # Prevent dangling symlink in -debuginfo (rhbz#878863).
 rm -rf %{buildroot}%{gem_instdir}/ext/
@@ -102,10 +75,10 @@ rm -rf %{buildroot}%{gem_instdir}/ext/
 mkdir -p %{buildroot}%{_bindir}
 cp -a .%{_bindir}/* \
         %{buildroot}%{_bindir}/
+
 find %{buildroot}%{gem_instdir}/bin -type f | xargs chmod a+x
 
 %check
-%{?scl:scl enable %{scl} - << \EOF}
 # Ideally, this would be something like this:
 # GEM_PATH="%{buildroot}%{gem_dir}:$GEM_PATH" ruby -e "require '%{gem_require_name}'"
 # But that fails to find native extensions on EL8, so we fake the structure that ruby expects
@@ -115,12 +88,10 @@ mkdir -p gem_ext_test/gems/extensions/%{_arch}-%{_target_os}/$(ruby -r rbconfig 
 cp -a %{buildroot}%{gem_extdir_mri} gem_ext_test/gems/extensions/%{_arch}-%{_target_os}/$(ruby -r rbconfig -e 'print RbConfig::CONFIG["ruby_version"]')/
 GEM_PATH="./gem_ext_test/gems:$GEM_PATH" ruby -e "require '%{gem_require_name}'"
 rm -rf gem_ext_test
-%{?scl:EOF}
 
 %files
 %dir %{gem_instdir}
 %{_bindir}/nokogiri
-%exclude %{gem_instdir}/ext
 %{gem_extdir_mri}
 %license %{gem_instdir}/LICENSE-DEPENDENCIES.md
 %license %{gem_instdir}/LICENSE.md
@@ -129,6 +100,7 @@ rm -rf gem_ext_test
 %{gem_instdir}/gumbo-parser
 %{gem_libdir}
 %{gem_instdir}/patches
+%exclude %{gem_instdir}/ports
 %exclude %{gem_cache}
 %{gem_spec}
 
@@ -138,6 +110,9 @@ rm -rf gem_ext_test
 %doc %{gem_instdir}/README.md
 
 %changelog
+* Sun Jul 17 2022 Foreman Packaging Automation <packaging@theforeman.org> 1.13.7-1
+- Update to 1.13.7
+
 * Wed May 18 2022 Eric D. Helms <ericdhelms@gmail.com> - 1.13.6-2
 - Add back disable of Autoreq
 

packages/foreman/rubygem-nokogiri/nokogiri-1.13.6.gem
@@ -1,1 +1,0 @@
-../../../.git/annex/objects/Z5/7G/SHA256E-s5379072--b1512fdc0aba446e1ee30de3e0671518eb363e75fab53486e99e8891d44b8587.6.gem/SHA256E-s5379072--b1512fdc0aba446e1ee30de3e0671518eb363e75fab53486e99e8891d44b8587.6.gem

packages/foreman/rubygem-nokogiri/nokogiri-1.13.7.gem
@@ -1,0 +1,1 @@
+../../../.git/annex/objects/P2/zp/SHA256E-s5379072--6ca1d753334418e749beb9bb69515a906451c9abfb9a5b060a36650419b61052.7.gem/SHA256E-s5379072--6ca1d753334418e749beb9bb69515a906451c9abfb9a5b060a36650419b61052.7.gem

commit ae1d58c93fbd85c38dc1acfea4d0ecba775e60ea

Turn on auto-requires for rubygem-nokogiri
In the new packaging template we rely on auto-requires to generate the
runtime dependencies. Turning this off but not stating the dependencies
breaks at runtime.

It hacks around it by removing racc as a dependency. The explicit
versions are stated so the build fails if they are ever changed. This
means they shouldn't go out of sync.

Fixes: d521dd04e9f4bd0b132aad86935c1fb9b6ce81bb

packages/foreman/rubygem-nokogiri/rubygem-nokogiri.spec
@@ -4,15 +4,11 @@
 
 Name: rubygem-%{gem_name}
 Version: 1.13.7
-Release: 1%{?dist}
+Release: 2%{?dist}
 Summary: Nokogiri (鋸) makes it easy and painless to work with XML and HTML from Ruby
 License: MIT
 URL: https://nokogiri.org
 Source0: https://rubygems.org/gems/%{gem_name}-%{version}.gem
-
-# On EL8 rubygem-racc is bundled into ruby-libs package and
-# auto-generated dependencies will break dependency resolution
-Autoreq: 0
 
 # start specfile generated dependencies
 Requires: ruby >= 2.6.0
@@ -27,6 +23,7 @@ BuildRequires: gcc
 BuildRequires: libxml2-devel
 BuildRequires: libxslt-devel
 
+# On EL8 rubygem-racc is bundled into ruby-libs package
 Requires: bundled(rubygem-racc) >= 1.4
 Requires: bundled(rubygem-racc) < 2
 BuildRequires: bundled(rubygem-racc) >= 1.4
@@ -51,6 +48,10 @@ Documentation for %{name}.
 
 %prep
 %setup -q -n  %{gem_name}-%{version}
+
+# On EL8 rubygem-racc is bundled into ruby-libs package and
+# auto-generated dependencies will break dependency resolution
+%gemspec_remove_dep -g racc "~> 1.4"
 
 %build
 # Create the gem as gem install only works on a gem file
@@ -110,6 +111,9 @@ rm -rf gem_ext_test
 %doc %{gem_instdir}/README.md
 
 %changelog
+* Tue Jul 19 2022 Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl> - 1.13.7-2
+- Turn on auto-requires again
+
 * Sun Jul 17 2022 Foreman Packaging Automation <packaging@theforeman.org> 1.13.7-1
 - Update to 1.13.7
 