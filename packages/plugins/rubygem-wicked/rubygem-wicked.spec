theforeman/foreman-packaging

commit c8cd0c659ff11c8366e30a2ff36e03323626be1e

Drop rubygem-foreman_docker and rubygem-hammer_cli_foreman_docker

comps/comps-foreman-plugins-el8.xml
@@ -55,7 +55,6 @@
       <packagereq type="default">rubygem-foreman_digitalocean</packagereq>
       <packagereq type="default">rubygem-foreman_discovery</packagereq>
       <packagereq type="default">rubygem-foreman_dlm</packagereq>
-      <packagereq type="default">rubygem-foreman_docker</packagereq>
       <packagereq type="default">rubygem-foreman_expire_hosts</packagereq>
       <packagereq type="default">rubygem-foreman_fog_proxmox</packagereq>
       <packagereq type="default">rubygem-foreman_git_templates</packagereq>
@@ -96,7 +95,6 @@
       <packagereq type="default">rubygem-hammer_cli_foreman_azure_rm</packagereq>
       <packagereq type="default">rubygem-hammer_cli_foreman_bootdisk</packagereq>
       <packagereq type="default">rubygem-hammer_cli_foreman_discovery</packagereq>
-      <packagereq type="default">rubygem-hammer_cli_foreman_docker</packagereq>
       <packagereq type="default">rubygem-hammer_cli_foreman_kubevirt</packagereq>
       <packagereq type="default">rubygem-hammer_cli_foreman_openscap</packagereq>
       <packagereq type="default">rubygem-hammer_cli_foreman_remote_execution</packagereq>
@@ -215,7 +213,6 @@
       <packagereq type="default">rubygem-foreman_digitalocean-doc</packagereq>
       <packagereq type="default">rubygem-foreman_discovery-doc</packagereq>
       <packagereq type="default">rubygem-foreman_dlm-doc</packagereq>
-      <packagereq type="default">rubygem-foreman_docker-doc</packagereq>
       <packagereq type="default">rubygem-foreman_expire_hosts-doc</packagereq>
       <packagereq type="default">rubygem-foreman_fog_proxmox-doc</packagereq>
       <packagereq type="default">rubygem-foreman_git_templates-doc</packagereq>
@@ -256,7 +253,6 @@
       <packagereq type="default">rubygem-hammer_cli_foreman_azure_rm-doc</packagereq>
       <packagereq type="default">rubygem-hammer_cli_foreman_bootdisk-doc</packagereq>
       <packagereq type="default">rubygem-hammer_cli_foreman_discovery-doc</packagereq>
-      <packagereq type="default">rubygem-hammer_cli_foreman_docker-doc</packagereq>
       <packagereq type="default">rubygem-hammer_cli_foreman_kubevirt-doc</packagereq>
       <packagereq type="default">rubygem-hammer_cli_foreman_openscap-doc</packagereq>
       <packagereq type="default">rubygem-hammer_cli_foreman_remote_execution-doc</packagereq>

package_manifest.yaml
@@ -981,7 +981,6 @@ plugin_scl_packages:
     rubygem-foreman_digitalocean: {}
     rubygem-foreman_discovery: {}
     rubygem-foreman_dlm: {}
-    rubygem-foreman_docker: {}
     rubygem-foreman_expire_hosts: {}
     rubygem-foreman_fog_proxmox: {}
     rubygem-foreman_git_templates: {}
@@ -1021,7 +1020,6 @@ plugin_scl_packages:
     rubygem-hammer_cli_foreman_azure_rm: {}
     rubygem-hammer_cli_foreman_bootdisk: {}
     rubygem-hammer_cli_foreman_discovery: {}
-    rubygem-hammer_cli_foreman_docker: {}
     rubygem-hammer_cli_foreman_kubevirt: {}
     rubygem-hammer_cli_foreman_leapp: {}
     rubygem-hammer_cli_foreman_openscap: {}

rel-eng/tito.props
@@ -664,7 +664,6 @@ whitelist = ansiblerole-foreman_scap_client
   rubygem-foreman_digitalocean
   rubygem-foreman_discovery
   rubygem-foreman_dlm
-  rubygem-foreman_docker
   rubygem-foreman_expire_hosts
   rubygem-foreman_fog_proxmox
   rubygem-foreman_git_templates
@@ -706,7 +705,6 @@ whitelist = ansiblerole-foreman_scap_client
   rubygem-hammer_cli_foreman_azure_rm
   rubygem-hammer_cli_foreman_bootdisk
   rubygem-hammer_cli_foreman_discovery
-  rubygem-hammer_cli_foreman_docker
   rubygem-hammer_cli_foreman_kubevirt
   rubygem-hammer_cli_foreman_openscap
   rubygem-hammer_cli_foreman_remote_execution
@@ -829,7 +827,6 @@ whitelist = foreman-discovery-image-service
   rubygem-foreman_digitalocean
   rubygem-foreman_discovery
   rubygem-foreman_dlm
-  rubygem-foreman_docker
   rubygem-foreman_expire_hosts
   rubygem-foreman_fog_proxmox
   rubygem-foreman_git_templates
@@ -871,7 +868,6 @@ whitelist = foreman-discovery-image-service
   rubygem-hammer_cli_foreman_azure_rm
   rubygem-hammer_cli_foreman_bootdisk
   rubygem-hammer_cli_foreman_discovery
-  rubygem-hammer_cli_foreman_docker
   rubygem-hammer_cli_foreman_kubevirt
   rubygem-hammer_cli_foreman_leapp
   rubygem-hammer_cli_foreman_openscap

packages/plugins/rubygem-foreman_docker/foreman_docker-5.0.0.gem
@@ -1,1 +1,0 @@
-../../../.git/annex/objects/mw/Xv/SHA256E-s96256--3783356364d62bc8a2c26318e8fda005f2984faa6c378d7f002087ab7299cfc8.0.gem/SHA256E-s96256--3783356364d62bc8a2c26318e8fda005f2984faa6c378d7f002087ab7299cfc8.0.gem

packages/plugins/rubygem-foreman_docker/rubygem-foreman_docker.spec
@@ -1,223 +1,0 @@
-# template: foreman_plugin
-%{?scl:%scl_package rubygem-%{gem_name}}
-%{!?scl:%global pkg_name %{name}}
-
-%global gem_name foreman_docker
-%global plugin_name docker
-%global foreman_min_version 1.17.0
-
-Summary:    Provision and manage Docker containers and images from Foreman
-Name:       %{?scl_prefix}rubygem-%{gem_name}
-Version:    5.0.0
-Release:    3%{?foremandist}%{?dist}
-Group:      Applications/Systems
-License:    GPLv3
-URL:        https://github.com/theforeman/foreman_docker
-Source0:    https://rubygems.org/gems/%{gem_name}-%{version}.gem
-
-# start specfile generated dependencies
-Requires: foreman >= %{foreman_min_version}
-Requires: %{?scl_prefix_ruby}ruby(release)
-Requires: %{?scl_prefix_ruby}ruby
-Requires: %{?scl_prefix_ruby}ruby(rubygems)
-Requires: %{?scl_prefix}rubygem(deface) < 2.0
-Requires: %{?scl_prefix}rubygem(wicked) >= 1.1
-Requires: %{?scl_prefix}rubygem(wicked) < 2
-BuildRequires: foreman-assets >= %{foreman_min_version}
-BuildRequires: foreman-plugin >= %{foreman_min_version}
-BuildRequires: %{?scl_prefix}rubygem(deface) < 2.0
-BuildRequires: %{?scl_prefix}rubygem(wicked) >= 1.1
-BuildRequires: %{?scl_prefix}rubygem(wicked) < 2
-BuildRequires: %{?scl_prefix_ruby}ruby(release)
-BuildRequires: %{?scl_prefix_ruby}ruby
-BuildRequires: %{?scl_prefix_ruby}rubygems-devel
-BuildArch: noarch
-Provides: %{?scl_prefix}rubygem(%{gem_name}) = %{version}
-Provides: foreman-plugin-%{plugin_name} = %{version}
-# end specfile generated dependencies
-Provides: foreman-docker
-%{?scl:Obsoletes: ruby193-rubygem-%{gem_name}}
-
-%description
-Provision and manage Docker containers and images from Foreman.
-This version does not provide any functionality and only makes plugin removal easier.
-
-
-%package doc
-BuildArch:  noarch
-Group:      Documentation
-Requires:   %{?scl_prefix}%{pkg_name} = %{version}-%{release}
-%{?scl:Obsoletes: ruby193-rubygem-%{gem_name}-doc}
-Summary:    Documentation for %{pkg_name}
-
-%description doc
-Documentation for %{pkg_name}.
-
-%prep
-%{?scl:scl enable %{scl} - << \EOF}
-gem unpack %{SOURCE0}
-%{?scl:EOF}
-
-%setup -q -D -T -n  %{gem_name}-%{version}
-
-%{?scl:scl enable %{scl} - << \EOF}
-gem spec %{SOURCE0} -l --ruby > %{gem_name}.gemspec
-%{?scl:EOF}
-
-%build
-# Create the gem as gem install only works on a gem file
-%{?scl:scl enable %{scl} - << \EOF}
-gem build %{gem_name}.gemspec
-%{?scl:EOF}
-
-# %%gem_install compiles any C extensions and installs the gem into ./%%gem_dir
-# by default, so that we can move it into the buildroot in %%install
-%{?scl:scl enable %{scl} - << \EOF}
-%gem_install
-%{?scl:EOF}
-
-%install
-mkdir -p %{buildroot}%{gem_dir}
-cp -pa .%{gem_dir}/* \
-        %{buildroot}%{gem_dir}/
-
-%foreman_bundlerd_file
-%foreman_precompile_plugin -a -s
-
-%files
-%dir %{gem_instdir}
-%exclude %{gem_instdir}/.rubocop.yml
-%license %{gem_instdir}/LICENSE
-%{gem_instdir}/app
-%{gem_instdir}/config
-%{gem_instdir}/db
-%{gem_libdir}
-%{gem_instdir}/locale
-%exclude %{gem_cache}
-%{gem_spec}
-%{foreman_bundlerd_plugin}
-%{foreman_apipie_cache_foreman}
-%{foreman_assets_plugin}
-
-%files doc
-%doc %{gem_docdir}
-%doc %{gem_instdir}/README.md
-%{gem_instdir}/Rakefile
-%{gem_instdir}/test
-
-%changelog
-* Tue Jan 07 2020 Eric D. Helms <ericdhelms@gmail.com> - 5.0.0-3
-- Drop migrate, seed and restart posttans
-
-* Thu Mar 28 2019 Evgeni Golov - 5.0.0-2
-- Drop foreman-compute requirement
-
-* Wed Mar 20 2019 Marek Hulan <mhulan@redhat.com> 5.0.0-1
-- Update to 5.0.0
-
-* Wed Feb 27 2019 Sebastian Gräßl <mail@bastilian.me> 4.1.1-1
-- Update to 4.1.1
-
-* Fri Sep 07 2018 Eric D. Helms <ericdhelms@gmail.com> - 4.1.0-2
-- Rebuild for Rails 5.2 and Ruby 2.5
-
-* Sun May 27 2018 Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl> - 4.1.0-1
-- Update to 4.1.0
-- Regenerate spec file based on the current template
-
-* Thu Jan 11 2018 Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl> 4.0.0-1
-- Release foreman_docker 4.0.0 (ericdhelms@gmail.com)
-- Use HTTPS URLs for github and rubygems (ewoud@kohlvanwijngaarden.nl)
-- Set proper download URLs for rubygems (komidore64@gmail.com)
-
-* Wed Jun 28 2017 Eric D. Helms <ericdhelms@gmail.com> 3.2.1-1
-- plugins:foreman_docker - Release 3.2.1 (mail@bastilian.me)
-
-* Mon Jun 19 2017 Eric D. Helms <ericdhelms@gmail.com> 3.1.0-1
-- Updated foreman_docker to 3.1.0 (me@daniellobato.me)
-- Switch to using gem_install macro (ericdhelms@gmail.com)
-
-* Mon Aug 01 2016 Eric D Helms <ericdhelms@gmail.com> 3.0.0-1
-- plugins: foreman-docker 3.0.0 (elobatocs@gmail.com)
-
-* Fri Apr 15 2016 Dominic Cleal <dominic@cleal.org> 2.1.1-1
-- plugins:foreman_docker - 2.1.1 (elobatocs@gmail.com)
-
-* Mon Jan 18 2016 Dominic Cleal <dcleal@redhat.com> 2.0.1-1
-- plugins:foreman_docker - Release 2.0.1 (elobatocs@gmail.com)
-
-* Wed Jan 06 2016 Dominic Cleal <dcleal@redhat.com> 2.0.0-2
-- Fix docker-api dependency version number in gemspec (dcleal@redhat.com)
-
-* Wed Jan 06 2016 Dominic Cleal <dcleal@redhat.com> 2.0.0-1
-- plugins:foreman_docker - Release 2.0.0 (elobatocs@gmail.com)
-- Replace ruby(abi) for ruby22 rebuild (dcleal@redhat.com)
-
-* Thu Aug 27 2015 Dominic Cleal <dcleal@redhat.com> 1.4.1-2
-- Converted to tfm SCL (dcleal@redhat.com)
-
-* Wed Aug 19 2015 Dominic Cleal <dcleal@redhat.com> 1.4.1-1
-- plugins:foreman_docker - Release 1.4.1 (elobatocs@gmail.com)
-
-* Tue Jul 28 2015 Dominic Cleal <dcleal@redhat.com> 1.4.0-2
-- Use foremandist macro in release (dcleal@redhat.com)
-
-* Fri Jul 24 2015 Dominic Cleal <dcleal@redhat.com> 1.4.0-1.fm1_10
-- Update foreman_docker to 1.4.0 (dcleal@redhat.com)
-
-* Wed Apr 29 2015 Dominic Cleal <dcleal@redhat.com> 1.3.1-1.fm1_9
-- plugins:foreman_docker - Release 1.3.1 (elobatocs@gmail.com)
-
-* Wed Apr 29 2015 Dominic Cleal <dcleal@redhat.com> 1.3.0-1.fm1_9
-- plugins:foreman_docker - Release 1.3.0 (elobatocs@gmail.com)
-- Partially revert "Better branched builds with Foreman version macro"
-  (dcleal@redhat.com)
-
-* Thu Apr 23 2015 Dominic Cleal <dcleal@redhat.com> 1.2.4-2
-- Use foremandist macro in release (dcleal@redhat.com)
-
-* Wed Mar 25 2015 Dominic Cleal <dcleal@redhat.com> 1.2.4-1.fm1_9
-- plugins:foreman_docker - Release 1.2.4 (elobatocs@gmail.com)
-- Min of foreman 1.8.0 due to post-install scriptlet (dcleal@redhat.com)
-
-* Fri Mar 20 2015 Dominic Cleal <dcleal@redhat.com> 1.2.3-1.fm1_9
-- Update foreman_docker to 1.2.3 (dcleal@redhat.com)
-
-* Wed Mar 11 2015 Daniel Lobato Garcia <dlobatog@redhat.com> 1.2.2-1.fm1_8
-- Updating the version of foreman_docker to 1.2.2
-
-* Wed Mar 04 2015 Dominic Cleal <dcleal@redhat.com> 1.2.1-2.fm1_8
-- Precompile foreman_docker API docs (dcleal@redhat.com)
-
-* Tue Mar 03 2015 Daniel Lobato Garcia <dlobatog@redhat.com> 1.2.1-1
-- Updating the version of foreman_docker to 1.2.1
-
-* Tue Feb 24 2015 Daniel Lobato Garcia <dlobatog@redhat.com> 1.2.0-1
-- Updating the version of foreman_docker to 1.2.0
-- docker-api dependency listed to 1.17 minimum
-
-* Mon Feb 23 2015 Dominic Cleal <dcleal@redhat.com> 1.1.0-2
-- Default options in foreman_precompile_plugin changed (rubygem-foreman_docker)
-  (martin.bacovsky@gmail.com)
-
-* Tue Feb 10 2015 Daniel Lobato Garcia <dlobatog@redhat.com> 1.1.0-1
-- Support katello content views to create containers
-
-* Thu Feb 5 2015 Daniel Lobato Garcia <dlobatog@redhat.com> 1.0.1-1
-- Fixes for 1.7 compatibility
-- Do not list the assets directory twice. (slukasik@redhat.com)
-
-* Mon Jan 12 2015 Daniel Lobato Garcia <dlobatog@redhat.com> 1.0.0-1
-- Updating the version of foreman_docker to 1.0.0
-
-* Wed Nov 05 2014 Dominic Cleal <dcleal@redhat.com> 0.2.0-2
-- Precompile foreman_docker assets (dcleal@redhat.com)
-
-* Mon Nov 3 2014 Daniel Lobato Garcia <dlobatog@redhat.com> 0.2.0-1
-- Updating the version of foreman_docker to 0.2.0
-
-* Tue Oct 21 2014 David Davis <daviddavis@redhat.com> 0.1.0-1
-- Updating the version of foreman_docker to 0.1.0
-
-* Tue Apr 29 2014 Dominic Cleal <dcleal@redhat.com> 0.0.3-1
-- new package built with tito

packages/plugins/rubygem-hammer_cli_foreman_docker/hammer_cli_foreman_docker-0.0.7.gem
@@ -1,1 +1,0 @@
-../../../.git/annex/objects/Ff/qK/SHA256E-s31744--d691a5799b6e83eb700ac183e0d1973fa40a27f214908a1cc0f17ca80c297b71.7.gem/SHA256E-s31744--d691a5799b6e83eb700ac183e0d1973fa40a27f214908a1cc0f17ca80c297b71.7.gem

packages/plugins/rubygem-hammer_cli_foreman_docker/rubygem-hammer_cli_foreman_docker.spec
@@ -1,111 +1,0 @@
-%{?scl:%scl_package rubygem-%{gem_name}}
-%{!?scl:%global pkg_name %{name}}
-
-%{!?_root_sysconfdir:%global _root_sysconfdir %{_sysconfdir}}
-
-%global gem_name hammer_cli_foreman_docker
-%global confdir hammer
-
-Summary: Foreman Docker-related commands for Hammer
-Name: %{?scl_prefix}rubygem-%{gem_name}
-Version: 0.0.7
-Release: 1%{?dist}
-Group: Applications/System
-License: GPLv3+
-URL: https://github.com/theforeman/hammer_cli_foreman_docker
-Source0: https://rubygems.org/gems/%{gem_name}-%{version}.gem
-
-Requires: %{?scl_prefix_ruby}ruby(release)
-Requires: %{?scl_prefix_ruby}ruby(rubygems)
-Requires: %{?scl_prefix}rubygem(hammer_cli_foreman) >= 0.1.2
-BuildRequires: %{?scl_prefix_ruby}ruby(rubygems)
-BuildRequires: %{?scl_prefix_ruby}rubygems-devel
-BuildRequires: %{?scl_prefix_ruby}ruby
-BuildArch: noarch
-Provides: %{?scl_prefix}rubygem(%{gem_name}) = %{version}
-%{?scl:Obsoletes: ruby193-rubygem-%{gem_name}}
-%if 0%{?scl:1}
-Obsoletes: rubygem-%{gem_name} < 0.0.3-3
-%endif
-
-%description
-Foreman docker plugin for Hammer CLI provides docker-related commands on
-command line
-
-%package doc
-Summary: Documentation for %{pkg_name}
-Group: Documentation
-Requires: %{?scl_prefix}%{pkg_name} = %{version}-%{release}
-%{?scl:Obsoletes: ruby193-rubygem-%{gem_name}-doc}
-BuildArch: noarch
-%if 0%{?scl:1}
-Obsoletes: rubygem-%{gem_name}-doc < 0.0.3-3
-%endif
-
-%description doc
-Documentation for %{pkg_name}
-
-%prep
-%setup -n %{pkg_name}-%{version} -q -c -T
-%{?scl:scl enable %{scl} - <<EOF}
-%gem_install -n %{SOURCE0}
-%{?scl:EOF}
-
-%install
-mkdir -p %{buildroot}%{gem_dir}
-cp -pa .%{gem_dir}/* \
-        %{buildroot}%{gem_dir}/
-
-mkdir -p %{buildroot}%{_root_sysconfdir}/%{confdir}/cli.modules.d
-install -m 755 .%{gem_instdir}/config/foreman_docker.yml \
-               %{buildroot}%{_root_sysconfdir}/%{confdir}/cli.modules.d/foreman_docker.yml
-
-%files
-%dir %{gem_instdir}
-%{gem_instdir}/lib
-%{gem_instdir}/locale
-%config(noreplace) %{_root_sysconfdir}/%{confdir}/cli.modules.d/foreman_docker.yml
-%exclude %{gem_cache}
-%{gem_spec}
-%{gem_instdir}/LICENSE
-
-%files doc
-%doc %{gem_docdir}
-%doc %{gem_instdir}/README.md
-%doc %{gem_instdir}/config
-%doc %{gem_instdir}/doc
-
-%changelog
-* Fri Oct 23 2020 Oleh Fedorenko <ofedoren@redhat.com> 0.0.7-1
-- Update to 0.0.7
-
-* Wed May 13 2020 Oleh Fedorenko <ofedoren@redhat.com> 0.0.5-1
-- Update to 0.0.5
-
-* Fri Sep 07 2018 Eric D. Helms <ericdhelms@gmail.com> - 0.0.4-4
-- Rebuild for Rails 5.2 and Ruby 2.5
-
-* Tue Jan 09 2018 Eric D. Helms <ericdhelms@gmail.com> 0.0.4-3
-- Bump releases for base foreman plugins packages (ericdhelms@gmail.com)
-- Use HTTPS URLs for github and rubygems (ewoud@kohlvanwijngaarden.nl)
-
-* Wed May 04 2016 Dominic Cleal <dominic@cleal.org> 0.0.4-2
-- Use gem_install macro (dominic@cleal.org)
-
-* Wed Mar 16 2016 Dominic Cleal <dominic@cleal.org> 0.0.4-1
-- Update hammer_cli_foreman_docker to 0.0.4 (daviddavis@redhat.com)
-
-* Thu Dec 24 2015 Dominic Cleal <dcleal@redhat.com> 0.0.3-5
-- Replace ruby(abi) for ruby22 rebuild (dcleal@redhat.com)
-
-* Wed Aug 26 2015 Dominic Cleal <dcleal@redhat.com> 0.0.3-4
-- Converted to tfm SCL (dcleal@redhat.com)
-
-* Tue Jul 28 2015 Dominic Cleal <dcleal@redhat.com> 0.0.3-3
-- fixes #8979 - convert hammer packages to SCL (dcleal@redhat.com)
-
-* Mon Mar 16 2015 Dominic Cleal <dcleal@redhat.com> 0.0.3-2
-- Fix changelog formatting (dcleal@redhat.com)
-
-* Mon Mar 16 2015 Martin Bačovský <martin.bacovsky@gmail.com> 0.0.3-1
-- new package built with tito

commit 06e45789b579dc02e1bb40272d0cc16a7cd9f32c

Remove unused package rubygem-wicked
Fixes: c8cd0c659ff11c8366e30a2ff36e03323626be1e

comps/comps-foreman-plugins-el8.xml
@@ -187,7 +187,6 @@
       <packagereq type="default">rubygem-timeliness</packagereq>
       <packagereq type="default">rubygem-ttfunk</packagereq>
       <packagereq type="default">rubygem-vault</packagereq>
-      <packagereq type="default">rubygem-wicked</packagereq>
       <packagereq type="default">rubygem-zscheduler</packagereq>
       <packagereq type="default">yggdrasil</packagereq>
       <packagereq type="default">yggdrasil-forwarder-worker</packagereq>
@@ -359,7 +358,6 @@
       <packagereq type="default">rubygem-timeliness-doc</packagereq>
       <packagereq type="default">rubygem-ttfunk-doc</packagereq>
       <packagereq type="default">rubygem-vault-doc</packagereq>
-      <packagereq type="default">rubygem-wicked-doc</packagereq>
       <packagereq type="default">rubygem-zscheduler-doc</packagereq>
       <!--RGD-END-->
     </packagelist>

package_manifest.yaml
@@ -774,7 +774,6 @@ foreman_plugin_packages:
     rubygem-timeliness: {}
     rubygem-ttfunk: {}
     rubygem-vault: {}
-    rubygem-wicked: {}
     rubygem-zscheduler: {}
     yggdrasil-worker-forwarder: {}
 

packages/plugins/rubygem-wicked/rubygem-wicked.spec
@@ -1,132 +1,0 @@
-# Generated from wicked-1.3.3.gem by gem2rpm -*- rpm-spec -*-
-# template: scl
-%{?scl:%scl_package rubygem-%{gem_name}}
-%{!?scl:%global pkg_name %{name}}
-
-%global gem_name wicked
-
-Name: %{?scl_prefix}rubygem-%{gem_name}
-Version: 1.3.3
-Release: 3%{?dist}
-Summary: Use Wicked to turn your controller into a wizard
-Group: Development/Languages
-License: MIT
-URL: https://github.com/schneems/wicked
-Source0: https://rubygems.org/gems/%{gem_name}-%{version}.gem
-
-# start specfile generated dependencies
-Requires: %{?scl_prefix_ruby}ruby(release)
-Requires: %{?scl_prefix_ruby}ruby
-Requires: %{?scl_prefix_ruby}ruby(rubygems)
-Requires: %{?scl_prefix}rubygem(railties) >= 3.0.7
-BuildRequires: %{?scl_prefix_ruby}ruby(release)
-BuildRequires: %{?scl_prefix_ruby}ruby
-BuildRequires: %{?scl_prefix_ruby}rubygems-devel
-BuildArch: noarch
-Provides: %{?scl_prefix}rubygem(%{gem_name}) = %{version}
-# end specfile generated dependencies
-
-%description
-Wicked is a Rails engine for producing easy wizard controllers.
-
-
-%package doc
-Summary: Documentation for %{pkg_name}
-Group: Documentation
-Requires: %{?scl_prefix}%{pkg_name} = %{version}-%{release}
-BuildArch: noarch
-
-%description doc
-Documentation for %{pkg_name}.
-
-%prep
-%{?scl:scl enable %{scl} - << \EOF}
-gem unpack %{SOURCE0}
-%{?scl:EOF}
-
-%setup -q -D -T -n  %{gem_name}-%{version}
-
-%{?scl:scl enable %{scl} - << \EOF}
-gem spec %{SOURCE0} -l --ruby > %{gem_name}.gemspec
-%{?scl:EOF}
-
-%build
-# Create the gem as gem install only works on a gem file
-%{?scl:scl enable %{scl} - << \EOF}
-gem build %{gem_name}.gemspec
-%{?scl:EOF}
-
-# %%gem_install compiles any C extensions and installs the gem into ./%%gem_dir
-# by default, so that we can move it into the buildroot in %%install
-%{?scl:scl enable %{scl} - << \EOF}
-%gem_install
-%{?scl:EOF}
-
-%install
-mkdir -p %{buildroot}%{gem_dir}
-cp -pa .%{gem_dir}/* \
-        %{buildroot}%{gem_dir}/
-
-%files
-%dir %{gem_instdir}
-%exclude %{gem_instdir}/.gitignore
-%exclude %{gem_instdir}/.travis.yml
-%{gem_instdir}/Appraisals
-%license %{gem_instdir}/MIT-LICENSE
-%{gem_instdir}/gemfiles
-%{gem_libdir}
-%exclude %{gem_cache}
-%{gem_spec}
-
-%files doc
-%doc %{gem_docdir}
-%doc %{gem_instdir}/CHANGELOG.md
-%doc %{gem_instdir}/CONTRIBUTING.md
-%{gem_instdir}/Gemfile
-%doc %{gem_instdir}/README.md
-%{gem_instdir}/Rakefile
-%{gem_instdir}/test
-%{gem_instdir}/wicked.gemspec
-
-%changelog
-* Tue Apr 06 2021 Eric D. Helms <ericdhelms@gmail.com> - 1.3.3-3
-- Rebuild for Ruby 2.7
-
-* Fri Jan 17 2020 Zach Huntington-Meath <zhunting@redhat.com> - 1.3.3-2
-- Update spec to remove the ror scl
-
-* Wed Sep 26 2018 Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl> 1.3.3-1
-- Update to 1.3.3-1
-
-* Fri Sep 07 2018 Eric D. Helms <ericdhelms@gmail.com> - 1.3.2-2
-- Rebuild for Rails 5.2 and Ruby 2.5
-
-* Thu Jan 11 2018 Eric D. Helms <ericdhelms@gmail.com> 1.3.2-1
-- Bump rubygem-wicked to 1.3.2 (ericdhelms@gmail.com)
-- Use HTTPS URLs for github and rubygems (ewoud@kohlvanwijngaarden.nl)
-- Set proper download URLs for rubygems (komidore64@gmail.com)
-- Switch to using gem_install macro (ericdhelms@gmail.com)
-
-* Thu Apr 21 2016 Dominic Cleal <dominic@cleal.org> 1.1.0-4
-- Rebuild tfm against sclo-ror42 (dominic@cleal.org)
-
-* Thu Dec 24 2015 Dominic Cleal <dcleal@redhat.com> 1.1.0-3
-- Update dependencies to ror SCL (dcleal@redhat.com)
-- Replace ruby(abi) for ruby22 rebuild (dcleal@redhat.com)
-
-* Wed Aug 26 2015 Dominic Cleal <dcleal@redhat.com> 1.1.0-2
-- Fix checks against scl name, optimise rhel/empty SCL conditional
-  (dcleal@redhat.com)
-- Converted to tfm SCL (dcleal@redhat.com)
-
-* Mon Nov 03 2014 Daniel Lobato Garcia <dlobatog@redhat.com> 1.1.0-1
-- Update release to 1.1.0 (dlobatog@redhat.com)
-
-* Fri Jun 06 2014 Dominic Cleal <dcleal@redhat.com> 1.0.2-3
-- Modernise and update for EL7 (dcleal@redhat.com)
-
-* Tue Apr 08 2014 Marek Hulan <mhulan@redhat.com> 1.0.2-2
-- Fixing wicked-doc dependency (mhulan@redhat.com)
-
-* Wed Apr 02 2014 Marek Hulan <mhulan@redhat.com> 1.0.2-1
-- new package built with tito

packages/plugins/rubygem-wicked/wicked-1.3.3.gem
@@ -1,1 +1,0 @@
-../../../.git/annex/objects/5f/vz/SHA256E-s93184--c93668a68984e30f305db89c7f2719801e3e65f2b1fc8760ca300645a7b2c5f0.3.gem/SHA256E-s93184--c93668a68984e30f305db89c7f2719801e3e65f2b1fc8760ca300645a7b2c5f0.3.gem

rel-eng/tito.props
@@ -501,7 +501,6 @@ whitelist = ansible-collection-theforeman-foreman
   rubygem-timeliness
   rubygem-ttfunk
   rubygem-vault
-  rubygem-wicked
   rubygem-zscheduler
   yggdrasil-worker-forwarder
 