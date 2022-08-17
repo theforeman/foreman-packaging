theforeman/foreman-packaging

commit 04ab0aa1b34f6ae18daee489b4b6119b79ec1fc3

Fixes #33879 - drop foreman-xen plugin

package_manifest.yaml
@@ -1018,7 +1018,6 @@ plugin_scl_packages:
     rubygem-foreman_webhooks: {}
     rubygem-foreman_vmwareannotations: {}
     rubygem-foreman_wreckingball: {}
-    rubygem-foreman_xen: {}
     rubygem-git: {}
     rubygem-graphite-api: {}
     rubygem-hammer_cli_foreman_admin: {}

rel-eng/tito.props
@@ -709,7 +709,6 @@ whitelist = ansible-collection-theforeman-foreman
   rubygem-foreman_vmwareannotations
   rubygem-foreman_webhooks
   rubygem-foreman_wreckingball
-  rubygem-foreman_xen
   rubygem-git
   rubygem-graphite-api
   rubygem-hammer_cli_foreman_admin
@@ -876,7 +875,6 @@ whitelist = foreman-discovery-image-service
   rubygem-foreman_vmwareannotations
   rubygem-foreman_webhooks
   rubygem-foreman_wreckingball
-  rubygem-foreman_xen
   rubygem-git
   rubygem-graphite-api
   rubygem-hammer_cli_foreman_admin

comps/comps-foreman-plugins-el8.xml
@@ -90,7 +90,6 @@
       <packagereq type="default">rubygem-foreman_vmwareannotations</packagereq>
       <packagereq type="default">rubygem-foreman_webhooks</packagereq>
       <packagereq type="default">rubygem-foreman_wreckingball</packagereq>
-      <packagereq type="default">rubygem-foreman_xen</packagereq>
       <packagereq type="default">rubygem-git</packagereq>
       <packagereq type="default">rubygem-graphite-api</packagereq>
       <packagereq type="default">rubygem-hammer_cli_foreman_admin</packagereq>
@@ -253,7 +252,6 @@
       <packagereq type="default">rubygem-foreman_vmwareannotations-doc</packagereq>
       <packagereq type="default">rubygem-foreman_webhooks-doc</packagereq>
       <packagereq type="default">rubygem-foreman_wreckingball-doc</packagereq>
-      <packagereq type="default">rubygem-foreman_xen-doc</packagereq>
       <packagereq type="default">rubygem-graphite-api-doc</packagereq>
       <packagereq type="default">rubygem-hammer_cli_foreman_admin-doc</packagereq>
       <packagereq type="default">rubygem-hammer_cli_foreman_ansible-doc</packagereq>

comps/comps-foreman-plugins-rhel7.xml
@@ -89,7 +89,6 @@
       <packagereq type="default">tfm-rubygem-foreman_vmwareannotations</packagereq>
       <packagereq type="default">tfm-rubygem-foreman_webhooks</packagereq>
       <packagereq type="default">tfm-rubygem-foreman_wreckingball</packagereq>
-      <packagereq type="default">tfm-rubygem-foreman_xen</packagereq>
       <packagereq type="default">tfm-rubygem-git</packagereq>
       <packagereq type="default">tfm-rubygem-graphite-api</packagereq>
       <packagereq type="default">tfm-rubygem-hammer_cli_foreman_admin</packagereq>
@@ -251,7 +250,6 @@
       <packagereq type="default">tfm-rubygem-foreman_vmwareannotations-doc</packagereq>
       <packagereq type="default">tfm-rubygem-foreman_webhooks-doc</packagereq>
       <packagereq type="default">tfm-rubygem-foreman_wreckingball-doc</packagereq>
-      <packagereq type="default">tfm-rubygem-foreman_xen-doc</packagereq>
       <packagereq type="default">tfm-rubygem-graphite-api-doc</packagereq>
       <packagereq type="default">tfm-rubygem-hammer_cli_foreman_admin-doc</packagereq>
       <packagereq type="default">tfm-rubygem-hammer_cli_foreman_ansible-doc</packagereq>

packages/plugins/rubygem-foreman_xen/foreman_xen-1.0.1.gem
@@ -1,1 +1,0 @@
-../../../.git/annex/objects/GZ/65/SHA256E-s31744--dfda922880ebe3846ccbe62cda90e0c55f2c84c63f36b316733c2b8592c7f417.1.gem/SHA256E-s31744--dfda922880ebe3846ccbe62cda90e0c55f2c84c63f36b316733c2b8592c7f417.1.gem

packages/plugins/rubygem-foreman_xen/rubygem-foreman_xen.spec
@@ -1,201 +1,0 @@
-# template: foreman_plugin
-%{?scl:%scl_package rubygem-%{gem_name}}
-%{!?scl:%global pkg_name %{name}}
-
-%global gem_name foreman_xen
-%global plugin_name xen
-%global foreman_min_version 1.20.0
-
-Summary:    Provision and manage XEN Server from Foreman
-Name:       %{?scl_prefix}rubygem-%{gem_name}
-Version:    1.0.1
-Release:    2%{?foremandist}%{?dist}
-Group:      Applications/Systems
-License:    GPLv3
-URL:        https://github.com/theforeman/foreman-xen
-Source0:    https://rubygems.org/gems/%{gem_name}-%{version}.gem
-
-# start specfile generated dependencies
-Requires: foreman >= %{foreman_min_version}
-Requires: %{?scl_prefix_ruby}ruby(release)
-Requires: %{?scl_prefix_ruby}ruby
-Requires: %{?scl_prefix_ruby}ruby(rubygems)
-Requires: %{?scl_prefix}rubygem(fog-xenserver) >= 1.0
-Requires: %{?scl_prefix}rubygem(fog-xenserver) < 2
-BuildRequires: foreman-assets >= %{foreman_min_version}
-BuildRequires: foreman-plugin >= %{foreman_min_version}
-BuildRequires: %{?scl_prefix}rubygem(fog-xenserver) >= 1.0
-BuildRequires: %{?scl_prefix}rubygem(fog-xenserver) < 2
-BuildRequires: %{?scl_prefix_ruby}ruby(release)
-BuildRequires: %{?scl_prefix_ruby}ruby
-BuildRequires: %{?scl_prefix_ruby}rubygems-devel
-BuildArch: noarch
-Provides: %{?scl_prefix}rubygem(%{gem_name}) = %{version}
-Provides: foreman-plugin-%{plugin_name} = %{version}
-Provides: foreman-%{plugin_name} = %{version}
-# end specfile generated dependencies
-%{?scl:Obsoletes: ruby193-rubygem-%{gem_name}}
-
-%description
-Provision and manage XEN Server from Foreman.
-
-
-%package doc
-BuildArch:  noarch
-Group:      Documentation
-Requires:   %{?scl_prefix}%{pkg_name} = %{version}-%{release}
-%{?scl:Obsoletes: ruby193-rubygem-%{gem_name}-doc}
-Summary:    Documentation for %{pkg_name}
-
-%description doc
-Documentation for %{pkg_name}.
-
-%prep
-%{?scl:scl enable %{scl} - << \EOF}
-gem unpack %{SOURCE0}
-%{?scl:EOF}
-
-%setup -q -D -T -n  %{gem_name}-%{version}
-
-%{?scl:scl enable %{scl} - << \EOF}
-gem spec %{SOURCE0} -l --ruby > %{gem_name}.gemspec
-%{?scl:EOF}
-
-%build
-# Create the gem as gem install only works on a gem file
-%{?scl:scl enable %{scl} - << \EOF}
-gem build %{gem_name}.gemspec
-%{?scl:EOF}
-
-# %%gem_install compiles any C extensions and installs the gem into ./%%gem_dir
-# by default, so that we can move it into the buildroot in %%install
-%{?scl:scl enable %{scl} - << \EOF}
-%gem_install
-%{?scl:EOF}
-
-%install
-mkdir -p %{buildroot}%{gem_dir}
-cp -pa .%{gem_dir}/* \
-        %{buildroot}%{gem_dir}/
-
-%foreman_bundlerd_file
-%foreman_precompile_plugin -s
-
-%files
-%dir %{gem_instdir}
-%license %{gem_instdir}/LICENSE
-%{gem_instdir}/app
-%{gem_instdir}/config
-%{gem_libdir}
-%{gem_instdir}/locale
-%exclude %{gem_cache}
-%{gem_spec}
-%{foreman_bundlerd_plugin}
-%{foreman_assets_plugin}
-
-%files doc
-%doc %{gem_docdir}
-%doc %{gem_instdir}/README.md
-%{gem_instdir}/Rakefile
-%{gem_instdir}/test
-
-%changelog
-* Tue Apr 06 2021 Eric D. Helms <ericdhelms@gmail.com> - 1.0.1-2
-- Rebuild plugins for Ruby 2.7
-
-* Sun Feb 02 2020 Michael Moll <mmoll@mmoll.at> 1.0.1-1
-- Update to 1.0.1
-
-* Fri Jan 17 2020 Eric D. Helms <ericdhelms@gmail.com> - 1.0.0-2
-- Drop posttrans macros
-
-* Sat Jul 06 2019 Michael Moll <mmoll@mmoll.at> 1.0.0-1
-- Update to 1.0.0
-
-* Fri Jan 18 2019 Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl> 0.7.1-1
-- Update to 0.7.1
-
-* Fri Sep 07 2018 Eric D. Helms <ericdhelms@gmail.com> - 0.6.1-3
-- Rebuild for Rails 5.2 and Ruby 2.5
-
-* Mon May 28 2018 Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl> - 0.6.1-2
-- Regenerate spec file based on the current template
-
-* Sat May 05 2018 Michael Moll <kvedulv@kvedulv.de> 0.6.1-1
-- update foreman_xen to 0.6.1 (kvedulv@kvedulv.de)
-
-* Wed Jan 10 2018 Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl> 0.5.2-2
-- Bump Foreman plugins release (ericdhelms@gmail.com)
-- Use HTTPS URLs for github and rubygems (ewoud@kohlvanwijngaarden.nl)
-- Set proper download URLs for rubygems (komidore64@gmail.com)
-
-* Wed May 24 2017 Dominic Cleal <dominic@cleal.org> 0.5.2-1
-- update foreman_xen to 0.5.2 (kvedulv@kvedulv.de)
-
-* Fri Mar 03 2017 Dominic Cleal <dominic@cleal.org> 0.5.1-1
-- update foreman_xen to 0.5.1 (kvedulv@kvedulv.de)
-- Switch to using gem_install macro (ericdhelms@gmail.com)
-
-* Tue Jan 03 2017 Dominic Cleal <dominic@cleal.org> 0.4.1-1
-- update foreman_xen to 0.4.1 (kvedulv@kvedulv.de)
-
-* Wed Nov 02 2016 Dominic Cleal <dominic@cleal.org> 0.4.0-1
-- update foreman_xen to 0.4.0 (kvedulv@kvedulv.de)
-
-* Wed Aug 31 2016 Dominic Cleal <dominic@cleal.org> 0.3.1-1
-- update foreman_xen to 0.3.1 (kvedulv@kvedulv.de)
-
-* Mon Feb 29 2016 Dominic Cleal <dominic@cleal.org> 0.3.0-1
-- update foreman_xen to 0.3.0 (kvedulv@kvedulv.de)
-
-* Mon Jan 18 2016 Dominic Cleal <dcleal@redhat.com> 0.2.4-1
-- update foreman_xen to 0.2.4 (kvedulv@kvedulv.de)
-
-* Thu Dec 24 2015 Dominic Cleal <dcleal@redhat.com> 0.2.3-2
-- Replace ruby(abi) for ruby22 rebuild (dcleal@redhat.com)
-
-* Wed Nov 18 2015 Dominic Cleal <dcleal@redhat.com> 0.2.3-1
-- update foreman_xen to 0.2.3 (kvedulv@kvedulv.de)
-
-* Tue Nov 10 2015 Dominic Cleal <dcleal@redhat.com> 0.2.2-1
-- update foreman_xen to 0.2.2 (kvedulv@kvedulv.de)
-- Add foremandist macro (dcleal@redhat.com)
-
-* Tue Oct 20 2015 Dominic Cleal <dcleal@redhat.com> 0.2.1-1
-- update foreman_xen to 0.2.1 (kvedulv@kvedulv.de)
-
-* Mon Sep 21 2015 Dominic Cleal <dcleal@redhat.com> 0.1.3-1
-- update foreman_xen to 0.1.3 (kvedulv@kvedulv.de)
-
-* Wed Aug 26 2015 Dominic Cleal <dcleal@redhat.com> 0.1.2-2
-- Converted to tfm SCL (dcleal@redhat.com)
-
-* Fri Jul 31 2015 Dominic Cleal <dcleal@redhat.com> 0.1.2-1
-- update foreman_xen to 0.1.2 (kvedulv@kvedulv.de)
-
-* Wed May 27 2015 Dominic Cleal <dcleal@redhat.com> 0.1.1-1
-- update foreman_xen to 0.1.1 (kvedulv@kvedulv.de)
-
-* Wed May 20 2015 Dominic Cleal <dcleal@redhat.com> 0.1.0-1
-- update foreman_xen to 0.1.0 (kvedulv@kvedulv.de)
-
-* Wed May 13 2015 Dominic Cleal <dcleal@redhat.com> 0.0.6-1
-- update foreman_xen to 0.0.6 (kvedulv@kvedulv.de)
-
-* Fri Jan 02 2015 Dominic Cleal <dcleal@redhat.com> 0.0.5.1-1
-- Update foreman_xen to 0.0.5.1 (dcleal@redhat.com)
-
-* Thu Dec 18 2014 Dominic Cleal <dcleal@redhat.com> 0.0.5-1
-- Update foreman_xen to 0.0.5 (dcleal@redhat.com)
-
-* Fri Nov 21 2014 Dominic Cleal <dcleal@redhat.com> 0.0.4.1-1
-- Update to 0.0.4.1 (dcleal@redhat.com)
-
-* Wed Jul 02 2014 Dominic Cleal <dcleal@redhat.com> 0.0.3-1
-- Update to 0.0.3 (dcleal@redhat.com)
-
-* Mon Jun 16 2014 Dominic Cleal <dcleal@redhat.com> 0.0.2-1
-- Update to 0.0.2 (dcleal@redhat.com)
-
-* Wed May 21 2014 Dominic Cleal <dcleal@redhat.com> 0.0.1-1
-- new package built with tito

commit cf40d02224575a0bc54eaefbf4e2f9eb0a2f777d

Remove unused package rubygem-fog-xenserver
Fixes: 04ab0aa1b34f6ae18daee489b4b6119b79ec1fc3

comps/comps-foreman-el8.xml
@@ -190,7 +190,6 @@
       <packagereq type="default">rubygem-fog-openstack</packagereq>
       <packagereq type="default">rubygem-fog-ovirt</packagereq>
       <packagereq type="default">rubygem-fog-vsphere</packagereq>
-      <packagereq type="default">rubygem-fog-xenserver</packagereq>
       <packagereq type="default">rubygem-fog-xml</packagereq>
       <packagereq type="default">rubygem-font-awesome-sass</packagereq>
       <packagereq type="default">rubygem-foreman_maintain</packagereq>
@@ -412,7 +411,6 @@
       <packagereq type="default">rubygem-fog-openstack-doc</packagereq>
       <packagereq type="default">rubygem-fog-ovirt-doc</packagereq>
       <packagereq type="default">rubygem-fog-vsphere-doc</packagereq>
-      <packagereq type="default">rubygem-fog-xenserver-doc</packagereq>
       <packagereq type="default">rubygem-fog-xml-doc</packagereq>
       <packagereq type="default">rubygem-font-awesome-sass-doc</packagereq>
       <packagereq type="default">rubygem-foreman_maintain-doc</packagereq>

package_manifest.yaml
@@ -168,7 +168,6 @@ foreman_core_packages:
     rubygem-fog-openstack: {}
     rubygem-fog-ovirt: {}
     rubygem-fog-vsphere: {}
-    rubygem-fog-xenserver: {}
     rubygem-fog-xml: {}
     rubygem-font-awesome-sass: {}
     rubygem-foreman_maintain: {}

packages/foreman/rubygem-fog-xenserver/fog-xenserver-1.0.0.gem
@@ -1,1 +1,0 @@
-../../../.git/annex/objects/V0/q1/SHA256E-s113664--1fee6d26500fcaf89b1220008d440807acc9d4804b7a60b5f2b74cc820505855.0.gem/SHA256E-s113664--1fee6d26500fcaf89b1220008d440807acc9d4804b7a60b5f2b74cc820505855.0.gem

packages/foreman/rubygem-fog-xenserver/rubygem-fog-xenserver.spec
@@ -1,117 +1,0 @@
-# Generated from fog-xenserver-1.0.0.gem by gem2rpm -*- rpm-spec -*-
-# template: scl
-%{?scl:%scl_package rubygem-%{gem_name}}
-%{!?scl:%global pkg_name %{name}}
-
-%global gem_name fog-xenserver
-
-Name: %{?scl_prefix}rubygem-%{gem_name}
-Version: 1.0.0
-Release: 3%{?dist}
-Summary: Module for the 'fog' gem to support XENSERVER
-Group: Development/Languages
-License: MIT
-URL: https://github.com/fog/fog-xenserver
-Source0: https://rubygems.org/gems/%{gem_name}-%{version}.gem
-
-# start specfile generated dependencies
-Requires: %{?scl_prefix_ruby}ruby(release)
-Requires: %{?scl_prefix_ruby}ruby >= 2.0
-Requires: %{?scl_prefix_ruby}ruby(rubygems)
-Requires: %{?scl_prefix}rubygem(fog-xml)
-Requires: %{?scl_prefix}rubygem(fog-core)
-Requires: %{?scl_prefix}rubygem(xmlrpc)
-BuildRequires: %{?scl_prefix_ruby}ruby(release)
-BuildRequires: %{?scl_prefix_ruby}ruby >= 2.0
-BuildRequires: %{?scl_prefix_ruby}rubygems-devel
-BuildArch: noarch
-Provides: %{?scl_prefix}rubygem(%{gem_name}) = %{version}
-# end specfile generated dependencies
-
-%description
-Module for the 'fog' gem to support XENSERVER.
-
-
-%package doc
-Summary: Documentation for %{pkg_name}
-Requires: %{?scl_prefix}%{pkg_name} = %{version}-%{release}
-%{?scl:Obsoletes: ruby193-rubygem-%{gem_name}-doc}
-BuildArch: noarch
-
-%description doc
-Documentation for %{pkg_name}.
-
-%prep
-%{?scl:scl enable %{scl} - << \EOF}
-gem unpack %{SOURCE0}
-%{?scl:EOF}
-
-%setup -q -D -T -n  %{gem_name}-%{version}
-
-%{?scl:scl enable %{scl} - << \EOF}
-gem spec %{SOURCE0} -l --ruby > %{gem_name}.gemspec
-%{?scl:EOF}
-
-%build
-# Create the gem as gem install only works on a gem file
-%{?scl:scl enable %{scl} - << \EOF}
-gem build %{gem_name}.gemspec
-%{?scl:EOF}
-
-# %%gem_install compiles any C extensions and installs the gem into ./%%gem_dir
-# by default, so that we can move it into the buildroot in %%install
-%{?scl:scl enable %{scl} - << \EOF}
-%gem_install
-%{?scl:EOF}
-
-%install
-mkdir -p %{buildroot}%{gem_dir}
-cp -pa .%{gem_dir}/* \
-        %{buildroot}%{gem_dir}/
-
-%files
-%dir %{gem_instdir}
-%{gem_instdir}/gemfiles
-%{gem_libdir}
-%exclude %{gem_cache}
-%{gem_spec}
-%license %{gem_instdir}/LICENSE.md
-%exclude %{gem_instdir}/.*
-
-%files doc
-%doc %{gem_docdir}
-%doc %{gem_instdir}/CONTRIBUTING.md
-%doc %{gem_instdir}/CONTRIBUTORS.md
-%{gem_instdir}/Gemfile
-%doc %{gem_instdir}/README.md
-%{gem_instdir}/Rakefile
-%{gem_instdir}/fog-xenserver.gemspec
-%{gem_instdir}/spec
-
-%changelog
-* Thu Mar 11 2021 Eric D. Helms <ericdhelms@gmail.com> - 1.0.0-3
-- Rebuild against rh-ruby27
-
-* Wed Apr 08 2020 Zach Huntington-Meath <zhunting@redhat.com> - 1.0.0-2
-- Bump to release for EL8
-
-* Thu Dec 06 2018 Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl> 1.0.0-1
-- Update to 1.0.0-1
-
-* Wed Sep 05 2018 Eric D. Helms <ericdhelms@gmail.com> - 0.2.3-3
-- Rebuild for Rails 5.2 and Ruby 2.5
-
-* Fri Jan 05 2018 Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl> 0.2.3-2
-- More rebuilds for Rails 5.1 (ericdhelms@gmail.com)
-- Use HTTPS URLs for github and rubygems (ewoud@kohlvanwijngaarden.nl)
-- Switch to using gem_install macro (ericdhelms@gmail.com)
-
-* Mon Feb 29 2016 Dominic Cleal <dominic@cleal.org> 0.2.3-1
-- update fog-xenserver to 0.2.3 (kvedulv@kvedulv.de)
-
-* Tue Dec 22 2015 Dominic Cleal <dcleal@redhat.com> 0.2.2-2
-- Replace ruby(abi) for ruby22 rebuild (dcleal@redhat.com)
-
-* Fri Nov 20 2015 Dominic Cleal <dcleal@redhat.com> 0.2.2-1
-- new package built with tito
-

rel-eng/tito.props
@@ -175,7 +175,6 @@ whitelist = dynflow-utils
   rubygem-fog-openstack
   rubygem-fog-ovirt
   rubygem-fog-vsphere
-  rubygem-fog-xenserver
   rubygem-fog-xml
   rubygem-font-awesome-sass
   rubygem-foreman_maintain