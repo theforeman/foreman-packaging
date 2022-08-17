theforeman/foreman-packaging

commit b5f52b0471b639b910efbeeae28faa1b7582db91

Remove azure dependencies

package_manifest.yaml
@@ -415,8 +415,6 @@ plugins_packages:
     nodejs-react-json-tree: {}
     puppet-foreman_scap_client: {}
     rubygem-angular-rails-templates: {}
-    rubygem-azure-core: {}
-    rubygem-azure: {}
     rubygem-bastion: {}
     rubygem-bootstrap-datepicker-rails: {}
     rubygem-chef-api: {}
@@ -428,7 +426,6 @@ plugins_packages:
     rubygem-docker-api: {}
     rubygem-faraday_middleware: {}
     rubygem-ffi: {}
-    rubygem-fog-azure: {}
     rubygem-foreman-tasks-core: {}
     rubygem-foreman-tasks: {}
     rubygem-foreman_ansible: {}

rel-eng/tito.props
@@ -280,8 +280,6 @@ scl = tfm
 disttag = .el7
 scl = tfm
 whitelist = rubygem-angular-rails-templates
-  rubygem-azure
-  rubygem-azure-core
   rubygem-bastion
   rubygem-bootstrap-datepicker-rails
   rubygem-chunky_png
@@ -293,7 +291,6 @@ whitelist = rubygem-angular-rails-templates
   rubygem-docker-api
   rubygem-faraday_middleware
   rubygem-ffi
-  rubygem-fog-azure
   rubygem-foreman_ansible
   rubygem-foreman_ansible_core
   rubygem-foreman_bootdisk

comps/comps-foreman-plugins-rhel7.xml
@@ -47,8 +47,6 @@
       <packagereq type="default">rubygem-smart_proxy_salt</packagereq>
       <packagereq type="default">rubygem-smart_proxy_spacewalk</packagereq>
       <packagereq type="default">tfm-rubygem-angular-rails-templates</packagereq>
-      <packagereq type="default">tfm-rubygem-azure</packagereq>
-      <packagereq type="default">tfm-rubygem-azure-core</packagereq>
       <packagereq type="default">tfm-rubygem-bastion</packagereq>
       <packagereq type="default">tfm-rubygem-bastion-devel</packagereq>
       <packagereq type="default">tfm-rubygem-bootstrap-datepicker-rails</packagereq>
@@ -60,7 +58,6 @@
       <packagereq type="default">tfm-rubygem-docker-api</packagereq>
       <packagereq type="default">tfm-rubygem-faraday_middleware</packagereq>
       <packagereq type="default">tfm-rubygem-ffi</packagereq>
-      <packagereq type="default">tfm-rubygem-fog-azure</packagereq>
       <packagereq type="default">tfm-rubygem-foreman-tasks</packagereq>
       <packagereq type="default">tfm-rubygem-foreman-tasks-core</packagereq>
       <packagereq type="default">tfm-rubygem-foreman_ansible</packagereq>
@@ -171,8 +168,6 @@
       <packagereq type="default">rubygem-smart_proxy_salt-doc</packagereq>
       <packagereq type="default">rubygem-smart_proxy_spacewalk-doc</packagereq>
       <packagereq type="default">tfm-rubygem-angular-rails-templates-doc</packagereq>
-      <packagereq type="default">tfm-rubygem-azure-core-doc</packagereq>
-      <packagereq type="default">tfm-rubygem-azure-doc</packagereq>
       <packagereq type="default">tfm-rubygem-bastion-doc</packagereq>
       <packagereq type="default">tfm-rubygem-bootstrap-datepicker-rails-doc</packagereq>
       <packagereq type="default">tfm-rubygem-chunky_png-doc</packagereq>
@@ -182,7 +177,6 @@
       <packagereq type="default">tfm-rubygem-diffy-doc</packagereq>
       <packagereq type="default">tfm-rubygem-docker-api-doc</packagereq>
       <packagereq type="default">tfm-rubygem-faraday_middleware-doc</packagereq>
-      <packagereq type="default">tfm-rubygem-fog-azure-doc</packagereq>
       <packagereq type="default">tfm-rubygem-foreman_ansible_core-doc</packagereq>
       <packagereq type="default">tfm-rubygem-foreman_ansible-doc</packagereq>
       <packagereq type="default">tfm-rubygem-foreman_bootdisk-doc</packagereq>

packages/foreman/tfm/tfm.spec
@@ -19,7 +19,7 @@
 Summary: Package that installs %scl
 Name: %scl_name
 Version: 4.0
-Release: 2%{?dist}
+Release: 3%{?dist}
 License: GPLv2+
 Group: Applications/File
 Source0: README
@@ -64,11 +64,14 @@ Obsoletes: %{scl_prefix}rubygem-ace-rails-ap < 4.1.1-2
 Obsoletes: %{scl_prefix}rubygem-ansi < 1.4.3-7
 Obsoletes: %{scl_prefix}rubygem-archive-tar-minitar < 0.5.2-12
 Obsoletes: %{scl_prefix}rubygem-audited-activerecord < 4.2.0-3
+Obsoletes: %{scl_prefix}rubygem-azure < 0.7.10-2
+Obsoletes: %{scl_prefix}rubygem-azure-core < 0.1.1-3
 Obsoletes: %{scl_prefix}rubygem-colorize < 0.7.7-5
 Obsoletes: %{scl_prefix}rubygem-concurrent-ruby < 1.0.1-2
 Obsoletes: %{scl_prefix}rubygem-dalli < 2.6.4-4
 Obsoletes: %{scl_prefix}rubygem-ejs < 1.1.1-4
 Obsoletes: %{scl_prefix}rubygem-flot-rails < 0.0.3-8
+Obsoletes: %{scl_prefix}rubygem-fog-azure < 0.0.8-9
 Obsoletes: %{scl_prefix}rubygem-foreigner < 1.7.1-4
 Obsoletes: %{scl_prefix}rubygem-foreman_abrt < 0.0.7-3
 Obsoletes: %{scl_prefix}rubygem-foreman_azure < 1.3.1-2
@@ -295,6 +298,10 @@ selinuxenabled && load_policy || :
 %{_root_sysconfdir}/rpm/macros.%{scl_name}-scldevel
 
 %changelog
+* Tue Jul 17 2018 Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl> - 4.0-3
+- Remove foreman_abrt + smart_proxy_abrt
+- Remove foreman_azure + dependencies
+
 * Wed May 30 2018 Eric D. Helms <ericdhelms@gmail.com> - 4.0-2
 - rebuilt
 

packages/plugins/rubygem-azure-core/azure-core-0.1.1.gem
@@ -1,1 +1,0 @@
-../../../.git/annex/objects/pZ/mx/SHA256E-s16384--924f0cd11c822e339bba763cdb78c62620e8f3ecc0f2cd7f450ec64ec8dcb6f6.1.gem/SHA256E-s16384--924f0cd11c822e339bba763cdb78c62620e8f3ecc0f2cd7f450ec64ec8dcb6f6.1.gem

packages/plugins/rubygem-azure-core/rubygem-azure-core.spec
@@ -1,92 +1,0 @@
-%{?scl:%scl_package rubygem-%{gem_name}}
-%{!?scl:%global pkg_name %{name}}
-
-# Generated from azure-core-0.1.1.gem by gem2rpm -*- rpm-spec -*-
-%global gem_name azure-core
-
-Name: %{?scl_prefix}rubygem-%{gem_name}
-Version: 0.1.1
-Release: 2%{?dist}
-Summary: Core library to be consumed by Ruby SDK gems
-Group: Development/Languages
-License: ASL 2.0
-URL: https://github.com/Azure/azure-ruby-asm-core
-Source0: https://rubygems.org/gems/%{gem_name}-%{version}.gem
-Requires: %{?scl_prefix_ruby}ruby(release)
-Requires: %{?scl_prefix_ruby}ruby(rubygems)
-Requires: %{?scl_prefix_ruby}ruby
-Requires: %{?scl_prefix}rubygem(faraday) >= 0.9
-Requires: %{?scl_prefix}rubygem(faraday) < 1.0
-Requires: %{?scl_prefix}rubygem(faraday_middleware) >= 0.10
-Requires: %{?scl_prefix}rubygem(faraday_middleware) < 1.0
-Requires: %{?scl_prefix_ror}rubygem(nokogiri) >= 1.6
-Requires: %{?scl_prefix_ror}rubygem(nokogiri) < 2.0
-BuildRequires: %{?scl_prefix_ruby}ruby(release)
-BuildRequires: %{?scl_prefix_ruby}rubygems-devel
-BuildRequires: %{?scl_prefix_ruby}ruby >= 1.9.3
-BuildArch: noarch
-Provides: %{?scl_prefix}rubygem(%{gem_name}) = %{version}
-
-%description
-Microsoft Azure Client Core Library for Ruby SDK.
-
-%package doc
-Summary: Documentation for %{pkg_name}
-Group: Documentation
-Requires: %{?scl_prefix}%{pkg_name} = %{version}-%{release}
-BuildArch: noarch
-
-%description doc
-Documentation for %{pkg_name}.
-
-%prep
-%{?scl:scl enable %{scl} - << \EOF}
-gem unpack %{SOURCE0}
-%{?scl:EOF}
-
-%setup -q -D -T -n  %{gem_name}-%{version}
-
-%{?scl:scl enable %{scl} - << \EOF}
-gem spec %{SOURCE0} -l --ruby > %{gem_name}.gemspec
-%{?scl:EOF}
-
-%build
-# Create the gem as gem install only works on a gem file
-%{?scl:scl enable %{scl} - << \EOF}
-gem build %{gem_name}.gemspec
-%{?scl:EOF}
-
-%{?scl:scl enable %{scl} - << \EOF}
-%gem_install
-%{?scl:EOF}
-
-%install
-mkdir -p %{buildroot}%{gem_dir}
-cp -a .%{gem_dir}/* \
-        %{buildroot}%{gem_dir}/
-
-
-%files
-%dir %{gem_instdir}
-%exclude %{gem_instdir}/.gitignore
-%exclude %{gem_instdir}/.travis.yml
-%{gem_libdir}
-%exclude %{gem_cache}
-%{gem_spec}
-
-%files doc
-%doc %{gem_docdir}
-%doc %{gem_instdir}/ChangeLog.md
-%{gem_instdir}/Gemfile
-%doc %{gem_instdir}/README.md
-%{gem_instdir}/Rakefile
-%{gem_instdir}/azure-core.gemspec
-%{gem_instdir}/test
-
-%changelog
-* Tue Jan 09 2018 Eric D. Helms <ericdhelms@gmail.com> 0.1.1-2
-- Bump releases for base foreman plugins packages (ericdhelms@gmail.com)
-- Use HTTPS URLs for github and rubygems (ewoud@kohlvanwijngaarden.nl)
-
-* Fri Jun 10 2016 Daniel Lobato - 0.1.1-1
-- Initial package

packages/plugins/rubygem-azure/azure-0.7.10.gem
@@ -1,1 +1,0 @@
-../../../.git/annex/objects/fJ/89/SHA256E-s96768--73562416ceb367dd92eb0f3122b76552e86c080ce0bebad4b562c3cb2c48bea7.10.gem/SHA256E-s96768--73562416ceb367dd92eb0f3122b76552e86c080ce0bebad4b562c3cb2c48bea7.10.gem

packages/plugins/rubygem-azure/rubygem-azure.spec
@@ -1,115 +1,0 @@
-%{?scl:%scl_package rubygem-%{gem_name}}
-%{!?scl:%global pkg_name %{name}}
-
-%global gem_name azure
-
-Name: %{?scl_prefix}rubygem-%{gem_name}
-Version: 0.7.10
-Release: 1%{?dist}
-Summary: Official ruby client library to consume Microsoft Azure services
-Group: Development/Languages
-License: ASL 2.0
-URL: https://github.com/azure/azure-sdk-for-ruby
-Source0: https://rubygems.org/gems/%{gem_name}-%{version}.gem
-Requires: %{?scl_prefix_ruby}ruby(release)
-Requires: %{?scl_prefix_ruby}ruby >= 1.9.3
-Requires: %{?scl_prefix_ruby}ruby(rubygems)
-Requires: %{?scl_prefix}rubygem(addressable) >= 2.3
-Requires: %{?scl_prefix}rubygem(addressable) < 3.0
-Requires: %{?scl_prefix}rubygem(azure-core) >= 0.1
-Requires: %{?scl_prefix}rubygem(azure-core) < 1.0
-Requires: %{?scl_prefix}rubygem(faraday) >= 0.9
-Requires: %{?scl_prefix}rubygem(faraday) < 1.0
-Requires: %{?scl_prefix}rubygem(faraday_middleware) >= 0.10
-Requires: %{?scl_prefix}rubygem(faraday_middleware) < 1.0
-Requires: %{?scl_prefix_ror}rubygem(mime-types) >= 1.0
-Requires: %{?scl_prefix_ror}rubygem(mime-types) < 4.0
-Requires: %{?scl_prefix_ror}rubygem(nokogiri) >= 1.6
-Requires: %{?scl_prefix_ror}rubygem(nokogiri) < 2.0
-Requires: %{?scl_prefix}rubygem(systemu) >= 2.6
-Requires: %{?scl_prefix}rubygem(systemu) < 3.0
-Requires: %{?scl_prefix_ror}rubygem(thor) >= 0.19
-Requires: %{?scl_prefix_ror}rubygem(thor) < 1.0
-BuildRequires: %{?scl_prefix_ruby}ruby(release)
-BuildRequires: %{?scl_prefix_ruby}ruby >= 1.9.3
-BuildRequires: %{?scl_prefix_ruby}rubygems-devel
-BuildArch: noarch
-Provides: %{?scl_prefix}rubygem(%{gem_name}) = %{version}
-
-%description
-Microsoft Azure Client Library for Ruby.
-
-%package doc
-Summary: Documentation for %{pkg_name}
-Group: Documentation
-Requires: %{?scl_prefix}%{pkg_name} = %{version}-%{release}
-BuildArch: noarch
-
-%description doc
-Documentation for %{pkg_name}.
-
-%prep
-%{?scl:scl enable %{scl} - << \EOF}
-gem unpack %{SOURCE0}
-%{?scl:EOF}
-
-%setup -q -D -T -n  %{gem_name}-%{version}
-
-%{?scl:scl enable %{scl} - << \EOF}
-gem spec %{SOURCE0} -l --ruby > %{gem_name}.gemspec
-%{?scl:EOF}
-
-%build
-# Create the gem as gem install only works on a gem file
-%{?scl:scl enable %{scl} - << \EOF}
-gem build %{gem_name}.gemspec
-%{?scl:EOF}
-
-# %%gem_install compiles any C extensions and installs the gem into ./%%gem_dir
-# by default, so that we can move it into the buildroot in %%install
-%{?scl:scl enable %{scl} - << \EOF}
-%gem_install
-%{?scl:EOF}
-
-%install
-mkdir -p %{buildroot}%{gem_dir}
-cp -pa .%{gem_dir}/* \
-        %{buildroot}%{gem_dir}/
-
-mkdir -p %{buildroot}%{_bindir}
-cp -pa .%{_bindir}/* \
-        %{buildroot}%{_bindir}/
-find %{buildroot}%{gem_instdir}/bin -type f | xargs chmod a+x
-
-%files
-%dir %{gem_instdir}
-%{_bindir}/pfxer
-%license %{gem_instdir}/LICENSE.txt
-%{gem_instdir}/bin
-%{gem_libdir}
-%exclude %{gem_cache}
-%{gem_spec}
-
-%exclude %{gem_instdir}/.*
-
-%files doc
-%doc %{gem_docdir}
-%doc %{gem_instdir}/ChangeLog.md
-%doc %{gem_instdir}/README.md
-%{gem_instdir}/Gemfile
-%{gem_instdir}/Rakefile
-%{gem_instdir}/azure.gemspec
-
-%changelog
-* Fri Jan 12 2018 Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl> 0.7.10-1
-- Bump rubygem-azure to 0.7.10 (ewoud@kohlvanwijngaarden.nl)
-
-* Tue Jan 09 2018 Eric D. Helms <ericdhelms@gmail.com> 0.7.7-2
-- Bump releases for base foreman plugins packages (ericdhelms@gmail.com)
-- Use HTTPS URLs for github and rubygems (ewoud@kohlvanwijngaarden.nl)
-
-* Tue Nov 15 2016 Dominic Cleal <dominic@cleal.org> 0.7.7-1
-- Update azure to 0.7.7 (dominic@cleal.org)
-
-* Fri May 13 2016 Daniel Lobato - 0.7.5-1
-- Initial package

commit a8c7c93073ee3881a22b0026ca84db5c604ea62a

Remove unused package rubygem-systemu
Fixes: b5f52b0471b639b910efbeeae28faa1b7582db91

comps/comps-foreman-plugins-el8.xml
@@ -184,7 +184,6 @@
       <packagereq type="default">rubygem-smart_proxy_salt</packagereq>
       <packagereq type="default">rubygem-smart_proxy_shellhooks</packagereq>
       <packagereq type="default">rubygem-smart_proxy_spacewalk</packagereq>
-      <packagereq type="default">rubygem-systemu</packagereq>
       <packagereq type="default">rubygem-timeliness</packagereq>
       <packagereq type="default">rubygem-ttfunk</packagereq>
       <packagereq type="default">rubygem-vault</packagereq>
@@ -357,7 +356,6 @@
       <packagereq type="default">rubygem-smart_proxy_salt-doc</packagereq>
       <packagereq type="default">rubygem-smart_proxy_shellhooks-doc</packagereq>
       <packagereq type="default">rubygem-smart_proxy_spacewalk-doc</packagereq>
-      <packagereq type="default">rubygem-systemu-doc</packagereq>
       <packagereq type="default">rubygem-timeliness-doc</packagereq>
       <packagereq type="default">rubygem-ttfunk-doc</packagereq>
       <packagereq type="default">rubygem-vault-doc</packagereq>

package_manifest.yaml
@@ -771,7 +771,6 @@ foreman_plugin_packages:
     rubygem-sentry-raven: {}
     rubygem-smart_proxy_container_gateway: {}
     rubygem-smart_proxy_acd: {}
-    rubygem-systemu: {}
     rubygem-timeliness: {}
     rubygem-ttfunk: {}
     rubygem-vault: {}

packages/plugins/rubygem-systemu/rubygem-systemu.spec
@@ -1,92 +1,0 @@
-%{?scl:%scl_package rubygem-%{gem_name}}
-%{!?scl:%global pkg_name %{name}}
-
-# Generated from systemu-2.6.5.gem by gem2rpm -*- rpm-spec -*-
-%global gem_name systemu
-
-Name: %{?scl_prefix}rubygem-%{gem_name}
-Version: 2.6.5
-Release: 4%{?dist}
-Summary: systemu
-Group: Development/Languages
-License: Ruby
-URL: https://github.com/ahoward/systemu
-Source0: https://rubygems.org/gems/%{gem_name}-%{version}.gem
-Requires: %{?scl_prefix_ruby}ruby(release)
-Requires: %{?scl_prefix_ruby}rubygems
-Requires: %{?scl_prefix_ruby}ruby
-BuildRequires: %{?scl_prefix_ruby}ruby(release)
-BuildRequires: %{?scl_prefix_ruby}rubygems-devel
-BuildRequires: %{?scl_prefix_ruby}ruby
-BuildArch: noarch
-Provides: %{?scl_prefix}rubygem(%{gem_name}) = %{version}
-
-%description
-universal capture of stdout and stderr and handling of child process pid for
-windows, *nix, etc.
-
-%package doc
-Summary: Documentation for %{pkg_name}
-Group: Documentation
-Requires: %{?scl_prefix}%{pkg_name} = %{version}-%{release}
-BuildArch: noarch
-
-%description doc
-Documentation for %{pkg_name}.
-
-%prep
-%{?scl:scl enable %{scl} - << \EOF}
-gem unpack %{SOURCE0}
-%{?scl:EOF}
-
-%setup -q -D -T -n  %{gem_name}-%{version}
-
-%{?scl:scl enable %{scl} - << \EOF}
-gem spec %{SOURCE0} -l --ruby > %{gem_name}.gemspec
-%{?scl:EOF}
-
-%build
-# Create the gem as gem install only works on a gem file
-%{?scl:scl enable %{scl} - << \EOF}
-gem build %{gem_name}.gemspec
-%{?scl:EOF}
-
-%{?scl:scl enable %{scl} - << \EOF}
-%gem_install
-%{?scl:EOF}
-
-%install
-mkdir -p %{buildroot}%{gem_dir}
-cp -a .%{gem_dir}/* \
-        %{buildroot}%{gem_dir}/
-
-%files
-%dir %{gem_instdir}
-%doc %{gem_instdir}/BSDL
-%doc %{gem_instdir}/LICENSE
-%{gem_libdir}
-%exclude %{gem_cache}
-%{gem_spec}
-
-%files doc
-%doc %{gem_docdir}
-%doc %{gem_instdir}/README
-%doc %{gem_instdir}/README.erb
-%{gem_instdir}/Rakefile
-%{gem_instdir}/systemu.gemspec
-%{gem_instdir}/test
-%{gem_instdir}/samples
-
-%changelog
-* Tue Apr 06 2021 Eric D. Helms <ericdhelms@gmail.com> - 2.6.5-4
-- Rebuild for Ruby 2.7
-
-* Fri Sep 07 2018 Eric D. Helms <ericdhelms@gmail.com> - 2.6.5-3
-- Rebuild for Rails 5.2 and Ruby 2.5
-
-* Tue Jan 09 2018 Eric D. Helms <ericdhelms@gmail.com> 2.6.5-2
-- Bump releases for base foreman plugins packages (ericdhelms@gmail.com)
-- Use HTTPS URLs for github and rubygems (ewoud@kohlvanwijngaarden.nl)
-
-* Tue May 17 2016 Daniel Lobato - 2.6.5-1
-- Initial package

packages/plugins/rubygem-systemu/systemu-2.6.5.gem
@@ -1,1 +1,0 @@
-../../../.git/annex/objects/2f/Wp/SHA256E-s15872--01f7d014b1453b28e5781e15c4d7d63fc9221c29b174b7aae5253207a75ab33e.5.gem/SHA256E-s15872--01f7d014b1453b28e5781e15c4d7d63fc9221c29b174b7aae5253207a75ab33e.5.gem

rel-eng/tito.props
@@ -498,7 +498,6 @@ whitelist = ansible-collection-theforeman-foreman
   rubygem-smart_proxy_salt
   rubygem-smart_proxy_shellhooks
   rubygem-smart_proxy_spacewalk
-  rubygem-systemu
   rubygem-timeliness
   rubygem-ttfunk
   rubygem-vault