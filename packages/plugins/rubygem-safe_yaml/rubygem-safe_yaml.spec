theforeman/foreman-packaging

commit 824fe940f3ae990bbbf84c3d64ffa85cd63e5b12

Add safe_yaml package

rel-eng/tito.props
@@ -248,6 +248,7 @@ whitelist = rubygem-algebrick
   rubygem-net-ssh-multi
   rubygem-ovirt_provision_plugin
   rubygem-puppetdb_foreman
+  rubygem-safe_yaml
   rubygem-wicked
   rubygem-zscheduler
 
@@ -329,6 +330,7 @@ whitelist = rubygem-algebrick
   rubygem-net-ssh-multi
   rubygem-ovirt_provision_plugin
   rubygem-puppetdb_foreman
+  rubygem-safe_yaml
   rubygem-wicked
   rubygem-zscheduler
 

rubygem-safe_yaml/rubygem-safe_yaml.spec
@@ -1,0 +1,87 @@
+%{?scl:%scl_package rubygem-%{gem_name}}
+%{!?scl:%global pkg_name %{name}}
+
+%global gem_name safe_yaml
+
+Summary: Parse YAML safely
+Name: %{?scl_prefix}rubygem-%{gem_name}
+Version: 1.0.4
+Release: 1%{?dist}
+Group: Development/Languages
+License: MIT
+URL: https://github.com/dtao/safe_yaml
+Source0: http://rubygems.org/downloads/%{gem_name}-%{version}.gem
+
+Requires: %{?scl_prefix_ruby}ruby(rubygems)
+Requires: %{?scl_prefix_ruby}ruby
+
+%if "%{?scl_ruby}" == "ruby193" || (0%{?el6} && 0%{!?scl:1})
+Requires: %{?scl_prefix_ruby}ruby(abi)
+BuildRequires: %{?scl_prefix_ruby}ruby(abi)
+%else
+Requires: %{?scl_prefix_ruby}ruby(release)
+BuildRequires: %{?scl_prefix_ruby}ruby(release)
+%endif
+
+BuildRequires: %{?scl_prefix_ruby}rubygems-devel
+BuildRequires: %{?scl_prefix_ruby}ruby
+BuildArch: noarch
+Provides: %{?scl_prefix}rubygem(%{gem_name}) = %{version}
+
+%description
+The SafeYAML gem provides an alternative implementation of YAML.load
+suitable for accepting user input in Ruby applications
+
+%package doc
+Summary: Documentation for %{name}
+Group: Documentation
+Requires: %{name} = %{version}-%{release}
+BuildArch: noarch
+
+%description doc
+Documentation for %{name}.
+
+%prep
+%setup -q -c -T
+mkdir -p .%{gem_dir}
+
+%{?scl:scl enable %{scl} "}
+gem install --local --install-dir .%{gem_dir} \
+            --bindir .%{_bindir} \
+            --force %{SOURCE0} --no-ri --no-rdoc
+%{?scl:"}
+
+%install
+mkdir -p %{buildroot}%{gem_dir}
+cp -a .%{gem_dir}/* \
+        %{buildroot}%{gem_dir}/
+
+
+mkdir -p %{buildroot}%{_bindir}
+cp -a .%{_bindir}/* \
+        %{buildroot}%{_bindir}/
+
+find %{buildroot}%{gem_instdir}/bin -type f | xargs chmod a+x
+
+%files
+%dir %{gem_instdir}
+%{_bindir}/safe_yaml
+%{gem_instdir}/bin
+%{gem_libdir}
+%exclude %{gem_cache}
+%{gem_spec}
+%doc %{gem_instdir}/LICENSE.txt
+%exclude %{gem_instdir}/.*
+%exclude %{gem_instdir}/*.sh
+%exclude %{gem_instdir}/spec
+%{gem_instdir}/Rakefile
+%{gem_instdir}/Gemfile
+%{gem_instdir}/%{gem_name}.gemspec
+
+%files doc
+%doc %{gem_instdir}/README.md
+%doc %{gem_instdir}/CHANGES.md
+
+%changelog
+* Tue Nov 3 2015 Ondrej Prazak <oprazak@redhat.com> - 1.0.4-1
+- Initial package

rubygem-safe_yaml/safe_yaml-1.0.4.gem
@@ -1,0 +1,1 @@
+../.git/annex/objects/mj/6x/SHA256E-s30208--248193992ef1730a0c9ec579999ef2256a2b3a32a9bd9d708a1e12544a489ec2.4.gem/SHA256E-s30208--248193992ef1730a0c9ec579999ef2256a2b3a32a9bd9d708a1e12544a489ec2.4.gem

comps/comps-foreman-plugins-rhel6.xml
@@ -79,6 +79,7 @@
       <packagereq type="default">tfm-rubygem-net-ssh-multi</packagereq>
       <packagereq type="default">tfm-rubygem-openscap</packagereq>
       <packagereq type="default">tfm-rubygem-opennebula</packagereq>
+      <packagereq type="default">tfm-rubygem-safe_yaml</packagereq>
       <packagereq type="default">tfm-rubygem-wicked</packagereq>
       <packagereq type="default">tfm-rubygem-zscheduler</packagereq>
       <packagereq type="default">rubygem-satyr</packagereq>
@@ -152,6 +153,7 @@
       <packagereq type="default">tfm-rubygem-net-ssh-multi-doc</packagereq>
       <packagereq type="default">tfm-rubygem-opennebula-doc</packagereq>
       <packagereq type="default">tfm-rubygem-ovirt_provision_plugin-doc</packagereq>
+      <packagereq type="default">tfm-rubygem-safe_yaml-doc</packagereq>
       <packagereq type="default">tfm-rubygem-wicked-doc</packagereq>
       <packagereq type="default">tfm-rubygem-zscheduler-doc</packagereq>
       <!--RGD-END-->

comps/comps-foreman-plugins-rhel7.xml
@@ -82,6 +82,7 @@
       <packagereq type="default">tfm-rubygem-net-ssh-multi</packagereq>
       <packagereq type="default">tfm-rubygem-openscap</packagereq>
       <packagereq type="default">tfm-rubygem-opennebula</packagereq>
+      <packagereq type="default">tfm-rubygem-safe_yaml</packagereq>
       <packagereq type="default">tfm-rubygem-wicked</packagereq>
       <packagereq type="default">tfm-rubygem-zscheduler</packagereq>
       <packagereq type="default">rubygem-apipie-params</packagereq>
@@ -174,6 +175,7 @@
       <packagereq type="default">tfm-rubygem-net-ssh-multi-doc</packagereq>
       <packagereq type="default">tfm-rubygem-opennebula-doc</packagereq>
       <packagereq type="default">tfm-rubygem-ovirt_provision_plugin-doc</packagereq>
+      <packagereq type="default">tfm-rubygem-safe_yaml-doc</packagereq>
       <packagereq type="default">tfm-rubygem-wicked-doc</packagereq>
       <packagereq type="default">tfm-rubygem-zscheduler-doc</packagereq>
       <!--RGD-END-->

commit bfef16a6de9af9aba7a037c42da1ec3fd6f9068a

Remove unused package rubygem-unused-gems
This was added as a preparation for foreman_deployments but that never
made it into the repository.

Fixes: 824fe940f3ae990bbbf84c3d64ffa85cd63e5b12

comps/comps-foreman-plugins-el8.xml
@@ -155,7 +155,6 @@
       <packagereq type="default">rubygem-rqrcode</packagereq>
       <packagereq type="default">rubygem-ruby-hmac</packagereq>
       <packagereq type="default">rubygem-ruby-nmap</packagereq>
-      <packagereq type="default">rubygem-safe_yaml</packagereq>
       <packagereq type="default">rubygem-sentry-raven</packagereq>
       <packagereq type="default">rubygem-smart-proxy-probing</packagereq>
       <packagereq type="default">rubygem-smart_proxy_acd</packagereq>
@@ -329,7 +328,6 @@
       <packagereq type="default">rubygem-rqrcode-doc</packagereq>
       <packagereq type="default">rubygem-ruby-hmac-doc</packagereq>
       <packagereq type="default">rubygem-ruby-nmap-doc</packagereq>
-      <packagereq type="default">rubygem-safe_yaml-doc</packagereq>
       <packagereq type="default">rubygem-sentry-raven-doc</packagereq>
       <packagereq type="default">rubygem-smart_proxy_acd-doc</packagereq>
       <packagereq type="default">rubygem-smart_proxy_ansible-doc</packagereq>

package_manifest.yaml
@@ -768,7 +768,6 @@ foreman_plugin_packages:
     rubygem-rchardet: {}
     rubygem-recursive-open-struct: {}
     rubygem-rqrcode: {}
-    rubygem-safe_yaml: {}
     rubygem-sentry-raven: {}
     rubygem-smart_proxy_container_gateway: {}
     rubygem-smart_proxy_acd: {}

packages/plugins/rubygem-safe_yaml/rubygem-safe_yaml.spec
@@ -1,92 +1,0 @@
-%{?scl:%scl_package rubygem-%{gem_name}}
-%{!?scl:%global pkg_name %{name}}
-
-%global gem_name safe_yaml
-
-Summary: Parse YAML safely
-Name: %{?scl_prefix}rubygem-%{gem_name}
-Version: 1.0.4
-Release: 5%{?dist}
-Group: Development/Languages
-License: MIT
-URL: https://github.com/dtao/safe_yaml
-Source0: https://rubygems.org/gems/%{gem_name}-%{version}.gem
-
-Requires: %{?scl_prefix_ruby}ruby(release)
-Requires: %{?scl_prefix_ruby}ruby(rubygems)
-Requires: %{?scl_prefix_ruby}ruby
-BuildRequires: %{?scl_prefix_ruby}ruby(release)
-BuildRequires: %{?scl_prefix_ruby}rubygems-devel
-BuildRequires: %{?scl_prefix_ruby}ruby
-BuildArch: noarch
-Provides: %{?scl_prefix}rubygem(%{gem_name}) = %{version}
-
-%description
-The SafeYAML gem provides an alternative implementation of YAML.load
-suitable for accepting user input in Ruby applications
-
-%package doc
-Summary: Documentation for %{name}
-Group: Documentation
-Requires: %{name} = %{version}-%{release}
-BuildArch: noarch
-
-%description doc
-Documentation for %{name}.
-
-%prep
-%setup -q -c -T
-%{?scl:scl enable %{scl} - <<EOF}
-%gem_install -n %{SOURCE0}
-%{?scl:EOF}
-
-%install
-mkdir -p %{buildroot}%{gem_dir}
-cp -a .%{gem_dir}/* \
-        %{buildroot}%{gem_dir}/
-
-
-mkdir -p %{buildroot}%{_bindir}
-cp -a .%{_bindir}/* \
-        %{buildroot}%{_bindir}/
-
-find %{buildroot}%{gem_instdir}/bin -type f | xargs chmod a+x
-
-%files
-%dir %{gem_instdir}
-%{_bindir}/safe_yaml
-%{gem_instdir}/bin
-%{gem_libdir}
-%exclude %{gem_cache}
-%{gem_spec}
-%doc %{gem_instdir}/LICENSE.txt
-%exclude %{gem_instdir}/.*
-%exclude %{gem_instdir}/*.sh
-%exclude %{gem_instdir}/spec
-%{gem_instdir}/Rakefile
-%{gem_instdir}/Gemfile
-%{gem_instdir}/%{gem_name}.gemspec
-
-%files doc
-%doc %{gem_docdir}
-%doc %{gem_instdir}/README.md
-%doc %{gem_instdir}/CHANGES.md
-
-%changelog
-* Tue Apr 06 2021 Eric D. Helms <ericdhelms@gmail.com> - 1.0.4-5
-- Rebuild for Ruby 2.7
-
-* Fri Sep 07 2018 Eric D. Helms <ericdhelms@gmail.com> - 1.0.4-4
-- Rebuild for Rails 5.2 and Ruby 2.5
-
-* Tue Jan 09 2018 Eric D. Helms <ericdhelms@gmail.com> 1.0.4-3
-- Bump releases for base foreman plugins packages (ericdhelms@gmail.com)
-- Use HTTPS URLs for github and rubygems (ewoud@kohlvanwijngaarden.nl)
-- Set proper download URLs for rubygems (komidore64@gmail.com)
-- Switch to using gem_install macro (ericdhelms@gmail.com)
-
-* Thu Dec 24 2015 Dominic Cleal <dcleal@redhat.com> 1.0.4-2
-- Replace ruby(abi) for ruby22 rebuild (dcleal@redhat.com)
-
-* Tue Nov 3 2015 Ondrej Prazak <oprazak@redhat.com> - 1.0.4-1
-- Initial package

packages/plugins/rubygem-safe_yaml/safe_yaml-1.0.4.gem
@@ -1,1 +1,0 @@
-../../../.git/annex/objects/mj/6x/SHA256E-s30208--248193992ef1730a0c9ec579999ef2256a2b3a32a9bd9d708a1e12544a489ec2.4.gem/SHA256E-s30208--248193992ef1730a0c9ec579999ef2256a2b3a32a9bd9d708a1e12544a489ec2.4.gem

rel-eng/tito.props
@@ -469,7 +469,6 @@ whitelist = ansible-collection-theforeman-foreman
   rubygem-rqrcode
   rubygem-ruby-hmac
   rubygem-ruby-nmap
-  rubygem-safe_yaml
   rubygem-sentry-raven
   rubygem-smart_proxy_acd
   rubygem-smart_proxy_ansible