theforeman/foreman-packaging

commit fa53fd5eb76a52a15db4cd01a97f8ff6e8575462

Remove the use of tito
With the change to using Koji CLI directly for build and release,
the tito configuration is unnecessary to continue managing.

add_npm_package.sh
@@ -3,8 +3,8 @@
 NPM_MODULE_NAME=$1
 VERSION=${2:-auto}
 STRATEGY=$3
-TITO_TAG=foreman-nightly-el8
-DISTRO=${TITO_TAG##*-}
+KOJI_TAG=foreman-nightly-el8
+DISTRO=${KOJI_TAG##*-}
 BASE_DIR=${4:-foreman}
 
 REWRITE_ON_SAME_VERSION=${REWRITE_ON_SAME_VERSION:-true}
@@ -76,17 +76,6 @@ generate_npm_package() {
   echo -e "Annexing sources... - "
   find "$PACKAGE_DIR" -name '*.tgz' -exec git annex add {} +
   echo "FINISHED"
-}
-
-add_to_tito_props() {
-  # Get tito.props whitelists and add node package
-  original_locale=$LC_COLLATE
-  export LC_COLLATE=en_GB
-  local current_whitelist=$(crudini --get rel-eng/tito.props $TITO_TAG whitelist)
-  local whitelist=$(echo "$current_whitelist $PACKAGE_NAME" | tr " " "\n" | sort -u)
-  crudini --set rel-eng/tito.props $TITO_TAG whitelist "$whitelist"
-  export LC_COLLATE=$original_locale
-  git add rel-eng/tito.props
 }
 
 add_npm_to_comps() {
@@ -167,9 +156,6 @@ if [[ $UPDATE == true ]] ; then
   fi
 else
   generate_npm_package
-  echo -n "Setting tito props..."
-  add_to_tito_props
-  echo "FINISHED"
   echo -e "Updating comps... - "
   add_npm_to_comps
   echo "FINISHED"

add_pypi_package.sh
@@ -2,8 +2,8 @@
 
 PYPI_NAME=$1
 VERSION=${2:-auto}
-TITO_TAG=${3:-foreman-nightly-el8}
-DISTRO=${TITO_TAG##*-}
+KOJI_TAG=${3:-foreman-nightly-el8}
+DISTRO=${KOJI_TAG##*-}
 BASE_DIR=${4:-foreman}
 TEMPLATE=${5:-fedora}
 BASE_PYTHON=3
@@ -71,20 +71,9 @@ generate_pypi_package() {
   echo "FINISHED"
 }
 
-add_to_tito_props() {
-  # Get tito.props whitelists and add node package
-  original_locale=$LC_COLLATE
-  export LC_COLLATE=en_GB
-  local current_whitelist=$(crudini --get rel-eng/tito.props $TITO_TAG whitelist)
-  local whitelist=$(echo "$current_whitelist $PACKAGE_NAME" | tr " " "\n" | sort -u)
-  crudini --set rel-eng/tito.props $TITO_TAG whitelist "$whitelist"
-  export LC_COLLATE=$original_locale
-  git add rel-eng/tito.props
-}
-
 add_pypi_to_comps() {
   local comps_packages=$(rpmspec --query --builtrpms --queryformat '%{NAME}\n' $PACKAGE_DIR/*.spec)
-  if [[ $TITO_TAG == katello-* ]]; then
+  if [[ $KOJI_TAG == katello-* ]]; then
     local comps_file="katello-server"
   else
     local comps_file="foreman"
@@ -100,11 +89,11 @@ add_pypi_to_comps() {
 }
 
 add_pypi_to_manifest() {
-	if [[ $TITO_TAG == "foreman-nightly-el8" ]] ; then
+	if [[ $KOJI_TAG == "foreman-nightly-el8" ]] ; then
 		local section="foreman_core_packages"
-	elif [[ $TITO_TAG == "foreman-plugins-nightly-el8" ]] ; then
+	elif [[ $KOJI_TAG == "foreman-plugins-nightly-el8" ]] ; then
 		local section="foreman_plugin_packages"
-	elif [[ $TITO_TAG == "katello-nightly-el8" ]] ; then
+	elif [[ $KOJI_TAG == "katello-nightly-el8" ]] ; then
 		local section="katello_packages"
 	else
 		# TODO: client packages
@@ -130,7 +119,7 @@ pypi_info() {
 
 if [[ -z $PYPI_NAME ]]; then
   echo "This script adds a new python package based on the module found on pypi.org"
-  echo -e "\nUsage:\n$0 PYPI_NAME [VERSION [TITO_TAG [PACKAGE_SUBDIR [TEMPLATE]]] \n"
+  echo -e "\nUsage:\n$0 PYPI_NAME [VERSION [KOJI_TAG [PACKAGE_SUBDIR [TEMPLATE]]] \n"
   echo "VERSION is optional but can be an exact version number or auto to use the latest version"
   exit 1
 fi
@@ -168,9 +157,6 @@ if [[ $UPDATE == true ]] ; then
   fi
 else
   generate_pypi_package
-  echo -n "Setting tito props..."
-  add_to_tito_props
-  echo "FINISHED"
   echo -e "Updating comps... - "
   add_pypi_to_comps
   echo "FINISHED"

add_gem_package.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 
 usage() {
-	echo "Usage: $0 GEM_NAME [TEMPLATE [TITO_TAG [PACKAGE_SUBDIR]]]"
+	echo "Usage: $0 GEM_NAME [TEMPLATE [PACKAGE_SUBDIR [KOJI_TAG]]]"
 	echo "Valid templates: $(ls gem2rpm | sed 's/.spec.erb//' | tr '\n' ' ')"
-	python3 -c "import configparser ; c = configparser.ConfigParser() ; c.read('rel-eng/tito.props') ; print('Tito tags: ' + ' '.join(s for s in c.sections() if s not in ('requirements', 'buildconfig', 'builder')))"
+	./list_koji_tags
 	exit 1
 }
 
@@ -49,30 +49,9 @@ generate_gem_package() {
 	popd
 }
 
-add_to_all_tito_props() {
-	add_to_tito_props $TITO_TAG
-	if [[ $TITO_TAG == "foreman-client-nightly-el8" ]] ; then
-		add_to_tito_props foreman-client-nightly-rhel7
-		add_to_tito_props foreman-client-nightly-el9
-	fi
-}
-
-add_to_tito_props() {
-	local tag=$1
-
-	# Get tito.props whitelists and add node package
-	original_locale=$LC_COLLATE
-	export LC_COLLATE=en_GB
-	local current_whitelist=$(crudini --get rel-eng/tito.props $tag whitelist)
-	local whitelist=$(echo "$current_whitelist $PACKAGE_NAME" | tr " " "\n" | sort -u)
-	crudini --set rel-eng/tito.props $tag whitelist "$whitelist"
-	export LC_COLLATE=$original_locale
-	git add rel-eng/tito.props
-}
-
 add_gem_to_all_comps() {
-	add_gem_to_comps $TITO_TAG
-	if [[ $TITO_TAG == "foreman-client-nightly-el8" ]] ; then
+	add_gem_to_comps $KOJI_TAG
+	if [[ $KOJI_TAG == "foreman-client-nightly-el8" ]] ; then
 		add_gem_to_comps foreman-client-nightly-el9
 		add_gem_to_comps foreman-client-nightly-rhel7
 	fi
@@ -95,13 +74,13 @@ add_gem_to_comps() {
 }
 
 add_to_manifest() {
-	if [[ $TITO_TAG == "foreman-nightly-el8" ]] ; then
+	if [[ $KOJI_TAG == "foreman-nightly-el8" ]] ; then
 		local section="foreman_core_packages"
-	elif [[ $TITO_TAG == "foreman-plugins-nightly-el8" ]] ; then
+	elif [[ $KOJI_TAG == "foreman-plugins-nightly-el8" ]] ; then
 		local section="foreman_plugin_packages"
-	elif [[ $TITO_TAG == "katello-nightly-el8" ]] ; then
+	elif [[ $KOJI_TAG == "katello-nightly-el8" ]] ; then
 		local section="katello_packages"
-	elif [[ $TITO_TAG == "foreman-client-*" ]] ; then
+	elif [[ $KOJI_TAG == "foreman-client-*" ]] ; then
 		local section="foreman_client_packages"
 	else
 		# TODO: installer, smart_proxy plugins, rails
@@ -129,7 +108,7 @@ GEM_NAME=${GEM_NAME_ARR[0]}
 GEM_VERSION=${GEM_NAME_ARR[1]}
 PACKAGE_NAME=rubygem-$GEM_NAME
 TEMPLATE_NAME=$2
-TITO_TAG=$3
+KOJI_TAG=$3
 PACKAGE_SUBDIR=$4
 ROOT=$(git rev-parse --show-toplevel)
 
@@ -145,16 +124,16 @@ if [[ -z $TEMPLATE_NAME ]] ; then
 	fi
 fi
 
-if [[ -z $TITO_TAG ]] ; then
+if [[ -z $KOJI_TAG ]] ; then
 	if [[ $TEMPLATE_NAME == *_plugin ]] ; then
-		TITO_TAG="foreman-plugins-nightly-el8"
+		KOJI_TAG="foreman-plugins-nightly-el8"
 	fi
 fi
 
 if [[ -z $PACKAGE_SUBDIR ]] ; then
-	if [[ $TITO_TAG == foreman-plugins-* ]] ; then
+	if [[ $KOJI_TAG == foreman-plugins-* ]] ; then
 		PACKAGE_SUBDIR="plugins"
-	elif [[ $TITO_TAG == katello-* ]] ; then
+	elif [[ $KOJI_TAG == katello-* ]] ; then
 		PACKAGE_SUBDIR="katello"
 	else
 		PACKAGE_SUBDIR="foreman"
@@ -174,7 +153,7 @@ else
 	UPDATE=false
 fi
 
-if [[ -z $GEM_NAME ]] || [[ -z $TEMPLATE_NAME ]] || [[ $UPDATE != true ]] && [[ -z $TITO_TAG ]]; then
+if [[ -z $GEM_NAME ]] || [[ -z $TEMPLATE_NAME ]] || [[ $UPDATE != true ]] && [[ -z $KOJI_TAG ]]; then
 	usage
 fi
 
@@ -199,7 +178,6 @@ if [[ $UPDATE == true ]] ; then
 else
 	generate_gem_package
 	add_to_manifest
-	add_to_all_tito_props
 	add_gem_to_all_comps
 	git commit -m "Add $PACKAGE_NAME package"
 fi

list_koji_tags
@@ -1,0 +1,17 @@
+#!/usr/bin/env python3
+
+import yaml
+
+try:
+    from yaml import CLoader as Loader
+except ImportError:
+    from yaml import Loader
+
+def main():
+    with open('package_manifest.yaml') as f:
+        manifest = yaml.load(f.read(), Loader=Loader)
+
+    print(' '.join(manifest['all']['vars']['all_tags']))
+
+if __name__ == '__main__':
+    main()

package_manifest.yaml
@@ -4,6 +4,31 @@ all:
     foremandist: .fm3_5
     foreman_version: 'nightly'
     katello_version: 'nightly'
+    all_tags:
+      foreman-el8:
+        name: "foreman-{{ foreman_version }}-el8"
+        dist: '.el8'
+        macros:
+          foremandist: "{{ foremandist }}"
+      foreman-plugins-el8:
+        name: "foreman-plugins-{{ foreman_version }}-el8"
+        dist: '.el8'
+        macros:
+          foremandist: "{{ foremandist }}"
+      foreman-client-el9:
+        name: "foreman-client-{{ foreman_version }}-el9"
+        dist: '.el9'
+      foreman-client-el8:
+        name: "foreman-client-{{ foreman_version }}-el8"
+        dist: '.el8'
+      foreman-client-el7:
+        name: "foreman-client-{{ foreman_version }}-rhel7"
+        dist: '.el7'
+      katello-el8:
+        name: "katello-{{ katello_version }}-el8"
+        dist: '.el8'
+        macros:
+          foremandist: "{{ foremandist }}"
   children:
     packages: {}
     repoclosures: {}
@@ -23,13 +48,7 @@ foreman_packages:
   vars:
     package_base_dir: 'packages/foreman/'
     koji_tags:
-      - name: "foreman-{{ foreman_version }}-el8"
-        dist: '.el8'
-        macros:
-          foremandist: "{{ foremandist }}"
-    releasers:
-      - koji-foreman
-    nightly_releaser: koji-foreman-jenkins
+      - "{{ all_tags['foreman-el8'] }}"
     repoclosure_target_repos:
       el8:
         - "el8-foreman-{{ foreman_version }}"
@@ -51,13 +70,7 @@ plugin_packages:
   vars:
     package_base_dir: 'packages/plugins/'
     koji_tags:
-      - name: "foreman-plugins-{{ foreman_version }}-el8"
-        dist: '.el8'
-        macros:
-          foremandist: "{{ foremandist }}"
-    releasers:
-      - koji-foreman-plugins
-    nightly_releaser: koji-foreman-plugins-jenkins
+      - "{{ all_tags['foreman-plugins-el8'] }}"
     repoclosure_target_repos:
       el8:
         - "el8-foreman-plugins-{{ foreman_version }}"
@@ -96,13 +109,6 @@ foreman_core_packages:
   hosts:
     dynflow-utils: {}
     foreman:
-      releasers:
-        - "{{ nightly_releaser }}"
-      build_package_tito_releaser_args: "{{ nightly_package_tito_releaser_args }}"
-      nightly_package_tito_releaser_args:
-        - "jenkins_job=foreman-develop-source-release"
-        - "jenkins_url=https://ci.theforeman.org"
-      build_package_tito_builder: "tito.builder.FetchBuilder"
       source_location: https://ci.theforeman.org/job/foreman-develop-source-release
       source_system: jenkins
     foreman-bootloaders-redhat: {}
@@ -147,10 +153,7 @@ foreman_core_packages:
     rubygem-execjs: {}
     rubygem-excon: {}
     rubygem-facter: {}
-    rubygem-faraday:
-      releasers:
-        - koji-foreman
-        - koji-foreman-plugins
+    rubygem-faraday: {}
     rubygem-faraday-em_http: {}
     rubygem-faraday-em_synchrony: {}
     rubygem-faraday-excon: {}
@@ -195,23 +198,9 @@ foreman_core_packages:
     rubygem-graphql: {}
     rubygem-graphql-batch: {}
     rubygem-hammer_cli:
-      releasers:
-        - "{{ nightly_releaser }}"
-      build_package_tito_releaser_args: "{{ nightly_package_tito_releaser_args }}"
-      nightly_package_tito_releaser_args:
-        - "jenkins_job=hammer-cli-master-source-release"
-        - "jenkins_url=https://ci.theforeman.org"
-      build_package_tito_builder: "tito.builder.FetchBuilder"
       source_location: https://ci.theforeman.org/job/hammer-cli-master-source-release
       source_system: jenkins
     rubygem-hammer_cli_foreman:
-      releasers:
-        - "{{ nightly_releaser }}"
-      build_package_tito_releaser_args: "{{ nightly_package_tito_releaser_args }}"
-      nightly_package_tito_releaser_args:
-        - "jenkins_job=hammer-cli-foreman-master-source-release"
-        - "jenkins_url=https://ci.theforeman.org"
-      build_package_tito_builder: "tito.builder.FetchBuilder"
       source_location: https://ci.theforeman.org/job/hammer-cli-foreman-master-source-release
       source_system: jenkins
     rubygem-hashie: {}
@@ -246,15 +235,9 @@ foreman_core_packages:
     rubygem-net-ldap: {}
     rubygem-net-ping: {}
     rubygem-net-scp: {}
-    rubygem-net-ssh:
-      releasers:
-        - koji-foreman
-        - koji-foreman-plugins
+    rubygem-net-ssh: {}
     rubygem-net_http_unix: {}
-    rubygem-netrc:
-      releasers:
-        - koji-foreman
-        - koji-foreman-plugins
+    rubygem-netrc: {}
     rubygem-nio4r: {}
     rubygem-nokogiri: {}
     rubygem-oauth: {}
@@ -445,23 +428,9 @@ foreman_nodejs_packages:
 foreman_installer_packages:
   hosts:
     foreman-installer:
-      releasers:
-        - "{{ nightly_releaser }}"
-      build_package_tito_releaser_args: "{{ nightly_package_tito_releaser_args }}"
-      nightly_package_tito_releaser_args:
-        - "jenkins_job=foreman-installer-develop-source-release"
-        - "jenkins_url=https://ci.theforeman.org"
-      build_package_tito_builder: "tito.builder.FetchBuilder"
       source_location: https://ci.theforeman.org/job/foreman-installer-develop-source-release
       source_system: jenkins
     foreman-selinux:
-      releasers:
-        - "{{ nightly_releaser }}"
-      build_package_tito_releaser_args: "{{ nightly_package_tito_releaser_args }}"
-      nightly_package_tito_releaser_args:
-        - "jenkins_job=foreman-selinux-develop-source-release"
-        - "jenkins_url=https://ci.theforeman.org"
-      build_package_tito_builder: "tito.builder.FetchBuilder"
       source_location: https://ci.theforeman.org/job/foreman-selinux-develop-source-release
       source_system: jenkins
     katello-certs-tools: {}
@@ -477,13 +446,6 @@ foreman_installer_packages:
 foreman_proxy_packages:
   hosts:
     foreman-proxy:
-      releasers:
-        - "{{ nightly_releaser }}"
-      build_package_tito_releaser_args: "{{ nightly_package_tito_releaser_args }}"
-      nightly_package_tito_releaser_args:
-        - "jenkins_job=smart-proxy-develop-source-release"
-        - "jenkins_url=https://ci.theforeman.org"
-      build_package_tito_builder: "tito.builder.FetchBuilder"
       source_location: https://ci.theforeman.org/job/smart-proxy-develop-source-release
       source_system: jenkins
     rubygem-concurrent-ruby: {}
@@ -498,15 +460,10 @@ foreman_proxy_packages:
 foreman_client_packages:
   vars:
     package_base_dir: 'packages/client/'
-    releasers:
-      - koji-foreman-client
     koji_tags:
-      - name: "foreman-client-{{ foreman_version }}-el9"
-        dist: '.el9'
-      - name: "foreman-client-{{ foreman_version }}-el8"
-        dist: '.el8'
-      - name: "foreman-client-{{ foreman_version }}-rhel7"
-        dist: '.el7'
+      - "{{ all_tags['foreman-client-el9'] }}"
+      - "{{ all_tags['foreman-client-el8'] }}"
+      - "{{ all_tags['foreman-client-el7'] }}"
     repoclosure_target_repos:
       el9:
         - "el9-foreman-client-{{ foreman_version }}"
@@ -546,13 +503,7 @@ katello_packages:
   vars:
     package_base_dir: 'packages/katello/'
     koji_tags:
-      - name: "katello-{{ katello_version }}-el8"
-        dist: '.el8'
-        macros:
-          foremandist: "{{ foremandist }}"
-    releasers:
-      - koji-katello
-    nightly_releaser: koji-katello-jenkins
+      - "{{ all_tags['katello-el8'] }}"
     repoclosure_target_repos:
       el8:
         - "el8-katello-{{ katello_version }}"
@@ -599,23 +550,9 @@ katello_packages:
     rubygem-hammer_cli_foreman_scc_manager: {}
     rubygem-hammer_cli_foreman_virt_who_configure: {}
     rubygem-hammer_cli_katello:
-      releasers:
-        - "{{ nightly_releaser }}"
-      build_package_tito_releaser_args: "{{ nightly_package_tito_releaser_args }}"
-      nightly_package_tito_releaser_args:
-        - "jenkins_job=hammer-cli-katello-master-source-release"
-        - "jenkins_url=https://ci.theforeman.org"
-      build_package_tito_builder: "tito.builder.FetchBuilder"
       source_location: https://ci.theforeman.org/job/hammer-cli-katello-master-source-release
       source_system: jenkins
     rubygem-katello:
-      releasers:
-        - "{{ nightly_releaser }}"
-      build_package_tito_releaser_args: "{{ nightly_package_tito_releaser_args }}"
-      nightly_package_tito_releaser_args:
-        - "jenkins_job=katello-master-source-release"
-        - "jenkins_url=https://ci.theforeman.org"
-      build_package_tito_builder: "tito.builder.FetchBuilder"
       source_location: https://ci.theforeman.org/job/katello-master-source-release
       source_system: jenkins
     rubygem-pulp_python_client: {}
@@ -902,16 +839,10 @@ repoclosures:
 foreman_release_packages:
   vars:
     koji_tags:
-      - name: "foreman-{{ foreman_version }}-el8"
-        dist: '.el8'
-      - name: "foreman-client-{{ foreman_version }}-el9"
-        dist: '.el9'
-      - name: "foreman-client-{{ foreman_version }}-el8"
-        dist: '.el8'
-      - name: "foreman-client-{{ foreman_version }}-rhel7"
-        dist: '.el7'
-    releasers:
-      - koji-foreman-client
+      - "{{ all_tags['foreman-el8'] }}"
+      - "{{ all_tags['foreman-client-el9'] }}"
+      - "{{ all_tags['foreman-client-el8'] }}"
+      - "{{ all_tags['foreman-client-el7'] }}"
     package_base_dir: 'packages/foreman/'
     repoclosure_target_repos:
       el9:

rel-eng/compare-with-tags
@@ -1,75 +1,0 @@
-#!/usr/bin/env python3
-#
-# Compares tito.props with koji and prints out differences in Github markup for
-# review and cleanup.
-#
-
-from configparser import ConfigParser
-from pathlib import Path
-from subprocess import check_output
-from typing import Set
-
-
-def sclize(package: str, scl: str) -> str:
-    if package.startswith(('rubygem-', 'nodejs-')):
-        return f"{scl}-{package}"
-    return package
-
-
-def get_koji_packages(tag: str) -> Set[str]:
-    command = ['koji', 'list-pkgs', f'--tag={tag}', '--quiet']
-    output = check_output(command, universal_newlines=True)
-    return {line.split(None, 1)[0] for line in output.splitlines()}
-
-
-def main(filename: Path) -> None:
-    cfg = ConfigParser()
-
-    read = cfg.read(filename)
-    if read != [str(filename)]:
-        raise SystemExit(f'Failed to read {filename}')
-
-    for tag, entry in cfg.items():
-        comps_packages = set(entry.get('whitelist', '').split())
-        if not comps_packages:
-            continue
-
-        try:
-            scl = entry['scl']
-        except KeyError:
-            pass
-        else:
-            comps_packages = {sclize(package, scl) for package in comps_packages}
-
-        koji_packages = get_koji_packages(tag)
-
-        missing_packages = comps_packages - koji_packages
-        extra_packages = koji_packages - comps_packages
-
-        # We can't automatically build foreman-release in client, koji will fail with duplicate
-        # builds. We always manually tag it to client after.
-        if tag.startswith('foreman-client-'):
-            extra_packages -= {'foreman-release'}
-
-        if missing_packages:
-            print(f"\n# Packages missing in tag {tag}")
-            for missing_package in sorted(missing_packages):
-                print(f" * [x] {missing_package}")
-            print("```shell")
-            print(f"koji add-pkg --owner=kojiadm {tag} {' '.join(sorted(missing_packages))}")
-            print("```")
-
-        if extra_packages:
-            print(f"\n# Packages not expected in {tag}")
-            for extra_package in sorted(extra_packages):
-                print(f" * [x] {extra_package}")
-            print("```shell")
-            print(f"koji remove-pkg {tag} {' '.join(sorted(extra_packages))}")
-            print("```")
-
-
-if __name__ == '__main__':
-    try:
-        main(Path(__file__).parent / 'tito.props')
-    except KeyboardInterrupt:
-        pass

rel-eng/custom/custom.py
@@ -1,248 +1,0 @@
-# Copyright (c) 2008-2014 Red Hat, Inc.
-#
-# This software is licensed to you under the GNU General Public License,
-# version 2 (GPLv2). There is NO WARRANTY for this software, express or
-# implied, including the implied warranties of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
-# along with this software; if not, see
-# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
-#
-# Red Hat trademarks are not licensed under GPLv2. No permission is
-# granted to use or replicate Red Hat trademarks that are incorporated
-# in this software or its documentation.
-
-from datetime import datetime
-import glob
-import json
-import os
-import os.path
-import re
-import shutil
-import subprocess
-from zipfile import ZipFile
-
-try:
-  from urllib import urlopen, urlretrieve
-except ImportError:
-  from urllib.request import urlopen, urlretrieve
-
-from tito.builder.fetch import SourceStrategy
-from tito.common import error_out, debug, run_command
-
-
-class ForemanSourceStrategy(SourceStrategy):
-    """
-    Designed to be used for nightly or pull request builds only.
-
-    It first copies source files from git, then copies the source file(s)
-    over the top, so they're merged (patches etc can then be stored
-    in git).
-    """
-    def fetch(self):
-        if "source_files" in self.builder.args:
-            gitrev = self._fetch_prebuilt_local()
-        elif "source_dir" in self.builder.args:
-            gitrev = self._fetch_local()
-        elif "jenkins_job" in self.builder.args:
-            gitrev = self._fetch_jenkins()
-        else:
-            raise Exception("Specify either '--arg jenkins_job=...', '--arg source_dir=...', or '--arg source_files=...[,...]")
-
-        # Copy the live spec from our starting location. Unlike most builders,
-        # we are not using a copy from a past git commit.
-        self.spec_file = os.path.join(self.builder.rpmbuild_sourcedir,
-                    '%s.spec' % self.builder.project_name)
-        shutil.copyfile(
-            os.path.join(self.builder.start_dir, '%s.spec' %
-                self.builder.project_name),
-            self.spec_file)
-        for s in os.listdir(self.builder.start_dir):
-            if os.path.exists(os.path.join(self.builder.start_dir, s)):
-                shutil.copyfile(
-                    os.path.join(self.builder.start_dir, s),
-                    os.path.join(self.builder.rpmbuild_sourcedir, os.path.basename(s)))
-        print("  %s.spec" % self.builder.project_name)
-
-        replacements = []
-        src_files = run_command("find %s -type f" %
-              os.path.join(self.builder.rpmbuild_sourcedir, 'archive')).split("\n")
-
-        def filter_archives(path):
-            base_name = os.path.basename(path)
-            return ".tar" in base_name or ".gem" in base_name
-
-        for i, s in enumerate(filter(filter_archives, src_files)):
-            base_name = os.path.basename(s)
-            debug("Downloaded file %s" % base_name)
-
-            dest_filepath = os.path.join(self.builder.rpmbuild_sourcedir,
-                    base_name)
-            shutil.move(s, dest_filepath)
-            self.sources.append(dest_filepath)
-
-            # Add a line to replace in the spec for each source:
-            source_regex = re.compile("^(source%s:\s*)(.+)$" % i, re.IGNORECASE)
-            new_line = "Source%s: %s\n" % (i, base_name)
-            replacements.append((source_regex, new_line))
-
-        rel_date = datetime.utcnow().strftime("%Y%m%d%H%M%S")
-        self.release = rel_date + gitrev
-        print("Building release: %s" % self.release)
-        run_command("sed -i '1i %%global nightly .%s' %s" % (self.release, self.spec_file))
-
-    """
-    Downloads the source files from Jenkins, from a job that produces them as
-    artifacts.  Will follow the version number present in the filename and
-    adds a timestamp & SHA if available from the Jenkins job start reasons.
-
-    Designed to be used for nightly or pull request builds only.
-
-    It first copies source files from git, then downloads the Jenkins
-    artifacts over the top, so they're merged (patches etc can then be stored
-    in git).
-
-    Takes the following arguments:
-      jenkins_url: base URL of Jenkins ("https://ci.theforeman.org")
-      jenkins_job: name of job ("test_develop")
-      jenkins_job_id: job number or alias ("123", "lastSuccessfulBuild")
-    """
-    def _fetch_jenkins(self):
-        url_base = self.builder.args['jenkins_url'][0]
-        job_name = self.builder.args['jenkins_job'][0]
-        if 'jenkins_job_id' in self.builder.args:
-            job_id = self.builder.args['jenkins_job_id'][0]
-        else:
-            job_id = "lastSuccessfulBuild"
-        job_url_base = "%s/job/%s/%s" % (url_base, job_name, job_id)
-        json_url = "%s/api/json" % job_url_base
-
-        debug(json_url)
-        job_info = json.loads(urlopen(json_url).read().decode("utf-8"))
-        if "number" in job_info:
-            job_id = job_info["number"]
-
-        if "runs" in job_info:
-            run_idx = 0
-            for idx, run in enumerate(job_info["runs"]):
-                if run["number"] == job_id:
-                    run_idx = idx
-                    break
-            job_url_base = job_info["runs"][run_idx]["url"]
-        elif "url" in job_info:
-            job_url_base = job_info["url"]
-
-        url = "%s/artifact/*zip*/archive.zip" % job_url_base
-        debug("Fetching from %s" % url)
-
-        (zip_path, zip_headers) = urlretrieve(url)
-        zip_file = ZipFile(zip_path, 'r')
-        try:
-            zip_file.extractall(self.builder.rpmbuild_sourcedir)
-        finally:
-            zip_file.close()
-
-        gitrev = ""
-        for action in job_info["actions"]:
-            if "lastBuiltRevision" in action:
-                gitrev = "git%s" % action["lastBuiltRevision"]["SHA1"][0:7]
-
-        return gitrev
-
-    """
-    Generates the source file from a local repo, useful for generating
-    packages of custom branches.
-
-    It first copies source files from git, then copies the source file
-    over the top, so they're merged (patches etc can then be stored
-    in git).
-
-    Takes the following arguments:
-      source_dir: path to repo ("~/foreman")
-    """
-    def _fetch_local(self):
-        source_dir = os.path.expanduser(self.builder.args['source_dir'][0])
-
-        old_dir = os.getcwd()
-        os.chdir(source_dir)
-
-        gemspecs = glob.glob("./*.gemspec")
-        if gemspecs and not gemspecs[0] == "./smart_proxy.gemspec":
-          subprocess.call(["gem", "build", gemspecs[0]])
-          sources = glob.glob("./*.gem")
-        else:
-          subprocess.call(["/bin/bash", "-l", "-c", "bundle exec rake pkg:generate_source"])
-          sources = glob.glob("./pkg/*")
-
-        fetchdir = os.path.join(self.builder.rpmbuild_sourcedir, 'archive')
-        if not os.path.exists(fetchdir):
-          os.mkdir(fetchdir)
-
-        for srcfile in sources:
-          debug("Copying %s from local source dir" % srcfile)
-          shutil.move(srcfile, os.path.join(fetchdir, os.path.basename(srcfile)))
-
-        gitrev = "local"
-        gitsha = subprocess.check_output(["git", "rev-parse", "HEAD"], universal_newlines=True)
-        if gitsha:
-          gitrev = "git%s" % gitsha[0:7]
-
-        os.chdir(old_dir)
-
-        return gitrev
-
-    def _fetch_prebuilt_local(self):
-        source_files = [os.path.expanduser(source_file) for source_file in self.builder.args['source_files']]
-        try:
-            gitsha = self.builder.args['git_hash'][0]
-        except (KeyError, IndexError):
-            raise Exception("Specify '--arg git_hash=...' when using '--arg source_files=...[,...]")
-        dest_dir = os.path.join(self.builder.rpmbuild_sourcedir, 'archive')
-        if not os.path.exists(dest_dir):
-            os.mkdir(dest_dir)
-
-        for src in source_files:
-            debug("Copying %s to %s" % (src, os.path.join(dest_dir, os.path.basename(src))))
-            shutil.copy(src, os.path.join(dest_dir, os.path.basename(src)))
-
-        return "git%s" % gitsha[0:7]
-
-    def _get_version(self):
-        """
-        Get the version from the builder.
-        Sources are configured at this point.
-        """
-        # Assuming source0 is a tar.gz we can extract a version from:
-        base_name = os.path.basename(self.sources[0])
-        debug("Extracting version from: %s" % base_name)
-
-        # Example filename: tito-0.4.18.tar.gz:
-        simple_version_re = re.compile(".*-(.*).(tar.gz|tgz|zip|tar.bz2|gem)")
-        match = re.search(simple_version_re, base_name)
-        if match:
-            version = match.group(1)
-        else:
-            error_out("Unable to determine version from file: %s" % base_name)
-
-        return version
-
-    def replace_in_spec(self, replacements):
-        """
-        Replace lines in the spec file using the given replacements.
-
-        Replacements are a tuple of a regex to look for, and a new line to
-        substitute in when the regex matches.
-
-        Replaces all lines with one pass through the file.
-        """
-        in_f = open(self.spec_file, 'r')
-        out_f = open(self.spec_file + ".new", 'w')
-        for line in in_f.readlines():
-            for line_regex, new_line in replacements:
-                match = re.match(line_regex, line)
-                if match:
-                    line = new_line
-            out_f.write(line)
-
-        in_f.close()
-        out_f.close()
-        shutil.move(self.spec_file + ".new", self.spec_file)

commit d5bcdd9e3c8ab80e8f33c4ca4605e05b8547ce13

Drop crudini dependency
Since tito was dropped, this is no longer needed.

Fixes: fa53fd5eb76a52a15db4cd01a97f8ff6e8575462

add_npm_package.sh
@@ -110,7 +110,6 @@ if [[ -z $NPM_MODULE_NAME ]]; then
   exit 1
 fi
 
-ensure_program crudini
 ensure_program npm2rpm
 ensure_program spectool
 

add_pypi_package.sh
@@ -124,7 +124,6 @@ if [[ -z $PYPI_NAME ]]; then
   exit 1
 fi
 
-ensure_program crudini
 ensure_program pyp2rpm
 ensure_program spec2scl
 