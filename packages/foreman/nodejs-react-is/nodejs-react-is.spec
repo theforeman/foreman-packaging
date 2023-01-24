theforeman/foreman-packaging

commit d553ea8701734fa45e1459955e2e21d5e91b9b09

Remove unused nodejs-react-helmet

package_manifest.yaml
@@ -836,8 +836,6 @@ katello_packages:
     nodejs-downshift: {}
     nodejs-ngreact: {}
     nodejs-query-string: {}
-    nodejs-react-helmet:
-      strategy: bundle
     nodejs-strict-uri-encode: {}
     python-qpid: {}
     qpid-cpp: {}

comps/comps-katello-el8.xml
@@ -33,7 +33,6 @@
       <packagereq type="default">nodejs-downshift</packagereq>
       <packagereq type="default">nodejs-ngreact</packagereq>
       <packagereq type="default">nodejs-query-string</packagereq>
-      <packagereq type="default">nodejs-react-helmet</packagereq>
       <packagereq type="default">nodejs-strict-uri-encode</packagereq>
       <!-- katello thirdparty rubygem packages -->
       <packagereq type="default">rubygem-anemone</packagereq>

comps/comps-katello-server-rhel7.xml
@@ -37,7 +37,6 @@
       <packagereq type="default">tfm-nodejs-downshift</packagereq>
       <packagereq type="default">tfm-nodejs-ngreact</packagereq>
       <packagereq type="default">tfm-nodejs-query-string</packagereq>
-      <packagereq type="default">tfm-nodejs-react-helmet</packagereq>
       <packagereq type="default">tfm-nodejs-strict-uri-encode</packagereq>
       <!-- katello thirdparty rubygem packages -->
       <packagereq type="default">tfm-rubygem-anemone</packagereq>

packages/katello/nodejs-react-helmet/deep-equal-1.1.0.tgz
@@ -1,1 +1,0 @@
-../../../.git/annex/objects/FW/vf/SHA256E-s6351--3efffbce6d4b45b8f9e4ef5f72d344b60554fee85a6f447963bb7fc03e87ae6d.0.tgz/SHA256E-s6351--3efffbce6d4b45b8f9e4ef5f72d344b60554fee85a6f447963bb7fc03e87ae6d.0.tgz

packages/katello/nodejs-react-helmet/define-properties-1.1.3.tgz
@@ -1,1 +1,0 @@
-../../../.git/annex/objects/PK/ZF/SHA256E-s6295--5524f4db15bb95af92c791ba208e2fc774bd8ae17eb347865492d444f95a70a5.3.tgz/SHA256E-s6295--5524f4db15bb95af92c791ba208e2fc774bd8ae17eb347865492d444f95a70a5.3.tgz

packages/katello/nodejs-react-helmet/function-bind-1.1.1.tgz
@@ -1,1 +1,0 @@
-../../../.git/annex/objects/vV/ZV/SHA256E-s6301--2d552e7c98b4c3acbf6420cc2eed535e592bb987ed2b32e35795bf777dd2e082.1.tgz/SHA256E-s6301--2d552e7c98b4c3acbf6420cc2eed535e592bb987ed2b32e35795bf777dd2e082.1.tgz

packages/katello/nodejs-react-helmet/has-1.0.3.tgz
@@ -1,1 +1,0 @@
-../../../.git/annex/objects/m8/pV/SHA256E-s1553--c38cf9eefce04e58ec3e945bfe448b9236d64f9337f6bf6e1f62e1c5b6b05573.3.tgz/SHA256E-s1553--c38cf9eefce04e58ec3e945bfe448b9236d64f9337f6bf6e1f62e1c5b6b05573.3.tgz

packages/katello/nodejs-react-helmet/is-arguments-1.0.4.tgz
@@ -1,1 +1,0 @@
-../../../.git/annex/objects/G5/6p/SHA256E-s5490--8afe33ec76149bd5dd1bab1cd3f251016defc16991b0703e8e711391bae4df2a.4.tgz/SHA256E-s5490--8afe33ec76149bd5dd1bab1cd3f251016defc16991b0703e8e711391bae4df2a.4.tgz

commit ec68dbf38a9a6682bb28761b7fa2ae8123a74820

Remove unused package nodejs-react-is
Fixes: d553ea8701734fa45e1459955e2e21d5e91b9b09

comps/comps-foreman-el8.xml
@@ -74,7 +74,6 @@
       <packagereq type="default">nodejs-react-bootstrap</packagereq>
       <packagereq type="default">nodejs-react-ellipsis-with-tooltip</packagereq>
       <packagereq type="default">nodejs-react-intl</packagereq>
-      <packagereq type="default">nodejs-react-is</packagereq>
       <packagereq type="default">nodejs-react-lifecycles-compat</packagereq>
       <packagereq type="default">nodejs-react-redux</packagereq>
       <packagereq type="default">nodejs-redhat-cloud-services-frontend-components</packagereq>

package_manifest.yaml
@@ -371,7 +371,6 @@ foreman_nodejs_packages:
     nodejs-react-ellipsis-with-tooltip: {}
     nodejs-react-intl:
       strategy: bundle
-    nodejs-react-is: {}
     nodejs-react-lifecycles-compat: {}
     nodejs-react-redux:
       strategy: bundle

packages/foreman/nodejs-react-is/nodejs-react-is.spec
@@ -1,58 +1,0 @@
-%{?scl:%scl_package nodejs-%{npm_name}}
-%{!?scl:%global pkg_name %{name}}
-
-%global npm_name react-is
-
-Name: %{?scl_prefix}nodejs-react-is
-Version: 16.5.2
-Release: 4%{?dist}
-Summary: Brand checking of React Elements
-License: MIT
-Group: Development/Libraries
-URL: https://reactjs.org/
-Source0: https://registry.npmjs.org/%{npm_name}/-/%{npm_name}-%{version}.tgz
-%if 0%{?scl:1}
-BuildRequires: %{?scl_prefix_nodejs}npm
-%else
-BuildRequires: nodejs-packaging
-BuildRequires: npm
-%endif
-BuildArch: noarch
-ExclusiveArch: %{nodejs_arches} noarch
-Provides: %{?scl_prefix}npm(%{npm_name}) = %{version}
-
-%description
-%{summary}
-
-%prep
-%setup -q -n package
-
-%install
-mkdir -p %{buildroot}%{nodejs_sitelib}/%{npm_name}
-cp -pfr cjs %{buildroot}%{nodejs_sitelib}/%{npm_name}
-cp -pfr index.js %{buildroot}%{nodejs_sitelib}/%{npm_name}
-cp -pfr package.json %{buildroot}%{nodejs_sitelib}/%{npm_name}
-cp -pfr umd %{buildroot}%{nodejs_sitelib}/%{npm_name}
-
-%nodejs_symlink_deps
-
-%check
-%{nodejs_symlink_deps} --check
-
-%files
-%{nodejs_sitelib}/%{npm_name}
-%license LICENSE
-%doc README.md
-
-%changelog
-* Tue Mar 17 2020 Zach Huntington-Meath <zhunting@redhat.com> - 16.5.2-4
-- Bump packages to build for el8
-
-* Tue Oct 22 2019 Eric D. Helms <ericdhelms@gmail.com> - 16.5.2-3
-- Build for SCL
-
-* Fri Oct 04 2019 Eric D. Helms <ericdhelms@gmail.com> - 16.5.2-2
-- Update specs to handle SCL
-
-* Wed Oct 10 2018 Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl> 16.5.2-1
-- Add nodejs-react-is generated by npm2rpm using the single strategy

packages/foreman/nodejs-react-is/react-is-16.5.2.tgz
@@ -1,1 +1,0 @@
-../../../.git/annex/objects/1f/Zp/SHA256E-s3981--24350f557c94c9324bbc472154659ba29dc0a3e335dcb5f3899d6f2eef44edee.2.tgz/SHA256E-s3981--24350f557c94c9324bbc472154659ba29dc0a3e335dcb5f3899d6f2eef44edee.2.tgz