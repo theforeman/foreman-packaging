theforeman/foreman-packaging

commit d553ea8701734fa45e1459955e2e21d5e91b9b09

Remove unused nodejs-react-helmet

package_manifest.yaml
@@ -836,8 +836,6 @@ katello_packages:
     nodejs-downshift: {}
     nodejs-ngreact: {}
     nodejs-query-string: {}
-    nodejs-react-helmet:
-      strategy: bundle
     nodejs-strict-uri-encode: {}
     python-qpid: {}
     qpid-cpp: {}

packages/katello/nodejs-react-helmet/object-assign-4.1.1.tgz
@@ -1,1 +1,0 @@
-../../../.git/annex/objects/8V/xk/SHA256E-s2675--782d726a263ba7b26cced612af97b80035516df4b0cd788524e7b2cebc4e29ed.1.tgz/SHA256E-s2675--782d726a263ba7b26cced612af97b80035516df4b0cd788524e7b2cebc4e29ed.1.tgz

comps/comps-katello-el8.xml
@@ -33,7 +33,6 @@
       <packagereq type="default">nodejs-downshift</packagereq>
       <packagereq type="default">nodejs-ngreact</packagereq>
       <packagereq type="default">nodejs-query-string</packagereq>
-      <packagereq type="default">nodejs-react-helmet</packagereq>
       <packagereq type="default">nodejs-strict-uri-encode</packagereq>
       <!-- katello thirdparty rubygem packages -->
       <packagereq type="default">rubygem-anemone</packagereq>

comps/comps-katello-server-rhel7.xml
@@ -37,7 +37,6 @@
       <packagereq type="default">tfm-nodejs-downshift</packagereq>
       <packagereq type="default">tfm-nodejs-ngreact</packagereq>
       <packagereq type="default">tfm-nodejs-query-string</packagereq>
-      <packagereq type="default">tfm-nodejs-react-helmet</packagereq>
       <packagereq type="default">tfm-nodejs-strict-uri-encode</packagereq>
       <!-- katello thirdparty rubygem packages -->
       <packagereq type="default">tfm-rubygem-anemone</packagereq>

packages/katello/nodejs-react-helmet/deep-equal-1.1.0.tgz
@@ -1,1 +1,0 @@
-../../../.git/annex/objects/FW/vf/SHA256E-s6351--3efffbce6d4b45b8f9e4ef5f72d344b60554fee85a6f447963bb7fc03e87ae6d.0.tgz/SHA256E-s6351--3efffbce6d4b45b8f9e4ef5f72d344b60554fee85a6f447963bb7fc03e87ae6d.0.tgz

packages/katello/nodejs-react-helmet/define-properties-1.1.3.tgz
@@ -1,1 +1,0 @@
-../../../.git/annex/objects/PK/ZF/SHA256E-s6295--5524f4db15bb95af92c791ba208e2fc774bd8ae17eb347865492d444f95a70a5.3.tgz/SHA256E-s6295--5524f4db15bb95af92c791ba208e2fc774bd8ae17eb347865492d444f95a70a5.3.tgz

packages/katello/nodejs-react-helmet/function-bind-1.1.1.tgz
@@ -1,1 +1,0 @@
-../../../.git/annex/objects/vV/ZV/SHA256E-s6301--2d552e7c98b4c3acbf6420cc2eed535e592bb987ed2b32e35795bf777dd2e082.1.tgz/SHA256E-s6301--2d552e7c98b4c3acbf6420cc2eed535e592bb987ed2b32e35795bf777dd2e082.1.tgz

packages/katello/nodejs-react-helmet/has-1.0.3.tgz
@@ -1,1 +1,0 @@
-../../../.git/annex/objects/m8/pV/SHA256E-s1553--c38cf9eefce04e58ec3e945bfe448b9236d64f9337f6bf6e1f62e1c5b6b05573.3.tgz/SHA256E-s1553--c38cf9eefce04e58ec3e945bfe448b9236d64f9337f6bf6e1f62e1c5b6b05573.3.tgz

commit 680ff857c79b48d7507d6f50e02e544345928bdb

Remove unused package nodejs-object-assign
Fixes: d553ea8701734fa45e1459955e2e21d5e91b9b09

comps/comps-foreman-el8.xml
@@ -69,7 +69,6 @@
       <packagereq type="default">nodejs-lodash-es</packagereq>
       <packagereq type="default">nodejs-loose-envify</packagereq>
       <packagereq type="default">nodejs-node-sass</packagereq>
-      <packagereq type="default">nodejs-object-assign</packagereq>
       <packagereq type="default">nodejs-optimize-css-assets-webpack-plugin</packagereq>
       <packagereq type="default">nodejs-performance-now</packagereq>
       <packagereq type="default">nodejs-popper.js</packagereq>

package_manifest.yaml
@@ -364,7 +364,6 @@ foreman_nodejs_packages:
     nodejs-loose-envify: {}
     nodejs-node-sass:
       strategy: bundle
-    nodejs-object-assign: {}
     nodejs-optimize-css-assets-webpack-plugin: {}
     nodejs-performance-now: {}
     nodejs-popper.js: {}

packages/foreman/nodejs-object-assign/nodejs-object-assign.spec
@@ -1,56 +1,0 @@
-%{?scl:%scl_package nodejs-%{npm_name}}
-%{!?scl:%global pkg_name %{name}}
-
-%global npm_name object-assign
-
-Name: %{?scl_prefix}nodejs-object-assign
-Version: 4.1.1
-Release: 4%{?dist}
-Summary: ES2015 `Object
-License: MIT
-Group: Development/Libraries
-URL: https://github.com/sindresorhus/object-assign#readme
-Source0: https://registry.npmjs.org/%{npm_name}/-/%{npm_name}-%{version}.tgz
-%if 0%{?scl:1}
-BuildRequires: %{?scl_prefix_nodejs}npm
-%else
-BuildRequires: nodejs-packaging
-BuildRequires: npm
-%endif
-BuildArch: noarch
-ExclusiveArch: %{nodejs_arches} noarch
-Provides: %{?scl_prefix}npm(%{npm_name}) = %{version}
-
-%description
-%{summary}
-
-%prep
-%setup -q -n package
-
-%install
-mkdir -p %{buildroot}%{nodejs_sitelib}/%{npm_name}
-cp -pfr index.js %{buildroot}%{nodejs_sitelib}/%{npm_name}
-cp -pfr package.json %{buildroot}%{nodejs_sitelib}/%{npm_name}
-
-%nodejs_symlink_deps
-
-%check
-%{nodejs_symlink_deps} --check
-
-%files
-%{nodejs_sitelib}/%{npm_name}
-%license license
-%doc readme.md
-
-%changelog
-* Tue Mar 17 2020 Zach Huntington-Meath <zhunting@redhat.com> - 4.1.1-4
-- Bump packages to build for el8
-
-* Tue Oct 22 2019 Eric D. Helms <ericdhelms@gmail.com> - 4.1.1-3
-- Build for SCL
-
-* Fri Oct 04 2019 Eric D. Helms <ericdhelms@gmail.com> - 4.1.1-2
-- Update specs to handle SCL
-
-* Wed Oct 18 2017 Eric D. Helms <ericdhelms@gmail.com> 4.1.1-1
-- new package built with tito

packages/foreman/nodejs-object-assign/object-assign-4.1.1.tgz
@@ -1,1 +1,0 @@
-../../../.git/annex/objects/8V/xk/SHA256E-s2675--782d726a263ba7b26cced612af97b80035516df4b0cd788524e7b2cebc4e29ed.1.tgz/SHA256E-s2675--782d726a263ba7b26cced612af97b80035516df4b0cd788524e7b2cebc4e29ed.1.tgz