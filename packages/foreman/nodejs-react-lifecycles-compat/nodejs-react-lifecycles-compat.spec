theforeman/foreman-packaging

commit 162257c13fb80076465f01c055c55a762e032412

Remove unused nodejs packages

comps/comps-foreman-el8.xml
@@ -54,29 +54,16 @@
       <packagereq type="default">nodejs-babel-preset-react</packagereq>
       <packagereq type="default">nodejs-babel-register</packagereq>
       <packagereq type="default">nodejs-babel-runtime</packagereq>
-      <packagereq type="default">nodejs-bootstrap-slider-without-jquery</packagereq>
-      <packagereq type="default">nodejs-breakjs</packagereq>
       <packagereq type="default">nodejs-c3</packagereq>
-      <packagereq type="default">nodejs-change-emitter</packagereq>
-      <packagereq type="default">nodejs-classnames</packagereq>
       <packagereq type="default">nodejs-compression-webpack-plugin</packagereq>
       <packagereq type="default">nodejs-core-js</packagereq>
-      <packagereq type="default">nodejs-create-react-context</packagereq>
-      <packagereq type="default">nodejs-css-element-queries</packagereq>
       <packagereq type="default">nodejs-css-loader</packagereq>
       <packagereq type="default">nodejs-d3</packagereq>
-      <packagereq type="default">nodejs-dom-helpers</packagereq>
       <packagereq type="default">nodejs-dotenv</packagereq>
-      <packagereq type="default">nodejs-escape-string-regexp</packagereq>
       <packagereq type="default">nodejs-expose-loader</packagereq>
       <packagereq type="default">nodejs-extract-text-webpack-plugin</packagereq>
-      <packagereq type="default">nodejs-fbjs</packagereq>
       <packagereq type="default">nodejs-file-loader</packagereq>
-      <packagereq type="default">nodejs-fortawesome-fontawesome-common-types</packagereq>
-      <packagereq type="default">nodejs-fortawesome-free-brands-svg-icons</packagereq>
-      <packagereq type="default">nodejs-gud</packagereq>
       <packagereq type="default">nodejs-harmony-reflect</packagereq>
-      <packagereq type="default">nodejs-hoist-non-react-statics</packagereq>
       <packagereq type="default">nodejs-humanize-duration</packagereq>
       <packagereq type="default">nodejs-identity-obj-proxy</packagereq>
       <packagereq type="default">nodejs-intl</packagereq>
@@ -86,44 +73,23 @@
       <packagereq type="default">nodejs-js-tokens</packagereq>
       <packagereq type="default">nodejs-lodash</packagereq>
       <packagereq type="default">nodejs-lodash-es</packagereq>
-      <packagereq type="default">nodejs-lodash.debounce</packagereq>
       <packagereq type="default">nodejs-loose-envify</packagereq>
       <packagereq type="default">nodejs-node-gyp</packagereq>
       <packagereq type="default">nodejs-node-sass</packagereq>
       <packagereq type="default">nodejs-object-assign</packagereq>
-      <packagereq type="default">nodejs-patternfly</packagereq>
-      <packagereq type="default">nodejs-patternfly-react</packagereq>
-      <packagereq type="default">nodejs-patternfly-react-icons</packagereq>
-      <packagereq type="default">nodejs-patternfly-react-tokens</packagereq>
       <packagereq type="default">nodejs-performance-now</packagereq>
       <packagereq type="default">nodejs-popper.js</packagereq>
-      <packagereq type="default">nodejs-prop-types</packagereq>
-      <packagereq type="default">nodejs-prop-types-extra</packagereq>
       <packagereq type="default">nodejs-raf</packagereq>
       <packagereq type="default">nodejs-react</packagereq>
       <packagereq type="default">nodejs-react-bootstrap</packagereq>
-      <packagereq type="default">nodejs-react-bootstrap-switch</packagereq>
-      <packagereq type="default">nodejs-react-bootstrap-typeahead</packagereq>
-      <packagereq type="default">nodejs-react-c3js</packagereq>
-      <packagereq type="default">nodejs-react-click-outside</packagereq>
-      <packagereq type="default">nodejs-react-collapse</packagereq>
-      <packagereq type="default">nodejs-react-debounce-input</packagereq>
       <packagereq type="default">nodejs-react-dom</packagereq>
       <packagereq type="default">nodejs-react-ellipsis-with-tooltip</packagereq>
-      <packagereq type="default">nodejs-react-fontawesome</packagereq>
       <packagereq type="default">nodejs-react-intl</packagereq>
       <packagereq type="default">nodejs-react-is</packagereq>
       <packagereq type="default">nodejs-react-lifecycles-compat</packagereq>
-      <packagereq type="default">nodejs-react-motion</packagereq>
-      <packagereq type="default">nodejs-react-overlays</packagereq>
-      <packagereq type="default">nodejs-react-popper</packagereq>
       <packagereq type="default">nodejs-react-redux</packagereq>
-      <packagereq type="default">nodejs-react-transition-group</packagereq>
-      <packagereq type="default">nodejs-reactabular-table</packagereq>
-      <packagereq type="default">nodejs-recompose</packagereq>
       <packagereq type="default">nodejs-redux</packagereq>
       <packagereq type="default">nodejs-redux-thunk</packagereq>
-      <packagereq type="default">nodejs-regenerator-runtime</packagereq>
       <packagereq type="default">nodejs-regenerator-runtime</packagereq>
       <packagereq type="default">nodejs-reselect</packagereq>
       <packagereq type="default">nodejs-reselect</packagereq>

package_manifest.yaml
@@ -370,34 +370,20 @@ foreman_nodejs_packages:
     nodejs-babel-register:
       strategy: bundle
     nodejs-babel-runtime: {}
-    nodejs-bootstrap-slider-without-jquery: {}
-    nodejs-breakjs: {}
     nodejs-c3: {}
-    nodejs-change-emitter: {}
-    nodejs-classnames: {}
     nodejs-compression-webpack-plugin:
       strategy: bundle
     nodejs-core-js: {}
-    nodejs-create-react-context: {}
-    nodejs-css-element-queries: {}
     nodejs-css-loader:
       strategy: bundle
     nodejs-d3: {}
-    nodejs-dom-helpers: {}
     nodejs-dotenv: {}
-    nodejs-escape-string-regexp: {}
     nodejs-expose-loader: {}
     nodejs-extract-text-webpack-plugin:
       strategy: bundle
-    nodejs-fbjs:
-      strategy: bundle
     nodejs-file-loader:
       strategy: bundle
-    nodejs-fortawesome-fontawesome-common-types: {}
-    nodejs-fortawesome-free-brands-svg-icons: {}
-    nodejs-gud: {}
     nodejs-harmony-reflect: {}
-    nodejs-hoist-non-react-statics: {}
     nodejs-humanize-duration: {}
     nodejs-identity-obj-proxy: {}
     nodejs-intl: {}
@@ -406,7 +392,6 @@ foreman_nodejs_packages:
     nodejs-jquery: {}
     nodejs-js-tokens: {}
     nodejs-lodash-es: {}
-    nodejs-lodash.debounce: {}
     nodejs-lodash: {}
     nodejs-loose-envify: {}
     nodejs-node-gyp:
@@ -414,42 +399,22 @@ foreman_nodejs_packages:
     nodejs-node-sass:
       strategy: bundle
     nodejs-object-assign: {}
-    nodejs-patternfly-react: {}
-    nodejs-patternfly-react-icons: {}
-    nodejs-patternfly-react-tokens: {}
-    nodejs-patternfly:
-      strategy: bundle
     nodejs-performance-now: {}
     nodejs-popper.js: {}
-    nodejs-prop-types: {}
-    nodejs-prop-types-extra: {}
     nodejs-raf: {}
-    nodejs-react-bootstrap-switch: {}
     nodejs-react-bootstrap:
       strategy: bundle
-    nodejs-react-bootstrap-typeahead: {}
-    nodejs-react-c3js: {}
-    nodejs-react-click-outside: {}
-    nodejs-react-collapse: {}
-    nodejs-react-debounce-input: {}
     nodejs-react-dom:
       strategy: bundle
     nodejs-react-ellipsis-with-tooltip: {}
-    nodejs-react-fontawesome: {}
     nodejs-react-intl:
       strategy: bundle
     nodejs-react-is: {}
     nodejs-react-lifecycles-compat: {}
-    nodejs-react-motion: {}
-    nodejs-react-overlays: {}
-    nodejs-react-popper: {}
     nodejs-react-redux:
       strategy: bundle
-    nodejs-react-transition-group: {}
     nodejs-react:
       strategy: bundle
-    nodejs-reactabular-table: {}
-    nodejs-recompose: {}
     nodejs-redux-thunk: {}
     nodejs-redux: {}
     nodejs-regenerator-runtime: {}

commit 39b0a33a980bb13d071824cafd4543e4ca16e44a

Remove unused package nodejs-react-lifecycles-compat
Fixes: 162257c13fb80076465f01c055c55a762e032412

comps/comps-foreman-el8.xml
@@ -74,7 +74,6 @@
       <packagereq type="default">nodejs-react-bootstrap</packagereq>
       <packagereq type="default">nodejs-react-ellipsis-with-tooltip</packagereq>
       <packagereq type="default">nodejs-react-intl</packagereq>
-      <packagereq type="default">nodejs-react-lifecycles-compat</packagereq>
       <packagereq type="default">nodejs-react-redux</packagereq>
       <packagereq type="default">nodejs-redhat-cloud-services-frontend-components</packagereq>
       <packagereq type="default">nodejs-redux</packagereq>

package_manifest.yaml
@@ -371,7 +371,6 @@ foreman_nodejs_packages:
     nodejs-react-ellipsis-with-tooltip: {}
     nodejs-react-intl:
       strategy: bundle
-    nodejs-react-lifecycles-compat: {}
     nodejs-react-redux:
       strategy: bundle
     nodejs-redhat-cloud-services-frontend-components: {}

packages/foreman/nodejs-react-lifecycles-compat/nodejs-react-lifecycles-compat.spec
@@ -1,60 +1,0 @@
-%{?scl:%scl_package nodejs-%{npm_name}}
-%{!?scl:%global pkg_name %{name}}
-
-%global npm_name react-lifecycles-compat
-
-Name: %{?scl_prefix}nodejs-react-lifecycles-compat
-Version: 3.0.4
-Release: 4%{?dist}
-Summary: Backwards compatibility polyfill for React class components
-License: MIT
-Group: Development/Libraries
-URL: https://github.com/reactjs/react-lifecycles-compat#readme
-Source0: https://registry.npmjs.org/%{npm_name}/-/%{npm_name}-%{version}.tgz
-%if 0%{?scl:1}
-BuildRequires: %{?scl_prefix_nodejs}npm
-%else
-BuildRequires: nodejs-packaging
-BuildRequires: npm
-%endif
-BuildArch: noarch
-ExclusiveArch: %{nodejs_arches} noarch
-Provides: %{?scl_prefix}npm(%{npm_name}) = %{version}
-
-%description
-%{summary}
-
-%prep
-%setup -q -n package
-
-%install
-mkdir -p %{buildroot}%{nodejs_sitelib}/%{npm_name}
-cp -pfr package.json %{buildroot}%{nodejs_sitelib}/%{npm_name}
-cp -pfr react-lifecycles-compat.cjs.js %{buildroot}%{nodejs_sitelib}/%{npm_name}
-cp -pfr react-lifecycles-compat.es.js %{buildroot}%{nodejs_sitelib}/%{npm_name}
-cp -pfr react-lifecycles-compat.js %{buildroot}%{nodejs_sitelib}/%{npm_name}
-cp -pfr react-lifecycles-compat.min.js %{buildroot}%{nodejs_sitelib}/%{npm_name}
-
-%nodejs_symlink_deps
-
-%check
-%{nodejs_symlink_deps} --check
-
-%files
-%{nodejs_sitelib}/%{npm_name}
-%license LICENSE.md
-%doc CHANGELOG.md
-%doc README.md
-
-%changelog
-* Tue Mar 17 2020 Zach Huntington-Meath <zhunting@redhat.com> - 3.0.4-4
-- Bump packages to build for el8
-
-* Tue Oct 22 2019 Eric D. Helms <ericdhelms@gmail.com> - 3.0.4-3
-- Build for SCL
-
-* Fri Oct 04 2019 Eric D. Helms <ericdhelms@gmail.com> - 3.0.4-2
-- Update specs to handle SCL
-
-* Wed Oct 10 2018 Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl> 3.0.4-1
-- Add nodejs-react-lifecycles-compat generated by npm2rpm using the single strategy

packages/foreman/nodejs-react-lifecycles-compat/react-lifecycles-compat-3.0.4.tgz
@@ -1,1 +1,0 @@
-../../../.git/annex/objects/Pp/2q/SHA256E-s5999--4e063ff3ccfa89ef3ee748e2a6c107b8ad48c96c7add226522464580623c9ae5.4.tgz/SHA256E-s5999--4e063ff3ccfa89ef3ee748e2a6c107b8ad48c96c7add226522464580623c9ae5.4.tgz