theforeman/foreman-packaging

commit b2ee5e7af99d6626e7ece6245e6241359ca1ccfb

Add nodejs-patternfly-react-catalog-view-extension package

package_manifest.yaml
@@ -937,6 +937,8 @@ plugin_scl_packages:
     foreman-discovery-image-service: {}
     nodejs-babel-plugin-module-resolver:
       strategy: bundle
+    nodejs-patternfly-react-catalog-view-extension:
+      strategy: bundle
     nodejs-react-json-tree:
       strategy: bundle
     rubygem-acts_as_list: {}

commit 3a434e93c2985f82aa90a66104a6d44295a06fda

Remove unused package nodejs-warning
Fixes: b2ee5e7af99d6626e7ece6245e6241359ca1ccfb

comps/comps-foreman-el8.xml
@@ -86,7 +86,6 @@
       <packagereq type="default">nodejs-url-loader</packagereq>
       <packagereq type="default">nodejs-use-deep-compare-effect</packagereq>
       <packagereq type="default">nodejs-uuid</packagereq>
-      <packagereq type="default">nodejs-warning</packagereq>
       <packagereq type="default">nodejs-webpack</packagereq>
       <packagereq type="default">nodejs-webpack-stats-plugin</packagereq>
       <packagereq type="default">pcp-mmvstatsd</packagereq>

package_manifest.yaml
@@ -390,7 +390,6 @@ foreman_nodejs_packages:
       strategy: bundle
     nodejs-use-deep-compare-effect: {}
     nodejs-uuid: {}
-    nodejs-warning: {}
     nodejs-webpack-stats-plugin: {}
     nodejs-webpack:
       strategy: bundle

packages/foreman/nodejs-warning/nodejs-warning.spec
@@ -1,59 +1,0 @@
-%{?scl:%scl_package nodejs-%{npm_name}}
-%{!?scl:%global pkg_name %{name}}
-
-%global npm_name warning
-
-Name: %{?scl_prefix}nodejs-warning
-Version: 4.0.2
-Release: 4%{?dist}
-Summary: A mirror of Facebook's Warning
-License: MIT
-Group: Development/Libraries
-URL: https://github.com/BerkeleyTrue/warning
-Source0: https://registry.npmjs.org/%{npm_name}/-/%{npm_name}-%{version}.tgz
-%if 0%{?scl:1}
-BuildRequires: %{?scl_prefix_nodejs}npm
-%else
-BuildRequires: nodejs-packaging
-BuildRequires: npm
-%endif
-Requires: %{?scl_prefix}npm(loose-envify) >= 1.0.0
-Requires: %{?scl_prefix}npm(loose-envify) < 2.0.0
-BuildArch: noarch
-ExclusiveArch: %{nodejs_arches} noarch
-Provides: %{?scl_prefix}npm(%{npm_name}) = %{version}
-
-%description
-%{summary}
-
-%prep
-%setup -q -n package
-
-%install
-mkdir -p %{buildroot}%{nodejs_sitelib}/%{npm_name}
-cp -pfr package.json %{buildroot}%{nodejs_sitelib}/%{npm_name}
-cp -pfr warning.js %{buildroot}%{nodejs_sitelib}/%{npm_name}
-
-%nodejs_symlink_deps
-
-%check
-%{nodejs_symlink_deps} --check
-
-%files
-%{nodejs_sitelib}/%{npm_name}
-%license LICENSE.md
-%doc CHANGELOG.md
-%doc README.md
-
-%changelog
-* Tue Mar 17 2020 Zach Huntington-Meath <zhunting@redhat.com> - 4.0.2-4
-- Bump packages to build for el8
-
-* Tue Oct 22 2019 Eric D. Helms <ericdhelms@gmail.com> - 4.0.2-3
-- Build for SCL
-
-* Fri Oct 04 2019 Eric D. Helms <ericdhelms@gmail.com> - 4.0.2-2
-- Update specs to handle SCL
-
-* Wed Oct 10 2018 Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl> 4.0.2-1
-- Add nodejs-warning generated by npm2rpm using the single strategy

packages/foreman/nodejs-warning/warning-4.0.2.tgz
@@ -1,1 +1,0 @@
-../../../.git/annex/objects/wp/XP/SHA256E-s3681--367b711d7372f52d5148fab7c0658e881a04d735225c3650aaf10f4a2cf4b401.2.tgz/SHA256E-s3681--367b711d7372f52d5148fab7c0658e881a04d735225c3650aaf10f4a2cf4b401.2.tgz