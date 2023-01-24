theforeman/foreman-packaging

commit b2ee5e7af99d6626e7ece6245e6241359ca1ccfb

Add nodejs-patternfly-react-catalog-view-extension package

package_manifest.yaml
@@ -937,6 +937,8 @@ plugin_scl_packages:
     foreman-discovery-image-service: {}
     nodejs-babel-plugin-module-resolver:
       strategy: bundle
+    nodejs-patternfly-react-catalog-view-extension:
+      strategy: bundle
     nodejs-react-json-tree:
       strategy: bundle
     rubygem-acts_as_list: {}

commit f70ed9bbbe158ee43c338eb6cc4247d7f1f28875

Remove unused package nodejs-popper.js
Fixes: b2ee5e7af99d6626e7ece6245e6241359ca1ccfb

comps/comps-foreman-el8.xml
@@ -71,7 +71,6 @@
       <packagereq type="default">nodejs-node-sass</packagereq>
       <packagereq type="default">nodejs-optimize-css-assets-webpack-plugin</packagereq>
       <packagereq type="default">nodejs-performance-now</packagereq>
-      <packagereq type="default">nodejs-popper.js</packagereq>
       <packagereq type="default">nodejs-raf</packagereq>
       <packagereq type="default">nodejs-react</packagereq>
       <packagereq type="default">nodejs-react-bootstrap</packagereq>

package_manifest.yaml
@@ -366,7 +366,6 @@ foreman_nodejs_packages:
       strategy: bundle
     nodejs-optimize-css-assets-webpack-plugin: {}
     nodejs-performance-now: {}
-    nodejs-popper.js: {}
     nodejs-raf: {}
     nodejs-react-bootstrap:
       strategy: bundle

packages/foreman/nodejs-popper.js/nodejs-popper.js.spec
@@ -1,57 +1,0 @@
-%{?scl:%scl_package nodejs-%{npm_name}}
-%{!?scl:%global pkg_name %{name}}
-
-%global npm_name popper.js
-
-Name: %{?scl_prefix}nodejs-popper.js
-Version: 1.14.4
-Release: 4%{?dist}
-Summary: A kickass library to manage your poppers
-License: MIT
-Group: Development/Libraries
-URL: https://popper.js.org
-Source0: https://registry.npmjs.org/%{npm_name}/-/%{npm_name}-%{version}.tgz
-%if 0%{?scl:1}
-BuildRequires: %{?scl_prefix_nodejs}npm
-%else
-BuildRequires: nodejs-packaging
-BuildRequires: npm
-%endif
-BuildArch: noarch
-ExclusiveArch: %{nodejs_arches} noarch
-Provides: %{?scl_prefix}npm(%{npm_name}) = %{version}
-
-%description
-%{summary}
-
-%prep
-%setup -q -n package
-
-%install
-mkdir -p %{buildroot}%{nodejs_sitelib}/%{npm_name}
-cp -pfr dist %{buildroot}%{nodejs_sitelib}/%{npm_name}
-cp -pfr index.d.ts %{buildroot}%{nodejs_sitelib}/%{npm_name}
-cp -pfr index.js.flow %{buildroot}%{nodejs_sitelib}/%{npm_name}
-cp -pfr package.json %{buildroot}%{nodejs_sitelib}/%{npm_name}
-
-%nodejs_symlink_deps
-
-%check
-%{nodejs_symlink_deps} --check
-
-%files
-%{nodejs_sitelib}/%{npm_name}
-%doc README.md
-
-%changelog
-* Tue Mar 17 2020 Zach Huntington-Meath <zhunting@redhat.com> - 1.14.4-4
-- Bump packages to build for el8
-
-* Tue Oct 22 2019 Eric D. Helms <ericdhelms@gmail.com> - 1.14.4-3
-- Build for SCL
-
-* Fri Oct 04 2019 Eric D. Helms <ericdhelms@gmail.com> - 1.14.4-2
-- Update specs to handle SCL
-
-* Wed Oct 10 2018 Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl> 1.14.4-1
-- Add nodejs-popper.js generated by npm2rpm using the single strategy

packages/foreman/nodejs-popper.js/popper.js-1.14.4.tgz
@@ -1,1 +1,0 @@
-../../../.git/annex/objects/x9/zp/SHA256E-s441697--c67db918e4ba864bcbe3b06403da432cd3e8193c255460ae31afe60642e1242e.4.tgz/SHA256E-s441697--c67db918e4ba864bcbe3b06403da432cd3e8193c255460ae31afe60642e1242e.4.tgz