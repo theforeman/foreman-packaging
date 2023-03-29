theforeman/foreman-packaging

commit ae91976bc8ef44611a6570513f972754add1d99d

Drop EL7 koji tags and EL7 only packages

package_manifest.yaml
@@ -46,14 +46,8 @@ rails_core_packages:
 
 foreman_scl_packages:
   vars:
-    scl: 'tfm'
     package_base_dir: 'packages/foreman/'
     koji_tags:
-      - name: "foreman-{{ foreman_version }}-rhel7"
-        scl: 'tfm'
-        dist: '.el7'
-        macros:
-          foremandist: "{{ foremandist }}"
       - name: "foreman-{{ foreman_version }}-el8"
         dist: '.el8'
         macros:
@@ -234,11 +228,6 @@ foreman_scl_packages:
     rubygem-puma-status: {}
     rubygem-quantile: {}
     rubygem-rabl: {}
-    rubygem-racc:
-      koji_tags:
-        - name: "foreman-{{ foreman_version }}-rhel7"
-          scl: 'tfm'
-          dist: '.el7'
     rubygem-rack: {}
     rubygem-rack-cors: {}
     rubygem-rack-jsonp: {}
@@ -299,20 +288,11 @@ foreman_scl_packages:
     rubygem-wirb: {}
     rubygem-xmlrpc: {}
     rubygem-zeitwerk: {}
-    tfm:
-      koji_tags:
-        - name: "foreman-{{ foreman_version }}-rhel7"
-          scl: 'tfm'
-          dist: '.el7'
 
 foreman_nodejs_packages:
   vars:
-    scl: 'tfm'
     package_base_dir: 'packages/foreman/'
     koji_tags:
-      - name: "foreman-{{ foreman_version }}-rhel7"
-        dist: '.el7'
-        scl: 'tfm'
       - name: "foreman-{{ foreman_version }}-el8"
         dist: '.el8'
     releasers:
@@ -436,11 +416,6 @@ foreman_installer_packages:
   vars:
     package_base_dir: 'packages/foreman/'
     koji_tags:
-      - name: "foreman-{{ foreman_version }}-nonscl-rhel7"
-        dist: '.el7'
-      - name: "foreman-{{ foreman_version }}-rhel7"
-        scl: 'tfm'
-        dist: '.el7'
       - name: "foreman-{{ foreman_version }}-el8"
         dist: '.el8'
     releasers:
@@ -492,8 +467,6 @@ foreman_nonscl_packages:
   vars:
     package_base_dir: 'packages/foreman/'
     koji_tags:
-      - name: "foreman-{{ foreman_version }}-nonscl-rhel7"
-        dist: '.el7'
       - name: "foreman-{{ foreman_version }}-el8"
         dist: '.el8'
     releasers:
@@ -521,11 +494,6 @@ foreman_proxy_packages:
   vars:
     package_base_dir: 'packages/foreman/'
     koji_tags:
-      - name: "foreman-{{ foreman_version }}-rhel7"
-        scl: 'tfm'
-        dist: '.el7'
-        macros:
-          foremandist: "{{ foremandist }}"
       - name: "foreman-{{ foreman_version }}-el8"
         dist: '.el8'
         macros:
@@ -566,11 +534,6 @@ foreman_proxy_plugin_packages:
   vars:
     package_base_dir: 'packages/plugins/'
     koji_tags:
-      - name: "foreman-plugins-{{ foreman_version }}-rhel7"
-        dist: '.el7'
-        scl: 'tfm'
-        macros:
-          foremandist: "{{ foremandist }}"
       - name: "foreman-plugins-{{ foreman_version }}-el8"
         dist: '.el8'
         macros:
@@ -630,11 +593,6 @@ foreman_scl_and_nonscl_packages:
   vars:
     package_base_dir: 'packages/foreman/'
     koji_tags:
-      - name: "foreman-{{ foreman_version }}-rhel7"
-        scl: 'tfm'
-        dist: '.el7'
-      - name: "foreman-{{ foreman_version }}-nonscl-rhel7"
-        dist: '.el7'
       - name: "foreman-{{ foreman_version }}-el8"
         dist: '.el8'
     releasers:
@@ -706,11 +664,6 @@ katello_packages:
   vars:
     package_base_dir: 'packages/katello/'
     koji_tags:
-      - name: "katello-{{ katello_version }}-rhel7"
-        scl: 'tfm'
-        dist: '.el7'
-        macros:
-          foremandist: "{{ foremandist }}"
       - name: "katello-{{ katello_version }}-el8"
         dist: '.el8'
         macros:
@@ -809,11 +762,6 @@ plugin_scl_packages:
   vars:
     package_base_dir: 'packages/plugins/'
     koji_tags:
-      - name: "foreman-plugins-{{ foreman_version }}-rhel7"
-        scl: 'tfm'
-        dist: '.el7'
-        macros:
-          foremandist: "{{ foremandist }}"
       - name: "foreman-plugins-{{ foreman_version }}-el8"
         dist: '.el8'
         macros:
@@ -979,8 +927,6 @@ plugin_nonscl_packages:
   vars:
     package_base_dir: 'packages/plugins/'
     koji_tags:
-      - name: "foreman-plugins-{{ foreman_version }}-nonscl-rhel7"
-        dist: '.el7'
       - name: "foreman-plugins-{{ foreman_version }}-el8"
         dist: '.el8'
     releasers:

packages/foreman/rubygem-racc/racc-1.5.2.gem
@@ -1,1 +1,0 @@
-../../../.git/annex/objects/0W/j0/SHA256E-s419840--2f48a44974ebc6b724f763f546a57706bbbeff571b7e1cf7d50919db413e139d.2.gem/SHA256E-s419840--2f48a44974ebc6b724f763f546a57706bbbeff571b7e1cf7d50919db413e139d.2.gem

packages/foreman/rubygem-racc/rubygem-racc.spec
@@ -1,122 +1,0 @@
-# template: scl
-%{?scl:%scl_package rubygem-%{gem_name}}
-%{!?scl:%global pkg_name %{name}}
-
-%global gem_name racc
-%global gem_require_name %{gem_name}
-
-Name: %{?scl_prefix}rubygem-%{gem_name}
-Version: 1.5.2
-Release: 1%{?dist}
-Summary: Racc is a LALR(1) parser generator
-Group: Development/Languages
-License: Ruby and BSD-2-Clause
-URL: http://i.loveruby.net/en/projects/racc/
-Source0: https://rubygems.org/gems/%{gem_name}-%{version}.gem
-
-# start specfile generated dependencies
-Requires: %{?scl_prefix_ruby}ruby(release)
-Requires: %{?scl_prefix_ruby}ruby
-Requires: %{?scl_prefix_ruby}ruby(rubygems)
-BuildRequires: %{?scl_prefix_ruby}ruby(release)
-BuildRequires: %{?scl_prefix_ruby}ruby-devel
-BuildRequires: %{?scl_prefix_ruby}rubygems-devel
-Provides: %{?scl_prefix}rubygem(%{gem_name}) = %{version}
-# end specfile generated dependencies
-
-%description
-Racc is a LALR(1) parser generator.
-It is written in Ruby itself, and generates Ruby program.
-NOTE: Ruby 1.8.x comes with Racc runtime module.  You
-can run your parsers generated by racc 1.4.x out of the
-box.
-
-
-%package doc
-Summary: Documentation for %{pkg_name}
-Group: Documentation
-Requires: %{?scl_prefix}%{pkg_name} = %{version}-%{release}
-BuildArch: noarch
-
-%description doc
-Documentation for %{pkg_name}.
-
-%prep
-%{?scl:scl enable %{scl} - << \EOF}
-gem unpack %{SOURCE0}
-%{?scl:EOF}
-
-%setup -q -D -T -n  %{gem_name}-%{version}
-
-%{?scl:scl enable %{scl} - << \EOF}
-gem spec %{SOURCE0} -l --ruby > %{gem_name}.gemspec
-%{?scl:EOF}
-
-%build
-# Create the gem as gem install only works on a gem file
-%{?scl:scl enable %{scl} - << \EOF}
-gem build %{gem_name}.gemspec
-%{?scl:EOF}
-
-# %%gem_install compiles any C extensions and installs the gem into ./%%gem_dir
-# by default, so that we can move it into the buildroot in %%install
-%{?scl:scl enable %{scl} - << \EOF}
-%gem_install
-%{?scl:EOF}
-
-%install
-mkdir -p %{buildroot}%{gem_dir}
-cp -a .%{gem_dir}/* \
-        %{buildroot}%{gem_dir}/
-
-mkdir -p %{buildroot}%{gem_extdir_mri}/%{gem_name}
-cp -a .%{gem_extdir_mri}/gem.build_complete %{buildroot}%{gem_extdir_mri}/
-cp -a .%{gem_extdir_mri}/%{gem_name}/*.so %{buildroot}%{gem_extdir_mri}/%{gem_name}
-
-# Prevent dangling symlink in -debuginfo (rhbz#878863).
-rm -rf %{buildroot}%{gem_instdir}/ext/
-
-mkdir -p %{buildroot}%{_bindir}
-cp -a .%{_bindir}/* \
-        %{buildroot}%{_bindir}/
-find %{buildroot}%{gem_instdir}/bin -type f | xargs chmod a+x
-
-%check
-%{?scl:scl enable %{scl} - << \EOF}
-# Ideally, this would be something like this:
-# GEM_PATH="%{buildroot}%{gem_dir}:$GEM_PATH" ruby -e "require '%{gem_require_name}'"
-# But that fails to find native extensions on EL8, so we fake the structure that ruby expects
-mkdir gem_ext_test
-cp -a %{buildroot}%{gem_dir} gem_ext_test/
-mkdir -p gem_ext_test/gems/extensions/%{_arch}-%{_target_os}/$(ruby -r rbconfig -e 'print RbConfig::CONFIG["ruby_version"]')/
-cp -a %{buildroot}%{gem_extdir_mri} gem_ext_test/gems/extensions/%{_arch}-%{_target_os}/$(ruby -r rbconfig -e 'print RbConfig::CONFIG["ruby_version"]')/
-GEM_PATH="./gem_ext_test/gems:$GEM_PATH" ruby -e "require '%{gem_require_name}'"
-rm -rf gem_ext_test
-%{?scl:EOF}
-
-%files
-%dir %{gem_instdir}
-%{_bindir}/racc
-%exclude %{gem_instdir}/ext
-%{gem_extdir_mri}
-%license %{gem_instdir}/COPYING
-%exclude %{gem_instdir}/TODO
-%{gem_instdir}/bin
-%{gem_libdir}
-%exclude %{gem_instdir}/web
-%exclude %{gem_cache}
-%{gem_spec}
-
-%files doc
-%doc %{gem_docdir}
-%doc %{gem_instdir}/ChangeLog
-%doc %{gem_instdir}/README.ja.rdoc
-%doc %{gem_instdir}/README.rdoc
-%{gem_instdir}/Rakefile
-%doc %{gem_instdir}/rdoc
-%doc %{gem_instdir}/sample
-%{gem_instdir}/test
-
-%changelog
-* Thu Apr 29 2021 Eric D. Helms <ericdhelms@gmail.com> 1.5.2-1
-- Add rubygem-racc generated by gem2rpm using the scl template

commit bcb6e0e408d36632f23a6b70903fe12f34ff80e5

drop rubygem-qpid_proton package
It was only used for EL7 SCL, as on EL8 we build the
`rubygem-qpid_proton` RPM from the `qpid-proton` source.

Fixes: ae91976bc8ef44611a6570513f972754add1d99d

package_manifest.yaml
@@ -532,7 +532,6 @@ katello_packages:
     rubygem-pulp_file_client: {}
     rubygem-pulp_ostree_client: {}
     rubygem-pulp_rpm_client: {}
-    rubygem-qpid_proton: {}
     rubygem-robotex: {}
     rubygem-runcible: {}
     rubygem-stomp: {}

packages/katello/rubygem-qpid_proton/qpid_proton-0.37.0.gem
@@ -1,1 +1,0 @@
-../../../.git/annex/objects/Jg/11/SHA256E-s148480--12fc2158890c65afe27985f87ab31c89be54ab1de2a02f95085fa63fd8235ed2.0.gem/SHA256E-s148480--12fc2158890c65afe27985f87ab31c89be54ab1de2a02f95085fa63fd8235ed2.0.gem

packages/katello/rubygem-qpid_proton/rubygem-qpid_proton.spec
@@ -1,137 +1,0 @@
-# template: scl
-%{?scl:%scl_package rubygem-%{gem_name}}
-%{!?scl:%global pkg_name %{name}}
-
-%global gem_name qpid_proton
-%global gem_require_name %{gem_name}
-%global qpid_version 0.37.0
-
-Name: %{?scl_prefix}rubygem-%{gem_name}
-Version: %{qpid_version}
-Release: 1%{?dist}
-Summary: Ruby language bindings for the Qpid Proton messaging framework
-Group: Development/Languages
-License: Apache-2.0
-URL: http://qpid.apache.org/proton
-Source0: https://rubygems.org/gems/%{gem_name}-%{version}.gem
-
-Autoreq: 0
-
-Requires: qpid-proton-c = %{qpid_version}
-BuildRequires: qpid-proton-c-devel = %{qpid_version}
-
-# start specfile generated dependencies
-Requires: %{?scl_prefix_ruby}ruby(release)
-Requires: %{?scl_prefix_ruby}ruby >= 1.9.3
-Requires: %{?scl_prefix_ruby}ruby(rubygems)
-BuildRequires: %{?scl_prefix_ruby}ruby(release)
-BuildRequires: %{?scl_prefix_ruby}ruby-devel >= 1.9.3
-BuildRequires: %{?scl_prefix_ruby}rubygems-devel
-Provides: %{?scl_prefix}rubygem(%{gem_name}) = %{version}
-# end specfile generated dependencies
-
-%description
-Proton is a high performance, lightweight messaging library. It can be used in
-the widest range of messaging applications including brokers, client
-libraries,
-routers, bridges, proxies, and more. Proton is based on the AMQP 1.0 messaging
-standard.
-
-
-%package doc
-Summary: Documentation for %{pkg_name}
-Group: Documentation
-Requires: %{?scl_prefix}%{pkg_name} = %{version}-%{release}
-BuildArch: noarch
-
-%description doc
-Documentation for %{pkg_name}.
-
-%prep
-%{?scl:scl enable %{scl} - << \EOF}
-gem unpack %{SOURCE0}
-%{?scl:EOF}
-
-%setup -q -D -T -n  %{gem_name}-%{version}
-
-%{?scl:scl enable %{scl} - << \EOF}
-gem spec %{SOURCE0} -l --ruby > %{gem_name}.gemspec
-%{?scl:EOF}
-
-%build
-# Create the gem as gem install only works on a gem file
-%{?scl:scl enable %{scl} - << \EOF}
-gem build %{gem_name}.gemspec
-%{?scl:EOF}
-
-# %%gem_install compiles any C extensions and installs the gem into ./%%gem_dir
-# by default, so that we can move it into the buildroot in %%install
-%{?scl:scl enable %{scl} - << \EOF}
-%gem_install
-%{?scl:EOF}
-
-%install
-mkdir -p %{buildroot}%{gem_dir}
-cp -a .%{gem_dir}/* \
-        %{buildroot}%{gem_dir}/
-
-mkdir -p %{buildroot}%{gem_extdir_mri}
-cp -a .%{gem_extdir_mri}/{gem.build_complete,*.so} %{buildroot}%{gem_extdir_mri}/
-
-# Prevent dangling symlink in -debuginfo (rhbz#878863).
-rm -rf %{buildroot}%{gem_instdir}/ext/
-
-%check
-%{?scl:scl enable %{scl} - << \EOF}
-# Ideally, this would be something like this:
-# GEM_PATH="%{buildroot}%{gem_dir}:$GEM_PATH" ruby -e "require '%{gem_require_name}'"
-# But that fails to find native extensions on EL8, so we fake the structure that ruby expects
-mkdir gem_ext_test
-cp -a %{buildroot}%{gem_dir} gem_ext_test/
-mkdir -p gem_ext_test/gems/extensions/%{_arch}-%{_target_os}/$(ruby -r rbconfig -e 'print RbConfig::CONFIG["ruby_version"]')/
-cp -a %{buildroot}%{gem_extdir_mri} gem_ext_test/gems/extensions/%{_arch}-%{_target_os}/$(ruby -r rbconfig -e 'print RbConfig::CONFIG["ruby_version"]')/
-GEM_PATH="./gem_ext_test/gems:$GEM_PATH" ruby -e "require '%{gem_require_name}'"
-rm -rf gem_ext_test
-%{?scl:EOF}
-
-%files
-%dir %{gem_instdir}
-%{gem_extdir_mri}
-%license %{gem_instdir}/LICENSE.txt
-%{gem_libdir}
-%exclude %{gem_cache}
-%{gem_spec}
-
-%files doc
-%doc %{gem_docdir}
-%{gem_instdir}/examples
-%{gem_instdir}/tests
-
-%changelog
-* Thu Apr 07 2022 Evgeni Golov 0.37.0-1
-- Update to 0.37.0
-
-* Mon Jan 24 2022 Justin Sherrill <jsherril@redhat.com> 0.36.0-1
-- Update to 0.36.0
-
-* Fri Sep 10 2021 Eric D. Helms <ericdhelms@gmail.com> - 0.35.0-1
-- Release 0.35.0
-
-* Thu May 20 2021 Eric D. Helms <ericdhelms@gmail.com> - 0.34.0-3
-- Bump release to build against rh-ruby27
-
-* Thu May 20 2021 Eric D. Helms <ericdhelms@gmail.com> - 0.34.0-2
-- Bump release to build against rh-ruby25
-
-* Thu May 20 2021 Justin Sherrill <jsherril@redhat.com> 0.34.0-1
-- Update to 0.34.0
-
-* Tue Apr 06 2021 Eric D. Helms <ericdhelms@gmail.com> - 0.33.0-2
-- Rebuild for Ruby 2.7
-
-* Mon Feb 1 2021 Jonathon Turel - 0.33.0-1
-- Bump to 0.33.0
-
-* Fri Jan 29 2021 Jonathon Turel - 0.32.0-1
-- Add rubygem-qpid_proton generated by gem2rpm using the scl template
-