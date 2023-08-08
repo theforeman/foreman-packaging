theforeman/foreman-packaging

commit abb2a5a0f684d049a9de26e6fd7930cef008d95b

Remove unused package nodejs-redux
Fixes: 33ce14cb9774202898c647cab74687c2479e0219

comps/comps-foreman-el8.xml
@@ -74,7 +74,6 @@
       <packagereq type="default">nodejs-react-bootstrap</packagereq>
       <packagereq type="default">nodejs-react-ellipsis-with-tooltip</packagereq>
       <packagereq type="default">nodejs-react-intl</packagereq>
-      <packagereq type="default">nodejs-redux</packagereq>
       <packagereq type="default">nodejs-redux-thunk</packagereq>
       <packagereq type="default">nodejs-regenerator-runtime</packagereq>
       <packagereq type="default">nodejs-reselect</packagereq>

package_manifest.yaml
@@ -372,7 +372,6 @@ foreman_nodejs_packages:
     nodejs-react-intl:
       strategy: bundle
     nodejs-redux-thunk: {}
-    nodejs-redux: {}
     nodejs-regenerator-runtime: {}
     nodejs-reselect: {}
     nodejs-sass-loader:

packages/foreman/nodejs-redux/nodejs-redux.spec
@@ -1,72 +1,0 @@
-%{?scl:%scl_package nodejs-%{npm_name}}
-%{!?scl:%global pkg_name %{name}}
-
-%global npm_name redux
-
-Name: %{?scl_prefix}nodejs-redux
-Version: 3.7.2
-Release: 4%{?dist}
-Summary: Predictable state container for JavaScript apps
-License: MIT
-Group: Development/Libraries
-URL: http://redux.js.org
-Source0: https://registry.npmjs.org/%{npm_name}/-/%{npm_name}-%{version}.tgz
-%if 0%{?scl:1}
-BuildRequires: %{?scl_prefix_nodejs}npm
-%else
-BuildRequires: nodejs-packaging
-BuildRequires: npm
-%endif
-Requires: %{?scl_prefix}npm(lodash) >= 4.2.1
-Requires: %{?scl_prefix}npm(lodash) < 5.0.0
-Requires: %{?scl_prefix}npm(lodash-es) >= 4.2.1
-Requires: %{?scl_prefix}npm(lodash-es) < 5.0.0
-Requires: %{?scl_prefix}npm(loose-envify) >= 1.1.0
-Requires: %{?scl_prefix}npm(loose-envify) < 2.0.0
-Requires: %{?scl_prefix}npm(symbol-observable) >= 1.0.3
-Requires: %{?scl_prefix}npm(symbol-observable) < 2.0.0
-BuildArch: noarch
-ExclusiveArch: %{nodejs_arches} noarch
-Provides: %{?scl_prefix}npm(%{npm_name}) = %{version}
-
-%description
-%{summary}
-
-%prep
-%setup -q -n package
-
-%install
-mkdir -p %{buildroot}%{nodejs_sitelib}/%{npm_name}
-cp -pfr dist %{buildroot}%{nodejs_sitelib}/%{npm_name}
-cp -pfr es %{buildroot}%{nodejs_sitelib}/%{npm_name}
-cp -pfr index.d.ts %{buildroot}%{nodejs_sitelib}/%{npm_name}
-cp -pfr lib %{buildroot}%{nodejs_sitelib}/%{npm_name}
-cp -pfr package.json %{buildroot}%{nodejs_sitelib}/%{npm_name}
-cp -pfr src %{buildroot}%{nodejs_sitelib}/%{npm_name}
-
-%nodejs_symlink_deps
-
-%check
-%{nodejs_symlink_deps} --check
-
-%files
-%{nodejs_sitelib}/%{npm_name}
-%license LICENSE.md
-%doc CHANGELOG.md
-%doc README.md
-
-%changelog
-* Tue Mar 17 2020 Zach Huntington-Meath <zhunting@redhat.com> - 3.7.2-4
-- Bump packages to build for el8
-
-* Tue Oct 22 2019 Eric D. Helms <ericdhelms@gmail.com> - 3.7.2-3
-- Build for SCL
-
-* Fri Oct 04 2019 Eric D. Helms <ericdhelms@gmail.com> - 3.7.2-2
-- Update specs to handle SCL
-
-* Thu Apr 26 2018 Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl> 3.7.2-1
-- Update to 3.7.2
-
-* Thu Feb 16 2017 Dominic Cleal <dominic@cleal.org> 3.6.0-1
-- new package built with tito

packages/foreman/nodejs-redux/redux-3.7.2.tgz
@@ -1,1 +1,0 @@
-../../../.git/annex/objects/ZJ/qX/SHA256E-s30775--986200b50836eedb92d92f52208b4948a5242b1d5fe5904bd4dea206a250335c.2.tgz/SHA256E-s30775--986200b50836eedb92d92f52208b4948a5242b1d5fe5904bd4dea206a250335c.2.tgz

commit 5f2cbeb3cdc5e1d0149481cdb2a77f8aa0261d11

Remove unused package nodejs-symbol-observable
Fixes: abb2a5a0f684 ("Remove unused package nodejs-redux")

comps/comps-foreman-el8.xml
@@ -75,7 +75,6 @@
       <packagereq type="default">nodejs-sass-loader</packagereq>
       <packagereq type="default">nodejs-sortabular</packagereq>
       <packagereq type="default">nodejs-style-loader</packagereq>
-      <packagereq type="default">nodejs-symbol-observable</packagereq>
       <packagereq type="default">nodejs-table-resolver</packagereq>
       <packagereq type="default">nodejs-theforeman-builder</packagereq>
       <packagereq type="default">nodejs-theforeman-vendor</packagereq>

package_manifest.yaml
@@ -374,7 +374,6 @@ foreman_nodejs_packages:
     nodejs-sortabular: {}
     nodejs-style-loader:
       strategy: bundle
-    nodejs-symbol-observable: {}
     nodejs-table-resolver: {}
     nodejs-theforeman-builder:
       name: "@theforeman/builder"

packages/foreman/nodejs-symbol-observable/nodejs-symbol-observable.spec
@@ -1,60 +1,0 @@
-%{?scl:%scl_package nodejs-%{npm_name}}
-%{!?scl:%global pkg_name %{name}}
-
-%global npm_name symbol-observable
-
-Name: %{?scl_prefix}nodejs-symbol-observable
-Version: 1.1.0
-Release: 4%{?dist}
-Summary: Symbol
-License: MIT
-Group: Development/Libraries
-URL: https://github.com/blesh/symbol-observable#readme
-Source0: https://registry.npmjs.org/%{npm_name}/-/%{npm_name}-%{version}.tgz
-%if 0%{?scl:1}
-BuildRequires: %{?scl_prefix_nodejs}npm
-%else
-BuildRequires: nodejs-packaging
-BuildRequires: npm
-%endif
-BuildArch: noarch
-ExclusiveArch: %{nodejs_arches} noarch
-Provides: %{?scl_prefix}npm(%{npm_name}) = %{version}
-
-%description
-%{summary}
-
-%prep
-%setup -q -n package
-
-%install
-mkdir -p %{buildroot}%{nodejs_sitelib}/%{npm_name}
-cp -pfr es %{buildroot}%{nodejs_sitelib}/%{npm_name}
-cp -pfr index.d.ts %{buildroot}%{nodejs_sitelib}/%{npm_name}
-cp -pfr index.js %{buildroot}%{nodejs_sitelib}/%{npm_name}
-cp -pfr lib %{buildroot}%{nodejs_sitelib}/%{npm_name}
-cp -pfr package.json %{buildroot}%{nodejs_sitelib}/%{npm_name}
-
-%nodejs_symlink_deps
-
-%check
-%{nodejs_symlink_deps} --check
-
-%files
-%{nodejs_sitelib}/%{npm_name}
-%license license
-%doc CHANGELOG.md
-%doc readme.md
-
-%changelog
-* Tue Mar 17 2020 Zach Huntington-Meath <zhunting@redhat.com> - 1.1.0-4
-- Bump packages to build for el8
-
-* Tue Oct 22 2019 Eric D. Helms <ericdhelms@gmail.com> - 1.1.0-3
-- Build for SCL
-
-* Fri Oct 04 2019 Eric D. Helms <ericdhelms@gmail.com> - 1.1.0-2
-- Update specs to handle SCL
-
-* Tue Dec 19 2017 Daniel Lobato Garcia <me@daniellobato.me> 1.1.0-1
-- new package built with tito

packages/foreman/nodejs-symbol-observable/symbol-observable-1.1.0.tgz
@@ -1,1 +1,0 @@
-../../../.git/annex/objects/11/x2/SHA256E-s3511--8bf5c35f343ed894b22f74c145617fb1becd5321816a3c94767ba28102478284.0.tgz/SHA256E-s3511--8bf5c35f343ed894b22f74c145617fb1becd5321816a3c94767ba28102478284.0.tgz