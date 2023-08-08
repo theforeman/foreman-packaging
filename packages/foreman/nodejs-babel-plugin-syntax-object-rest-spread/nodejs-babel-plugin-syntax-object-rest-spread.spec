theforeman/foreman-packaging

commit 82583d24d2a22eb78f62d34dc8ee8162d80f0b8a

Remove unused package nodejs-babel-plugin-transform-object-rest-spread
Fixes: a0ecfae51e6735489f96af45219597d951da23d6

comps/comps-foreman-el8.xml
@@ -44,7 +44,6 @@
       <packagereq type="default">nodejs-babel-loader</packagereq>
       <packagereq type="default">nodejs-babel-plugin-syntax-object-rest-spread</packagereq>
       <packagereq type="default">nodejs-babel-plugin-transform-class-properties</packagereq>
-      <packagereq type="default">nodejs-babel-plugin-transform-object-rest-spread</packagereq>
       <packagereq type="default">nodejs-babel-polyfill</packagereq>
       <packagereq type="default">nodejs-babel-preset-env</packagereq>
       <packagereq type="default">nodejs-babel-preset-react</packagereq>

package_manifest.yaml
@@ -331,7 +331,6 @@ foreman_nodejs_packages:
     nodejs-babel-plugin-syntax-object-rest-spread: {}
     nodejs-babel-plugin-transform-class-properties:
       strategy: bundle
-    nodejs-babel-plugin-transform-object-rest-spread: {}
     nodejs-babel-polyfill:
       strategy: bundle
     nodejs-babel-preset-env:

packages/foreman/nodejs-babel-plugin-transform-object-rest-spread/babel-plugin-transform-object-rest-spread-6.26.0.tgz
@@ -1,1 +1,0 @@
-../../../.git/annex/objects/23/Vk/SHA256E-s3436--63026ba1f2bd5761141e18a1e5c73c428bb3d96d01d71483640fda6a620fa03b.0.tgz/SHA256E-s3436--63026ba1f2bd5761141e18a1e5c73c428bb3d96d01d71483640fda6a620fa03b.0.tgz

packages/foreman/nodejs-babel-plugin-transform-object-rest-spread/nodejs-babel-plugin-transform-object-rest-spread.spec
@@ -1,65 +1,0 @@
-%{?scl:%scl_package nodejs-%{npm_name}}
-%{!?scl:%global pkg_name %{name}}
-
-%global npm_name babel-plugin-transform-object-rest-spread
-
-Name: %{?scl_prefix}nodejs-babel-plugin-transform-object-rest-spread
-Version: 6.26.0
-Release: 5%{?dist}
-Summary: Compile object rest and spread to ES5
-License: MIT
-Group: Development/Libraries
-URL: https://github.com/babel/babel/tree/master/packages/babel-plugin-transform-object-rest-spread
-Source0: https://registry.npmjs.org/%{npm_name}/-/%{npm_name}-%{version}.tgz
-%if 0%{?scl:1}
-BuildRequires: %{?scl_prefix_nodejs}npm
-%else
-BuildRequires: nodejs-packaging
-BuildRequires: npm
-%endif
-Requires: %{?scl_prefix}npm(babel-plugin-syntax-object-rest-spread) >= 6.8.0
-Requires: %{?scl_prefix}npm(babel-plugin-syntax-object-rest-spread) < 7.0.0
-Requires: %{?scl_prefix}npm(babel-runtime) >= 6.26.0
-Requires: %{?scl_prefix}npm(babel-runtime) < 7.0.0
-BuildArch: noarch
-ExclusiveArch: %{nodejs_arches} noarch
-Provides: %{?scl_prefix}npm(%{npm_name}) = %{version}
-
-%description
-%{summary}
-
-%prep
-%setup -q -n package
-
-%install
-mkdir -p %{buildroot}%{nodejs_sitelib}/%{npm_name}
-cp -pfr lib %{buildroot}%{nodejs_sitelib}/%{npm_name}
-cp -pfr package.json %{buildroot}%{nodejs_sitelib}/%{npm_name}
-
-%nodejs_symlink_deps
-
-%check
-%{nodejs_symlink_deps} --check
-
-%files
-%{nodejs_sitelib}/%{npm_name}
-%doc README.md
-
-%changelog
-* Tue Mar 17 2020 Zach Huntington-Meath <zhunting@redhat.com> - 6.26.0-5
-- Bump packages to build for el8
-
-* Mon Oct 21 2019 Eric D. Helms <ericdhelms@gmail.com> - 6.26.0-4
-- Build for SCL
-
-* Thu Oct 10 2019 Eric D. Helms <ericdhelms@gmail.com> - 6.26.0-3
-- Update requires for SCL prefix
-
-* Fri Oct 04 2019 Eric D. Helms <ericdhelms@gmail.com> - 6.26.0-2
-- Update specs to handle SCL
-
-* Mon May 28 2018 Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl> 6.26.0-1
-- Update to 6.26.0
-
-* Fri Oct 14 2016 Dominic Cleal <dominic@cleal.org> 6.16.0-1
-- new package built with tito

commit 0641c6586f5041618491c38b61dedad66130e180

Remove unused package nodejs-babel-plugin-syntax-object-rest-spread
Fixes: 82583d24d2a2 ("Remove unused package nodejs-babel-plugin-transform-object-rest-spread")

comps/comps-foreman-el8.xml
@@ -42,7 +42,6 @@
       <packagereq type="default">nodejs-argv-parse</packagereq>
       <packagereq type="default">nodejs-babel-core</packagereq>
       <packagereq type="default">nodejs-babel-loader</packagereq>
-      <packagereq type="default">nodejs-babel-plugin-syntax-object-rest-spread</packagereq>
       <packagereq type="default">nodejs-babel-plugin-transform-class-properties</packagereq>
       <packagereq type="default">nodejs-babel-preset-env</packagereq>
       <packagereq type="default">nodejs-babel-preset-react</packagereq>

package_manifest.yaml
@@ -327,7 +327,6 @@ foreman_nodejs_packages:
       strategy: bundle
     nodejs-babel-loader:
       strategy: bundle
-    nodejs-babel-plugin-syntax-object-rest-spread: {}
     nodejs-babel-plugin-transform-class-properties:
       strategy: bundle
     nodejs-babel-preset-env:

packages/foreman/nodejs-babel-plugin-syntax-object-rest-spread/babel-plugin-syntax-object-rest-spread-6.13.0.tgz
@@ -1,1 +1,0 @@
-../../../.git/annex/objects/F6/6k/SHA256E-s746--5c3ba107b2374d7438a7d7306b67e6183cfb5d50ddafd03ecb74a286d3580856.0.tgz/SHA256E-s746--5c3ba107b2374d7438a7d7306b67e6183cfb5d50ddafd03ecb74a286d3580856.0.tgz

packages/foreman/nodejs-babel-plugin-syntax-object-rest-spread/nodejs-babel-plugin-syntax-object-rest-spread.spec
@@ -1,55 +1,0 @@
-%{?scl:%scl_package nodejs-%{npm_name}}
-%{!?scl:%global pkg_name %{name}}
-
-%global npm_name babel-plugin-syntax-object-rest-spread
-
-Name: %{?scl_prefix}nodejs-babel-plugin-syntax-object-rest-spread
-Version: 6.13.0
-Release: 4%{?dist}
-Summary: Allow parsing of object rest/spread
-License: MIT
-Group: Development/Libraries
-URL: https://github.com/babel/babel/tree/master/packages/babel-plugin-syntax-object-rest-spread
-Source0: https://registry.npmjs.org/%{npm_name}/-/%{npm_name}-%{version}.tgz
-%if 0%{?scl:1}
-BuildRequires: %{?scl_prefix_nodejs}npm
-%else
-BuildRequires: nodejs-packaging
-BuildRequires: npm
-%endif
-BuildArch: noarch
-ExclusiveArch: %{nodejs_arches} noarch
-Provides: %{?scl_prefix}npm(%{npm_name}) = %{version}
-
-%description
-%{summary}
-
-%prep
-%setup -q -n package
-
-%install
-mkdir -p %{buildroot}%{nodejs_sitelib}/%{npm_name}
-cp -pfr lib %{buildroot}%{nodejs_sitelib}/%{npm_name}
-cp -pfr package.json %{buildroot}%{nodejs_sitelib}/%{npm_name}
-
-%nodejs_symlink_deps
-
-%check
-%{nodejs_symlink_deps} --check
-
-%files
-%{nodejs_sitelib}/%{npm_name}
-%doc README.md
-
-%changelog
-* Tue Mar 17 2020 Zach Huntington-Meath <zhunting@redhat.com> - 6.13.0-4
-- Bump packages to build for el8
-
-* Mon Oct 21 2019 Eric D. Helms <ericdhelms@gmail.com> - 6.13.0-3
-- Build for SCL
-
-* Fri Oct 04 2019 Eric D. Helms <ericdhelms@gmail.com> - 6.13.0-2
-- Update specs to handle SCL
-
-* Thu Apr 26 2018 Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl> 6.13.0-1
-- Add nodejs-babel-plugin-syntax-object-rest-spread generated by npm2rpm using the single strategy