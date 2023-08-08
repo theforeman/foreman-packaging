theforeman/foreman-packaging

commit 573afc396b4661ca066529e2297b8193533cee7c

Remove unused package nodejs-warning
Fixes: b2ee5e7af99d6626e7ece6245e6241359ca1ccfb

comps/comps-foreman-el8.xml
@@ -86,7 +86,6 @@
       <packagereq type="default">nodejs-url-loader</packagereq>
       <packagereq type="default">nodejs-use-deep-compare-effect</packagereq>
       <packagereq type="default">nodejs-uuid</packagereq>
-      <packagereq type="default">nodejs-warning</packagereq>
       <packagereq type="default">nodejs-webpack</packagereq>
       <packagereq type="default">nodejs-webpack-stats-plugin</packagereq>
       <packagereq type="default">pcp-mmvstatsd</packagereq>

package_manifest.yaml
@@ -390,7 +390,6 @@ foreman_nodejs_packages:
       strategy: bundle
     nodejs-use-deep-compare-effect: {}
     nodejs-uuid: {}
-    nodejs-warning: {}
     nodejs-webpack-stats-plugin: {}
     nodejs-webpack:
       strategy: bundle

packages/foreman/nodejs-warning/nodejs-warning.spec
@@ -1,59 +1,0 @@
-%{?scl:%scl_package nodejs-%{npm_name}}
-%{!?scl:%global pkg_name %{name}}
-
-%global npm_name warning
-
-Name: %{?scl_prefix}nodejs-warning
-Version: 4.0.2
-Release: 4%{?dist}
-Summary: A mirror of Facebook's Warning
-License: MIT
-Group: Development/Libraries
-URL: https://github.com/BerkeleyTrue/warning
-Source0: https://registry.npmjs.org/%{npm_name}/-/%{npm_name}-%{version}.tgz
-%if 0%{?scl:1}
-BuildRequires: %{?scl_prefix_nodejs}npm
-%else
-BuildRequires: nodejs-packaging
-BuildRequires: npm
-%endif
-Requires: %{?scl_prefix}npm(loose-envify) >= 1.0.0
-Requires: %{?scl_prefix}npm(loose-envify) < 2.0.0
-BuildArch: noarch
-ExclusiveArch: %{nodejs_arches} noarch
-Provides: %{?scl_prefix}npm(%{npm_name}) = %{version}
-
-%description
-%{summary}
-
-%prep
-%setup -q -n package
-
-%install
-mkdir -p %{buildroot}%{nodejs_sitelib}/%{npm_name}
-cp -pfr package.json %{buildroot}%{nodejs_sitelib}/%{npm_name}
-cp -pfr warning.js %{buildroot}%{nodejs_sitelib}/%{npm_name}
-
-%nodejs_symlink_deps
-
-%check
-%{nodejs_symlink_deps} --check
-
-%files
-%{nodejs_sitelib}/%{npm_name}
-%license LICENSE.md
-%doc CHANGELOG.md
-%doc README.md
-
-%changelog
-* Tue Mar 17 2020 Zach Huntington-Meath <zhunting@redhat.com> - 4.0.2-4
-- Bump packages to build for el8
-
-* Tue Oct 22 2019 Eric D. Helms <ericdhelms@gmail.com> - 4.0.2-3
-- Build for SCL
-
-* Fri Oct 04 2019 Eric D. Helms <ericdhelms@gmail.com> - 4.0.2-2
-- Update specs to handle SCL
-
-* Wed Oct 10 2018 Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl> 4.0.2-1
-- Add nodejs-warning generated by npm2rpm using the single strategy

packages/foreman/nodejs-warning/warning-4.0.2.tgz
@@ -1,1 +1,0 @@
-../../../.git/annex/objects/wp/XP/SHA256E-s3681--367b711d7372f52d5148fab7c0658e881a04d735225c3650aaf10f4a2cf4b401.2.tgz/SHA256E-s3681--367b711d7372f52d5148fab7c0658e881a04d735225c3650aaf10f4a2cf4b401.2.tgz

commit c5768543726ba635e9402df3538d9c5c39fa8866

Remove unused package nodejs-loose-envify
Fixes: 573afc396b46 ("Remove unused package nodejs-warning")

comps/comps-foreman-el8.xml
@@ -64,7 +64,6 @@
       <packagereq type="default">nodejs-jquery</packagereq>
       <packagereq type="default">nodejs-js-tokens</packagereq>
       <packagereq type="default">nodejs-lodash</packagereq>
-      <packagereq type="default">nodejs-loose-envify</packagereq>
       <packagereq type="default">nodejs-node-gyp</packagereq>
       <packagereq type="default">nodejs-node-sass</packagereq>
       <packagereq type="default">nodejs-optimize-css-assets-webpack-plugin</packagereq>

package_manifest.yaml
@@ -357,7 +357,6 @@ foreman_nodejs_packages:
     nodejs-jquery: {}
     nodejs-js-tokens: {}
     nodejs-lodash: {}
-    nodejs-loose-envify: {}
     nodejs-node-gyp:
       strategy: bundle
     nodejs-node-sass:

packages/foreman/nodejs-loose-envify/loose-envify-1.3.1.tgz
@@ -1,1 +1,0 @@
-../../../.git/annex/objects/MJ/8f/SHA256E-s2932--fb526ac195ab33e34c3a5fc5a4f68ae865de3310209191c2f5ab56d9631ce088.1.tgz/SHA256E-s2932--fb526ac195ab33e34c3a5fc5a4f68ae865de3310209191c2f5ab56d9631ce088.1.tgz

packages/foreman/nodejs-loose-envify/nodejs-loose-envify.spec
@@ -1,67 +1,0 @@
-%{?scl:%scl_package nodejs-%{npm_name}}
-%{!?scl:%global pkg_name %{name}}
-
-%global npm_name loose-envify
-
-Name: %{?scl_prefix}nodejs-loose-envify
-Version: 1.3.1
-Release: 4%{?dist}
-Summary: Fast (and loose) selective `process
-License: MIT
-Group: Development/Libraries
-URL: https://github.com/zertosh/loose-envify
-Source0: https://registry.npmjs.org/%{npm_name}/-/%{npm_name}-%{version}.tgz
-%if 0%{?scl:1}
-BuildRequires: %{?scl_prefix_nodejs}npm
-%else
-BuildRequires: nodejs-packaging
-BuildRequires: npm
-%endif
-Requires: %{?scl_prefix}npm(js-tokens) >= 3.0.0
-Requires: %{?scl_prefix}npm(js-tokens) < 4.0.0
-BuildArch: noarch
-ExclusiveArch: %{nodejs_arches} noarch
-Provides: %{?scl_prefix}npm(%{npm_name}) = %{version}
-
-%description
-%{summary}
-
-%prep
-%setup -q -n package
-
-%install
-mkdir -p %{buildroot}%{nodejs_sitelib}/%{npm_name}
-cp -pfr cli.js %{buildroot}%{nodejs_sitelib}/%{npm_name}
-cp -pfr custom.js %{buildroot}%{nodejs_sitelib}/%{npm_name}
-cp -pfr index.js %{buildroot}%{nodejs_sitelib}/%{npm_name}
-cp -pfr loose-envify.js %{buildroot}%{nodejs_sitelib}/%{npm_name}
-cp -pfr package.json %{buildroot}%{nodejs_sitelib}/%{npm_name}
-cp -pfr replace.js %{buildroot}%{nodejs_sitelib}/%{npm_name}
-
-mkdir -p %{buildroot}%{_bindir}
-chmod 0755 %{buildroot}%{nodejs_sitelib}/%{npm_name}/cli.js
-ln -sf %{nodejs_sitelib}/%{npm_name}/cli.js %{buildroot}%{_bindir}/loose-envify
-
-%nodejs_symlink_deps
-
-%check
-%{nodejs_symlink_deps} --check
-
-%files
-%{nodejs_sitelib}/%{npm_name}
-%{_bindir}/loose-envify
-%license LICENSE
-%doc README.md
-
-%changelog
-* Tue Mar 17 2020 Zach Huntington-Meath <zhunting@redhat.com> - 1.3.1-4
-- Bump packages to build for el8
-
-* Tue Oct 22 2019 Eric D. Helms <ericdhelms@gmail.com> - 1.3.1-3
-- Build for SCL
-
-* Fri Oct 04 2019 Eric D. Helms <ericdhelms@gmail.com> - 1.3.1-2
-- Update specs to handle SCL
-
-* Wed Oct 18 2017 Eric D. Helms <ericdhelms@gmail.com> 1.3.1-1
-- new package built with tito