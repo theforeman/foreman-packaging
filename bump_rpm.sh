theforeman/foreman-packaging

commit 52ea3509d11ff1e3709578951940efd6c9353b1d

Fix determining latest version in bump_rpm.sh
While refactoring I suggested to determine GEM_NAME lazily, but this
broke.

Fixes: 3d4a05733316 ("Handle bumping nodejs RPMs")

bump_rpm.sh
@@ -42,6 +42,7 @@ if [[ -z $2 ]] ; then
 	if [[ $PACKAGE_NAME == *rubygem-* ]]; then
 		ensure_program curl
 		ensure_program jq
+		GEM_NAME=$(awk '/^%global\s+gem_name/ { print $3 }' $SPEC_FILE)
 		NEW_VERSION=$(curl -s https://rubygems.org/api/v1/versions/${GEM_NAME}/latest.json | jq -r .version)
 	else
 		echo "Unknown package type for $1; a version must be specified"

commit ab125932a118d51e60aca658c5f1ba2b29856a95

Use an absolute path to the spec file
The line was copied from a place where there was a cd "$1" in place.

Fixes: 52ea3509d11ff1e3709578951940efd6c9353b1d ("Fix determining latest version in bump_rpm.sh")

bump_rpm.sh
@@ -42,7 +42,7 @@ if [[ -z $2 ]] ; then
 	if [[ $PACKAGE_NAME == *rubygem-* ]]; then
 		ensure_program curl
 		ensure_program jq
-		GEM_NAME=$(awk '/^%global\s+gem_name/ { print $3 }' $SPEC_FILE)
+		GEM_NAME=$(awk '/^%global\s+gem_name/ { print $3 }' "$1"/$SPEC_FILE)
 		NEW_VERSION=$(curl -s "https://rubygems.org/api/v1/versions/${GEM_NAME}/latest.json" | jq -r .version)
 	else
 		echo "Unknown package type for $1; a version must be specified"