theforeman/foreman-packaging

commit dbb95dc2c110110eba29151e31aabc8d7b3ceb66

Update puppet-foreman_scap_client to 1.0.0
This updates the dependencies to also include puppet-agent since
puppet-agent 8.0.0 it no longer Provides: puppet.

packages/plugins/puppet-foreman_scap_client/puppet-foreman_scap_client.spec
@@ -1,19 +1,17 @@
 %global puppet_module foreman_scap_client
 %global puppet_full_name theforeman-%{puppet_module}
-
-%global puppet_modules_dir %{_datadir}/puppet/modules
-%global puppet_foreman_scap_client_dir %{puppet_modules_dir}/%{puppet_module}
+%global puppet_foreman_scap_client_dir %{_datadir}/puppet/modules/%{puppet_module}
 
 Name:       puppet-%{puppet_module}
-Version:    0.4.0
+Version:    1.0.0
 Release:    1%{?dist}
 Summary:    Puppet module to configure foreman_scap_client
 License:    GPLv2
 URL:        https://github.com/theforeman/%{name}
 Source0:    https://forgeapi.puppetlabs.com/v3/files/%{puppet_full_name}-%{version}.tar.gz
 BuildArch:  noarch
-Requires:   puppet >= 2.7.0
-Requires:   puppetlabs-stdlib >= 4.2.0
+Requires:   (puppet-agent >= 7 or puppet >= 7)
+Requires:   puppetlabs-stdlib >= 4.25.0
 
 %description
 Foreman SCAP client Puppet Module configures the client of the same name
@@ -30,24 +28,21 @@ cp -rp . %{buildroot}/%{puppet_foreman_scap_client_dir}/
 
 
 %files
-%doc NEWS COPYING README.md
-%dir %{puppet_modules_dir}/%{puppet_module}
-%{puppet_foreman_scap_client_dir}/COPYING
-%{puppet_foreman_scap_client_dir}/README.md
-%{puppet_foreman_scap_client_dir}/NEWS
+%doc NEWS README.md REFERENCE.md
+%dir %{puppet_foreman_scap_client_dir}
+%license %{puppet_foreman_scap_client_dir}/COPYING
+%exclude %{puppet_foreman_scap_client_dir}/NEWS
+%exclude %{puppet_foreman_scap_client_dir}/README.md
+%exclude %{puppet_foreman_scap_client_dir}/REFERENCE.md
 %{puppet_foreman_scap_client_dir}/metadata.json
-%{puppet_foreman_scap_client_dir}/Gemfile
-%{puppet_foreman_scap_client_dir}/Rakefile
 %{puppet_foreman_scap_client_dir}/lib
 %{puppet_foreman_scap_client_dir}/manifests
 %{puppet_foreman_scap_client_dir}/templates
-%exclude %{puppet_foreman_scap_client_dir}/spec
-%exclude %{puppet_foreman_scap_client_dir}/.travis.yml
-%exclude %{puppet_foreman_scap_client_dir}/.fixtures.yml
-%exclude %{puppet_foreman_scap_client_dir}/.git
-%exclude %{puppet_foreman_scap_client_dir}/.gitignore
 
 %changelog
+* Wed Nov 08 2023 Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl> - 1.0.0-1
+- Update to 1.0.0
+
 * Mon Mar 02 2020 Ondrej Prazak <oprazak@redhat.com> 0.4.0-1
 - Update to 0.4.0
 

packages/plugins/puppet-foreman_scap_client/theforeman-foreman_scap_client-0.4.0.tar.gz
@@ -1,1 +1,0 @@
-../../../.git/annex/objects/Jz/0J/SHA256E-s163049--f390013bba27339cadf6b8bd48e88dcc17f257d59bc3342353a4f805dc214770.tar.gz/SHA256E-s163049--f390013bba27339cadf6b8bd48e88dcc17f257d59bc3342353a4f805dc214770.tar.gz

packages/plugins/puppet-foreman_scap_client/theforeman-foreman_scap_client-1.0.0.tar.gz
@@ -1,0 +1,1 @@
+../../../.git/annex/objects/Gz/Pf/SHA256E-s13148--73e3c80ecdbce35a6089ae7a2ea629f750ffbabf22f68e9f382102624890b9cf.tar.gz/SHA256E-s13148--73e3c80ecdbce35a6089ae7a2ea629f750ffbabf22f68e9f382102624890b9cf.tar.gz

commit 69270068ffb91b807317c4c74c2778c1e4394b04

Correctly identify license file for puppet-foreman_scap_client
Fixes: dbb95dc2c110 ("Update puppet-foreman_scap_client to 1.0.0")

packages/plugins/puppet-foreman_scap_client/puppet-foreman_scap_client.spec
@@ -4,7 +4,7 @@
 
 Name:       puppet-%{puppet_module}
 Version:    1.0.0
-Release:    1%{?dist}
+Release:    2%{?dist}
 Summary:    Puppet module to configure foreman_scap_client
 License:    GPLv2
 URL:        https://github.com/theforeman/%{name}
@@ -29,8 +29,9 @@ cp -rp . %{buildroot}/%{puppet_foreman_scap_client_dir}/
 
 %files
 %doc NEWS README.md REFERENCE.md
+%license COPYING
 %dir %{puppet_foreman_scap_client_dir}
-%license %{puppet_foreman_scap_client_dir}/COPYING
+%exclude %{puppet_foreman_scap_client_dir}/COPYING
 %exclude %{puppet_foreman_scap_client_dir}/NEWS
 %exclude %{puppet_foreman_scap_client_dir}/README.md
 %exclude %{puppet_foreman_scap_client_dir}/REFERENCE.md
@@ -40,6 +41,9 @@ cp -rp . %{buildroot}/%{puppet_foreman_scap_client_dir}/
 %{puppet_foreman_scap_client_dir}/templates
 
 %changelog
+* Wed Nov 08 2023 Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl> - 1.0.0-2
+- Correctly identify license file
+
 * Wed Nov 08 2023 Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl> - 1.0.0-1
 - Update to 1.0.0
 