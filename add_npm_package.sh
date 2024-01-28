theforeman/foreman-packaging

commit e539c7b7518b01ae862891d8c29e6163b6b7707b

correctly find existing katello packages w/o passing base_dir
bump_rpm.sh doesn't pass a base_dir, so let's find the package on our
own instead of trying to add a new package to the foreman dir, while it
really can be updated in the katello dir

add_npm_package.sh
@@ -17,8 +17,12 @@ if [[ $NPM_MODULE_NAME == */* ]]; then
 else
   PACKAGE_NAME=nodejs-${PACKAGE_MODULE}
 fi
-PACKAGE_DIR=packages/$BASE_DIR/$PACKAGE_NAME
-SPEC_FILE="${PACKAGE_DIR}/${PACKAGE_NAME}.spec"
+
+SPEC_FILE=$(compgen -G "packages/*/${PACKAGE_NAME}/${PACKAGE_NAME}.spec")
+if [[ -z "${SPEC_FILE}" ]]; then
+  PACKAGE_DIR=packages/$BASE_DIR/$PACKAGE_NAME
+  SPEC_FILE="${PACKAGE_DIR}/${PACKAGE_NAME}.spec"
+fi
 
 ROOT=$(git rev-parse --show-toplevel)
 

commit bea643d234d2bcd6db4ea7bcccaac6c5bde4110d

find the package dir, not the spec file
we need both

Fixes: e539c7b7518b01ae862891d8c29e6163b6b7707b

add_npm_package.sh
@@ -18,11 +18,11 @@ else
   PACKAGE_NAME=nodejs-${PACKAGE_MODULE}
 fi
 
-SPEC_FILE=$(compgen -G "packages/*/${PACKAGE_NAME}/${PACKAGE_NAME}.spec")
-if [[ -z "${SPEC_FILE}" ]]; then
+PACKAGE_DIR=$(compgen -G "packages/*/${PACKAGE_NAME}/")
+if [[ -z "${PACKAGE_DIR}" ]]; then
   PACKAGE_DIR=packages/$BASE_DIR/$PACKAGE_NAME
-  SPEC_FILE="${PACKAGE_DIR}/${PACKAGE_NAME}.spec"
 fi
+SPEC_FILE="${PACKAGE_DIR}/${PACKAGE_NAME}.spec"
 
 ROOT=$(git rev-parse --show-toplevel)
 