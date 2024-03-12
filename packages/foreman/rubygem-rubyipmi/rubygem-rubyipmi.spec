theforeman/foreman-packaging

commit 6cc3c98edd01fe2f24db8a426b9eed405c283200

Update rubygem-rubyipmi to 0.11.1

packages/foreman/rubygem-rubyipmi/rubygem-rubyipmi.spec
@@ -1,67 +1,43 @@
-# template: scl
-%{?scl:%scl_package rubygem-%{gem_name}}
-%{!?scl:%global pkg_name %{name}}
-
+# template: default
 %global gem_name rubyipmi
 
-Name: %{?scl_prefix}rubygem-%{gem_name}
-Version: 0.11.0
+Name: rubygem-%{gem_name}
+Version: 0.11.1
 Release: 1%{?dist}
 Summary: A ruby wrapper for ipmi command line tools that supports ipmitool and freeipmi
-Group: Development/Languages
-License: LGPLv2
+License: LGPLv2.1
 URL: https://github.com/logicminds/rubyipmi
 Source0: https://rubygems.org/gems/%{gem_name}-%{version}.gem
 
-Requires: ipmitool
-
 # start specfile generated dependencies
-Requires: %{?scl_prefix_ruby}ruby(release)
-Requires: %{?scl_prefix_ruby}ruby
-Requires: %{?scl_prefix_ruby}ruby(rubygems)
-Requires: %{?scl_prefix}rubygem(highline)
-BuildRequires: %{?scl_prefix_ruby}ruby(release)
-BuildRequires: %{?scl_prefix_ruby}ruby
-BuildRequires: %{?scl_prefix_ruby}rubygems-devel
+Requires: ruby
+BuildRequires: ruby
+BuildRequires: rubygems-devel
 BuildArch: noarch
-Provides: %{?scl_prefix}rubygem(%{gem_name}) = %{version}
 # end specfile generated dependencies
 
 %description
-Provides a library for controlling IPMI devices using pure ruby code.
+Controls IPMI devices via command line wrapper for ipmitool and freeipmi.
 
 
 %package doc
-Summary: Documentation for %{pkg_name}
-Group: Documentation
-Requires: %{?scl_prefix}%{pkg_name} = %{version}-%{release}
+Summary: Documentation for %{name}
+Requires: %{name} = %{version}-%{release}
 BuildArch: noarch
 
 %description doc
-Documentation for %{pkg_name}.
+Documentation for %{name}.
 
 %prep
-%{?scl:scl enable %{scl} - << \EOF}
-gem unpack %{SOURCE0}
-%{?scl:EOF}
-
-%setup -q -D -T -n  %{gem_name}-%{version}
-
-%{?scl:scl enable %{scl} - << \EOF}
-gem spec %{SOURCE0} -l --ruby > %{gem_name}.gemspec
-%{?scl:EOF}
+%setup -q -n  %{gem_name}-%{version}
 
 %build
 # Create the gem as gem install only works on a gem file
-%{?scl:scl enable %{scl} - << \EOF}
-gem build %{gem_name}.gemspec
-%{?scl:EOF}
+gem build ../%{gem_name}-%{version}.gemspec
 
 # %%gem_install compiles any C extensions and installs the gem into ./%%gem_dir
 # by default, so that we can move it into the buildroot in %%install
-%{?scl:scl enable %{scl} - << \EOF}
 %gem_install
-%{?scl:EOF}
 
 %install
 mkdir -p %{buildroot}%{gem_dir}
@@ -75,11 +51,6 @@ cp -a .%{gem_dir}/* \
 %{gem_libdir}
 %exclude %{gem_cache}
 %{gem_spec}
-%exclude %{gem_instdir}/.document
-%exclude %{gem_instdir}/.gitignore
-%exclude %{gem_instdir}/.rspec
-%exclude %{gem_instdir}/.rubocop.yml
-%exclude %{gem_instdir}/.travis.yml
 
 %files doc
 %doc %{gem_docdir}
@@ -87,9 +58,12 @@ cp -a .%{gem_dir}/* \
 %doc %{gem_instdir}/README.md
 %doc %{gem_instdir}/RELEASE_NOTES.md
 %{gem_instdir}/Rakefile
-%{gem_instdir}/rubyipmi.gemspec
+%exclude %{gem_instdir}/rubyipmi.gemspec
 
 %changelog
+* Tue Aug 16 2022 Foreman Packaging Automation <packaging@theforeman.org> 0.11.1-1
+- Update to 0.11.1
+
 * Mon Oct 04 2021 Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl> 0.11.0-1
 - Update to 0.11.0
 

packages/foreman/rubygem-rubyipmi/rubyipmi-0.11.0.gem
@@ -1,1 +1,0 @@
-../../../.git/annex/objects/FZ/1f/SHA256E-s36864--6b7f3217a5b1935621c2ea327f423c833d1bd943895355ab83c367ea8305bbe7.0.gem/SHA256E-s36864--6b7f3217a5b1935621c2ea327f423c833d1bd943895355ab83c367ea8305bbe7.0.gem

packages/foreman/rubygem-rubyipmi/rubyipmi-0.11.1.gem
@@ -1,0 +1,1 @@
+../../../.git/annex/objects/PW/w0/SHA256E-s35840--7469b5d02aee71bf304c25ba04d4ab35f6c7a88894a30663a0a3ffe5a51b7096.1.gem/SHA256E-s35840--7469b5d02aee71bf304c25ba04d4ab35f6c7a88894a30663a0a3ffe5a51b7096.1.gem

commit f0f6d2c498a223469467c1a68f513fa1c767e567

Fixes #37246 - Add back ipmitool dependency
During a rewrite of the spec file this was dropped.

Fixes: 6cc3c98edd01 ("Update rubygem-rubyipmi to 0.11.1")

packages/foreman/rubygem-rubyipmi/rubygem-rubyipmi.spec
@@ -3,7 +3,7 @@
 
 Name: rubygem-%{gem_name}
 Version: 0.11.1
-Release: 1%{?dist}
+Release: 2%{?dist}
 Summary: A ruby wrapper for ipmi command line tools that supports ipmitool and freeipmi
 License: LGPLv2.1
 URL: https://github.com/logicminds/rubyipmi
@@ -15,6 +15,8 @@ BuildRequires: ruby
 BuildRequires: rubygems-devel
 BuildArch: noarch
 # end specfile generated dependencies
+
+Requires: ipmitool
 
 %description
 Controls IPMI devices via command line wrapper for ipmitool and freeipmi.
@@ -61,6 +63,9 @@ cp -a .%{gem_dir}/* \
 %exclude %{gem_instdir}/rubyipmi.gemspec
 
 %changelog
+* Tue Mar 12 2024 Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl> - 0.11.1-2
+- Add back ipmitool dependency
+
 * Tue Aug 16 2022 Foreman Packaging Automation <packaging@theforeman.org> 0.11.1-1
 - Update to 0.11.1
 