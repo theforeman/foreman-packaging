theforeman/foreman-packaging

commit fdf2c00e9009024960a8f115f6f69d66e62a5cd1

Update katello-repos to use Pulpcore nightly

packages/katello/katello-repos/katello-repos.spec
@@ -1,4 +1,4 @@
-%global pulpcore_version 3.39
+%global pulpcore_version nightly
 %global candlepin_version 4.3
 
 %define repo_dir %{_sysconfdir}/yum.repos.d
@@ -6,7 +6,7 @@
 
 %global prereleasesource nightly
 %global prerelease %{?prereleasesource:.}%{?prereleasesource}
-%global release 1
+%global release 2
 
 Name:           katello-repos
 Version:        4.13
@@ -73,6 +73,9 @@ rm -rf %{buildroot}
 %{_sysconfdir}/pki/rpm-gpg/RPM-GPG-KEY-candlepin
 
 %changelog
+* Wed Apr 17 2024 Samir Jha <samirjha1525@gmail.com> - 4.13-0.2.nightly
+- Update pulpcore version to nightly
+
 * Mon Feb 26 2024 Quinn James <qjames@redhat.com> - 4.13-0.1.nightly
 - Bump version to 4.13.0
 

commit fe535569377e172236d57b9d324991df58c38b4a

Disable gpgcheck when using pulpcore nightly
Fixes: fdf2c00e9009024960a8f115f6f69d66e62a5cd1

packages/katello/katello-repos/katello-repos.spec
@@ -6,7 +6,7 @@
 
 %global prereleasesource nightly
 %global prerelease %{?prereleasesource:.}%{?prereleasesource}
-%global release 3
+%global release 4
 
 Name:           katello-repos
 Version:        4.13
@@ -73,6 +73,9 @@ rm -rf %{buildroot}
 %{_sysconfdir}/pki/rpm-gpg/RPM-GPG-KEY-candlepin
 
 %changelog
+* Fri May 03 2024 Evgeni Golov - 4.13-0.4.nightly
+- Disable gpgcheck when using pulpcore nightly
+
 * Wed May 01 2024 Eric D. Helms <ericdhelms@gmail.com> - 4.13-0.3.nightly
 - Use Candlepin 4.4
 

packages/katello/katello-repos/katello.repo
@@ -22,7 +22,7 @@ name=pulpcore: Fetch, Upload, Organize, and Distribute Software Packages.
 baseurl=https://yum.theforeman.org/pulpcore/@PULPCORE_VERSION@/@DIST@/$basearch/
 gpgkey=https://yum.theforeman.org/pulpcore/@PULPCORE_VERSION@/GPG-RPM-KEY-pulpcore
 enabled=1
-gpgcheck=1
+gpgcheck=@REPO_GPGCHECK@
 
 # source repositories
 
@@ -45,4 +45,4 @@ name=pulpcore source
 baseurl=https://yum.theforeman.org/pulpcore/@PULPCORE_VERSION@/@DIST@/source/
 gpgkey=https://yum.theforeman.org/pulpcore/@PULPCORE_VERSION@/GPG-RPM-KEY-pulpcore
 enabled=0
-gpgcheck=1
+gpgcheck=@REPO_GPGCHECK@