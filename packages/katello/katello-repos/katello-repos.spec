theforeman/foreman-packaging

commit 40608e826a9efd9bc1535b37aab90131c60bdef2

Use Candlepin 4.4

packages/katello/katello-repos/katello-repos.spec
@@ -1,12 +1,12 @@
 %global pulpcore_version nightly
-%global candlepin_version 4.3
+%global candlepin_version 4.4
 
 %define repo_dir %{_sysconfdir}/yum.repos.d
 %define repo_dist %{dist}
 
 %global prereleasesource nightly
 %global prerelease %{?prereleasesource:.}%{?prereleasesource}
-%global release 2
+%global release 3
 
 Name:           katello-repos
 Version:        4.13
@@ -73,6 +73,9 @@ rm -rf %{buildroot}
 %{_sysconfdir}/pki/rpm-gpg/RPM-GPG-KEY-candlepin
 
 %changelog
+* Wed May 01 2024 Eric D. Helms <ericdhelms@gmail.com> - 4.13-0.3.nightly
+- Use Candlepin 4.4
+
 * Wed Apr 17 2024 Samir Jha <samirjha1525@gmail.com> - 4.13-0.2.nightly
 - Update pulpcore version to nightly
 

commit 0d22553b40557cb4f665a8a88d5deebae7ecff2d

Use Candlepin 4.4 key
Fixes: 40608e826a9efd9bc1535b37aab90131c60bdef2

packages/katello/katello-repos/candlepin.gpg
@@ -1,29 +1,29 @@
 -----BEGIN PGP PUBLIC KEY BLOCK-----
 
-mQINBGVp5x0BEADIV0NVLAtRF9paP7Et7m167KCmz4xt7YokbGTMiNXR0e9IEKZQ
-yHZ3ZhE2N2eyPjGdJw+fAbu1FFD5jKftjqqOilWEH8wJpZIubcsO4BpGbhMinKQr
-Dhd0QjEQkHxH1AOTZ4rclN/he4StGMnfyjJrnYLUlmaZ6MUly/hEkqZItXY42Az1
-nPxug51wnNUjAD/zwnJi0eStb7/H0LXQSdWCt4xGnrcurs6fBStsu1aOyZQRe2vz
-8Q5OV2mkkIH7qLoQExW4UZVVn2lkHSNNB7DvwDTHAnNX4PrwNiaI4EqJGez96Rcu
-FBZiXt+PF1iz1gcCuvMm/Ai+ZlUSUIOugqEPIgyrWjFxambhhk8Kp7GO9prohQd3
-uu6rdFEjU0U1jE2Q6RtNrsfzlzTsKWnJ3QgTwJzp83pGr4el4mQUzlblDKc8wgyE
-V/LeO9sUSWkq+gGL6mzL6RQd2YTzvbJ5hZsdlWisYnKJ8pxFYaalY92i+Og+Z5J0
-Ce+Jhsg2w+/E/1VmMEK6hbnVpXPbMPWkdWYldv3gBgdRfCZUVByVz9HbwZTg7xI+
-CpjGTt6j3C6PEE06mBuN3rtzJYtGYSfKheqCAg+bbAQ57eQQ96+SoEItKLJWgzXe
-cdRmqV4m14Hm0SIcOXmmcMEPP6cIPw4vjD9dRcu5Z6izGw+qq7tbaz2ZtQARAQAB
-tD9DYW5kbGVwaW4gQXV0b21hdGljIFNpZ25pbmcgS2V5ICg0LjMpIDxwYWNrYWdl
-c0B0aGVmb3JlbWFuLm9yZz6JAlcEEwEIAEEWIQRfV/tM8tVPcIo7wT7XTAu1oHs4
-zwUCZWnnHQIbLwUJAeEzgAULCQgHAgIiAgYVCgkICwIEFgIDAQIeBwIXgAAKCRDX
-TAu1oHs4z50ND/4kclkCjlNZRgeNBe0Zsc8652Y0xrEy+VFiF4NgYPOSIlh8YC+X
-a7wA2G7NPpz+sTpziUAKcfbOgxktTG/K3hw9SRRRCG8FsjJfAVh+bju9CHIl7G0x
-UcSV8zv0WInrWfAQON0B4pVW5RWddD+knSiGanngZJWuRHV9zPcQjLo9fpY2mtIQ
-OKV9hVVNWaQND5Yy9oz4lwQ/dJo13RQnj9lWE3DgpLeJZack/Z+lf7LJzIi+4MBH
-ERrYSi5z8RQW2e3P9V0SwpHgwz94/cmuM8ghkQLpM6Mjypl+3DCCKlaXl6LQuYEA
-z+XhJdXAM4bvK5dImcZJRhITy0iPMtkYfTZVOiPiraBgjFp9zyd7G2es/zxiPVSY
-MFtUZxV0rS6nma3L0bCFSSDWmWnP0sY8Y3UzYnJmufWvJihMmatIMzZf/vGq/qgy
-CQUNxfA4RoniL2wIBicNmO+YOLPqPfGOgt/pBK9lhSogg28fHEwxiQNNSg6Zfza8
-NEXN9zU+KxhGLn9kBG/NCivatOXmHSgdlB0R8G6XEGzMvg7HxCHRk4MmOhUvNRqq
-QrOBvP26B6Qz5ZAlWD5L35TFcQRK+cj6Yaj6qCtbgPRoOUisFY103WMXWq9UQ9JE
-gcpU/a3UFyjmgN4UqPUPtVgCQ53Z4JI0AU9tw+ZavkPF2Kq3AbRNt5uobg==
-=qpgn
+mQINBGYMPkIBEACzFhChv7jIPL3KE7fGchtNVm5xbJxY+OLjUzjmSrBMssQKxYYh
+U6dfLQGjye51YsJ/r+sYzW5tSQtTzYIF6vEkqiPZy850zqK2Jkl4J3iDJ5trBlSO
+F0mvl7f+2ceWpe0/9RdUzXvxbLHDzb11P1YSkwWsoPLpQbhydUqCHRWyVeMHkjIG
+0hOc2R1xtRLxn5cqNq58aU0gV/GDsWr43QQTi7k3MSKqgbRhlspKlu0sotOT8aOf
+qPNF7XeZ938WVCT1oR08K10ipandWzBOZtbXTWL81P4iMzOy50+oUqg6vKD8X4k1
+Wydu2tORM5B6B95UdwEuPgtDvX/ivz26m8yp3n8OnLHcGTeC4IWm5zZzz3xysKtf
+vVa3AL1F/lZMsDtJTu/88ZXiUxgtq5qYUo6auPVU0zCIsikRb7jo19QGcpKciyz1
+28Sb3msV8WGSOqRpITsxSWfM5273Pgb/31ZWGvYXcYVX1acWvApU188EJUG0uSDU
+5zYR69fL64SODoAo9nQQ7MBSWJaR2Gsan/KjoWfhv4iSWewbCm9mlRUKtLAOL8sT
+PbE/R8RdTmZQtb8YcyHhDk5S9HdMuBLyidbCYeYetvBHwo7VuhhX/Hompb3rwc0d
+mA7lhhkZwmYB7v2VNRr0SdpSdjLst29CC/l1SHIcSawxPSdFzBnkLgoalwARAQAB
+tD9DYW5kbGVwaW4gQXV0b21hdGljIFNpZ25pbmcgS2V5ICg0LjQpIDxwYWNrYWdl
+c0B0aGVmb3JlbWFuLm9yZz6JAlgEEwEIAEIWIQS5eiUtAc9n5WF8BdwMDLDNAIxe
+KgUCZgw+QgMbLwQFCQHhM4AFCwkIBwICIgIGFQoJCAsCBBYCAwECHgcCF4AACgkQ
+DAywzQCMXiqJ4Q//cfkvEjqgkUg302R1oi/vMCtAO8D8Rl1lwlF+FT/AY4YfIns4
+JgHUJIqKv/cudw6BxUVQiEDbUbHfOlNwEghMkAhh/r1J3aDFDoE2634fJmyMfYVY
++wNhCjj4AU0/5GElScWwtQz3Biv9T1HwcJjNvYw9TAgvbk1DiTKamg2QmVk9hg3S
+guS6b3pDXQxDsfUDs25s0ixcadQG5B6EILFOX59LK2OA0q/zIcOkNu0uf9j4+Sia
+BlobKY/Ud+EDxB0fF5/Sb0JtCA1qrfNxZZHNo2Jp9CacerFakGscNtpLNZxfACfy
+MsG7iIRW8NSjfkh5HCJ+N1RGKh5dzeBgPhFPHmEIAupxBm+SG2iiAp0r/Simyld6
+U7NAnpoHR5ePqSgN1cebQU4z9PzZDGUQbsvSsFWu1FUp42J+WVoSC2WmfExmTYqs
+2W1TFQ/XSNmqQA7kPjmo+lhvtpQq6QK6RLxIpiVhJPlktHb/gedpVIzsyBDe6FkJ
+DViJ9Dp+kp5nvmSmDWj8woLi7uIhM2+u/C4U0LsgLZg8lhBvqILmpcKeXDa5MTPE
+8SyCV9dKcymNaCTWcdyl7BS+pG2kkoq5rqCb3846idlGzZczq+sNDoMUDxSSyFi7
+qfW7NABQq2xgiCkfRj4SFP0KdgGs9xmKFYL/bftmUzpivKz2iMWU6+bbkto=
+=1c/J
 -----END PGP PUBLIC KEY BLOCK-----

packages/katello/katello-repos/katello-repos.spec
@@ -6,7 +6,7 @@
 
 %global prereleasesource nightly
 %global prerelease %{?prereleasesource:.}%{?prereleasesource}
-%global release 1
+%global release 2
 
 Name:           katello-repos
 Version:        4.14
@@ -73,6 +73,9 @@ rm -rf %{buildroot}
 %{_sysconfdir}/pki/rpm-gpg/RPM-GPG-KEY-candlepin
 
 %changelog
+* Mon Jun 03 2024 Evgeni Golov - 4.14-0.2.nightly
+- Update Candlepin 4.4 key
+
 * Tue May 28 2024 Ian Ballou <ianballou67@gmail.com> - 4.14-0.1.nightly
 - Bump version to 4.14.0
 