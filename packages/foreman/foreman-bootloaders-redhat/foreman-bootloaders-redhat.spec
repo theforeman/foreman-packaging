theforeman/foreman-packaging

commit f69b2386e9749024e7d02e5cff808b1c7abd6085

Only require shim-ia32 bootloader on RHEL 8 or less

packages/foreman/foreman-bootloaders-redhat/foreman-bootloaders-redhat.spec
@@ -1,6 +1,6 @@
 Name: foreman-bootloaders-redhat
 Version: 202102220000
-Release: 2%{?dist}
+Release: 3%{?dist}
 Summary: Metapackage with Grub2 and Shim TFTP bootloaders
 
 Group: Applications/System
@@ -15,10 +15,10 @@ Requires: /usr/bin/grub2-mkimage
 
 %if 0%{rhel} < 9
 Requires: grub2-efi-ia32-modules
+Requires: shim-ia32
 %endif
 Requires: grub2-efi-x64-modules
 Requires: grub2-pc-modules
-Requires: shim-ia32
 Requires: shim-x64
 
 %description
@@ -73,6 +73,9 @@ install -Dp -m0755 %{SOURCE0} %{buildroot}%{_bindir}/foreman-generate-bootloader
 
 
 %changelog
+* Sat Mar 23 2024 Eric D. Helms <ericdhelms@gmail.com> 202102220000-3
+- Only requires shim-ia32 on RHEL 8 or less
+
 * Wed Jan 03 2024 Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl> 202102220000-2
 - EL9 compatibility
 

commit 6bbd515f16a41ff93b7615a079212c0aa8794e33

Don't fail when shim-ia32 is missing when generating bootloaders
Fixes: f69b2386e9749024e7d02e5cff808b1c7abd6085

packages/foreman/foreman-bootloaders-redhat/foreman-bootloaders-redhat.spec
@@ -1,6 +1,6 @@
 Name: foreman-bootloaders-redhat
 Version: 202102220000
-Release: 3%{?dist}
+Release: 4%{?dist}
 Summary: Metapackage with Grub2 and Shim TFTP bootloaders
 
 Group: Applications/System
@@ -73,6 +73,9 @@ install -Dp -m0755 %{SOURCE0} %{buildroot}%{_bindir}/foreman-generate-bootloader
 
 
 %changelog
+* Mon Nov 11 2024 Evgeni Golov 202102220000-4
+- Don't fail if shim-ia32 is missing
+
 * Sat Mar 23 2024 Eric D. Helms <ericdhelms@gmail.com> 202102220000-3
 - Only requires shim-ia32 on RHEL 8 or less
 

packages/foreman/foreman-bootloaders-redhat/foreman-generate-bootloaders
@@ -55,7 +55,6 @@ else
   generate x86_64-efi grub2/grubx64.efi
 fi
 
-check_pkg shim-ia32
 check_pkg shim-x64
 cp -f /boot/efi/EFI/*/shim*.efi /var/lib/tftpboot/grub2
 chmod 644 /var/lib/tftpboot/grub2/*.{efi,0}