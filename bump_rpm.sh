theforeman/foreman-packaging

commit 89fb7bad4329f03f8ab2ed0312f9e919594ded94

Rewrite spec files with %scl_package
There are some spec files (most notably, hammer_plugin) that still use
SCL packaging macros. Those aren't needed anymore and break on modern
packaging tools, which you notice on Fedora and Packit.

bump_rpm.sh
@@ -88,11 +88,14 @@ if [[ $CURRENT_VERSION != "$NEW_VERSION" ]] ; then
 		bump_spec $SPEC_FILE "${NEW_VERSION}"
 
 		TEMPLATE="$(awk '/^# template: / { print $3 }' $SPEC_FILE)"
-		if [[ $TEMPLATE == 'scl' ]] || [[ $TEMPLATE == 'nonscl' ]] || [[ -z $TEMPLATE ]]; then
+		if [[ $TEMPLATE == 'scl' ]] || [[ $TEMPLATE == 'nonscl' ]] || [[ -z $TEMPLATE ]] || grep --no-messages %scl_package $SPEC_FILE; then
+			if [[ $TEMPLATE == 'scl' ]] || [[ $TEMPLATE == 'nonscl' ]] || [[ -z $TEMPLATE ]] ; then
+				TEMPLATE=default
+			fi
 			CHANGELOG=$(mktemp)
 			trap "rm -f $CHANGELOG" EXIT
 			sed -e '1,/%changelog/ d' $SPEC_FILE > "$CHANGELOG"
-			gem2rpm -t "$ROOT/gem2rpm/default.spec.erb" -o $SPEC_FILE ./*.gem
+			gem2rpm -t "$ROOT/gem2rpm/$TEMPLATE.spec.erb" -o $SPEC_FILE ./*.gem
 			cat "$CHANGELOG" >> $SPEC_FILE
 			git add $SPEC_FILE
 		elif [[ -n $TEMPLATE ]] ; then

commit a55f1c04b8e281c1e0d1df5bee4334bd71c28b11

always update foreman_min_version
Fixes: 89fb7bad4329f03f8ab2ed0312f9e919594ded94

bump_rpm.sh
@@ -97,22 +97,21 @@ if [[ $CURRENT_VERSION != "$NEW_VERSION" ]] ; then
 			sed -e '1,/%changelog/ d' $SPEC_FILE > "$CHANGELOG"
 			gem2rpm -t "$ROOT/gem2rpm/$TEMPLATE.spec.erb" -o $SPEC_FILE ./*.gem
 			cat "$CHANGELOG" >> $SPEC_FILE
-			git add $SPEC_FILE
 		elif [[ -n $TEMPLATE ]] ; then
 			echo "* Updating requirements"
 			gem2rpm -t "$ROOT/gem2rpm/$TEMPLATE.spec.erb" ./*.gem | "$SCRIPT_DIR"/update-requirements specfile - $SPEC_FILE
-			if [[ $TEMPLATE == foreman_plugin ]]; then
-				UNPACKED_GEM_DIR=$(mktemp -d)
-				gem unpack --target "$UNPACKED_GEM_DIR" ./*.gem
-				PLUGIN_LIB="${UNPACKED_GEM_DIR}/${GEM_NAME}-${NEW_VERSION}/lib"
-				REQUIRES_FOREMAN=$(grep --extended-regexp --recursive --no-filename 'requires_foreman\s' "$PLUGIN_LIB" | sed -E 's/[^0-9.]//g; q')
-				if [[ -n $REQUIRES_FOREMAN ]]; then
-					sed -i "/%global foreman_min_version/ s/foreman_min_version.*/foreman_min_version $REQUIRES_FOREMAN/" $SPEC_FILE
-				fi
-				rm -rf "$UNPACKED_GEM_DIR"
-			fi
-			git add $SPEC_FILE
 		fi
+		if [[ $TEMPLATE == foreman_plugin ]]; then
+			UNPACKED_GEM_DIR=$(mktemp -d)
+			gem unpack --target "$UNPACKED_GEM_DIR" ./*.gem
+			PLUGIN_LIB="${UNPACKED_GEM_DIR}/${GEM_NAME}-${NEW_VERSION}/lib"
+			REQUIRES_FOREMAN=$(grep --extended-regexp --recursive --no-filename 'requires_foreman\s' "$PLUGIN_LIB" | sed -E 's/[^0-9.]//g; q')
+			if [[ -n $REQUIRES_FOREMAN ]]; then
+				sed -i "/%global foreman_min_version/ s/foreman_min_version.*/foreman_min_version $REQUIRES_FOREMAN/" $SPEC_FILE
+			fi
+			rm -rf "$UNPACKED_GEM_DIR"
+		fi
+		git add $SPEC_FILE
 
 		if grep -q "# start package.json" $SPEC_FILE ; then
 			UNPACKED_GEM_DIR=$(mktemp -d)