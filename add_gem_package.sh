theforeman/foreman-packaging

commit 5b16b1eb23e3ecfd4c55771d902f29b9a0baab9b

Resolve many shellcheck warnings in add_gem_package.sh

add_gem_package.sh
@@ -10,21 +10,21 @@ usage() {
 generate_gem_package() {
 	if [[ $UPDATE == true ]] ; then
 		CHANGELOG=$(mktemp)
-		sed -n '/%changelog/,$p' $PACKAGE_DIR/$SPEC_FILE > $CHANGELOG
-		git rm -r $PACKAGE_DIR
+		sed -n '/%changelog/,$p' "$PACKAGE_DIR/$SPEC_FILE" > "$CHANGELOG"
+		git rm -r "$PACKAGE_DIR"
 	fi
-	mkdir $PACKAGE_DIR
-	pushd $PACKAGE_DIR
-	GEM_FILE_NAME=( $( gem fetch $GEM_NAME -q ${GEM_VERSION:+-v $GEM_VERSION} --platform=ruby ) ) # the output of gem fetch is "Downloaded foo-1.2.3" we need to extract only the second word
-	gem2rpm -o $SPEC_FILE "${GEM_FILE_NAME[1]}.gem" -t $TEMPLATE
-	sed -i 's/\s\+$//' $SPEC_FILE
-	git annex add *.gem
+	mkdir "$PACKAGE_DIR"
+	pushd "$PACKAGE_DIR"
+	GEM_FILE_NAME=( $( gem fetch "$GEM_NAME" -q ${GEM_VERSION:+-v $GEM_VERSION} --platform=ruby ) ) # the output of gem fetch is "Downloaded foo-1.2.3" we need to extract only the second word
+	gem2rpm -o "$SPEC_FILE" "${GEM_FILE_NAME[1]}.gem" -t "$TEMPLATE"
+	sed -i 's/\s\+$//' "$SPEC_FILE"
+	git annex add -- *.gem
 
-	VERSION=$(rpmspec --srpm -q --queryformat="%{version}-%{release}" --undefine=dist $SPEC_FILE)
+	VERSION=$(rpmspec --srpm -q --queryformat="%{version}-%{release}" --undefine=dist "$SPEC_FILE")
 
 	if [[ $TEMPLATE_NAME == foreman_plugin ]]; then
 		UNPACKED_GEM_DIR=$(mktemp -d)
-		gem unpack --target "$UNPACKED_GEM_DIR" *.gem
+		gem unpack --target "$UNPACKED_GEM_DIR" -- *.gem
 		PLUGIN_LIB="${UNPACKED_GEM_DIR}"/${GEM_NAME}-*/lib
 		REQUIRES_FOREMAN=$(grep --extended-regexp --recursive --no-filename 'requires_foreman\s' $PLUGIN_LIB | sed -E 's/[^0-9.]//g')
 		if [[ -n $REQUIRES_FOREMAN ]]; then
@@ -34,29 +34,29 @@ generate_gem_package() {
 	fi
 
 	if [[ $UPDATE == true ]]; then
-		cat $CHANGELOG >> $SPEC_FILE
-		sed -i '/^%changelog/,/^%changelog/{0,//!d}' $SPEC_FILE
-		rm $CHANGELOG
+		cat "$CHANGELOG" >> "$SPEC_FILE"
+		sed -i '/^%changelog/,/^%changelog/{0,//!d}' "$SPEC_FILE"
+		rm "$CHANGELOG"
 		CHANGELOG="- Update to $VERSION"
 	else
 		CHANGELOG="- Add $PACKAGE_NAME generated by gem2rpm using the $TEMPLATE_NAME template"
 	fi
-	echo "$CHANGELOG" | $ROOT/add_changelog.sh $SPEC_FILE
+	echo "$CHANGELOG" | "$ROOT"/add_changelog.sh "$SPEC_FILE"
 
-	if grep -q "# start package.json" $SPEC_FILE ; then
-		GEM_VERSION=$(rpmspec --srpm -q --queryformat="%{version}" --undefine=dist $SPEC_FILE)
+	if grep -q "# start package.json" "$SPEC_FILE" ; then
+		GEM_VERSION=$(rpmspec --srpm -q --queryformat="%{version}" --undefine=dist "$SPEC_FILE")
 		UNPACKED_GEM_DIR=$(mktemp -d)
-		gem unpack --target "$UNPACKED_GEM_DIR" *.gem
+		gem unpack --target "$UNPACKED_GEM_DIR" -- *.gem
 		PACKAGE_JSON="${UNPACKED_GEM_DIR}/${GEM_NAME}-${GEM_VERSION}/package.json"
 		if [[ -f $PACKAGE_JSON ]] ; then
-			$ROOT/update-requirements npm $PACKAGE_JSON $SPEC_FILE
+			"$ROOT"/update-requirements npm "$PACKAGE_JSON" "$SPEC_FILE"
 		else
 			echo "Unable to find package.json in gem: $PACKAGE_JSON"
 		fi
 		rm -rf "$UNPACKED_GEM_DIR"
 	fi
 
-	git add $SPEC_FILE
+	git add "$SPEC_FILE"
 	popd
 }
 
@@ -176,15 +176,14 @@ TEMPLATE=$(pwd)/gem2rpm/$TEMPLATE_NAME.spec.erb
 if [[ ! -e $TEMPLATE ]] ; then
 	echo "Template $TEMPLATE does not exist."
 	usage
-	exit 1
 fi
 
 if [[ $UPDATE == true ]] ; then
-	EXISTING_VERSION=$(rpmspec --srpm -q --queryformat="%{version}" $PACKAGE_DIR/$SPEC_FILE)
-	NEW_VERSION=$(curl -s https://rubygems.org/api/v1/gems/${GEM_NAME}.json | jq -r .version)
-	if [[ $REWRITE_ON_SAME_VERSION == true ]] || [[ $NEW_VERSION != $EXISTING_VERSION ]]; then
+	EXISTING_VERSION=$(rpmspec --srpm -q --queryformat="%{version}" "$PACKAGE_DIR/$SPEC_FILE")
+	NEW_VERSION=$(curl -s "https://rubygems.org/api/v1/gems/${GEM_NAME}.json" | jq -r .version)
+	if [[ $REWRITE_ON_SAME_VERSION == true ]] || [[ $NEW_VERSION != "$EXISTING_VERSION" ]]; then
 		generate_gem_package
-		VERSION=$(rpmspec --srpm -q --queryformat="%{version}-%{release}" --undefine=dist $PACKAGE_DIR/$SPEC_FILE)
+		VERSION=$(rpmspec --srpm -q --queryformat="%{version}-%{release}" --undefine=dist "$PACKAGE_DIR/$SPEC_FILE")
 		git commit -m "Bump $PACKAGE_NAME to $VERSION"
 	else
 		echo "$PACKAGE_NAME is already at version $VERSION"

commit 002cba56fbeab3309d366461a9272d2ba960714c

Fix broken "gem unpack" calls in add_gem_package.sh
This fixes a regression introduced in commit 5b16b1eb23e3ecfd4c55771d902f29b9a0baab9b.

add_gem_package.sh
@@ -23,7 +23,7 @@ generate_gem_package() {
 
 	if [[ $TEMPLATE_NAME == foreman_plugin ]]; then
 		UNPACKED_GEM_DIR=$(mktemp -d)
-		gem unpack --target "$UNPACKED_GEM_DIR" -- *.gem
+		gem unpack --target "$UNPACKED_GEM_DIR" *.gem
 		PLUGIN_LIB="${UNPACKED_GEM_DIR}"/${GEM_NAME}-*/lib
 		REQUIRES_FOREMAN=$(grep --extended-regexp --recursive --no-filename 'requires_foreman\s' $PLUGIN_LIB | sed -E 's/[^0-9.]//g')
 		if [[ -n $REQUIRES_FOREMAN ]]; then
@@ -45,7 +45,7 @@ generate_gem_package() {
 	if grep -q "# start package.json" "$SPEC_FILE" ; then
 		GEM_VERSION=$(rpmspec --srpm -q --queryformat="%{version}" --undefine=dist "$SPEC_FILE")
 		UNPACKED_GEM_DIR=$(mktemp -d)
-		gem unpack --target "$UNPACKED_GEM_DIR" -- *.gem
+		gem unpack --target "$UNPACKED_GEM_DIR" *.gem
 		PACKAGE_JSON="${UNPACKED_GEM_DIR}/${GEM_NAME}-${GEM_VERSION}/package.json"
 		if [[ -f $PACKAGE_JSON ]] ; then
 			"$SCRIPT_ROOT"/update-requirements npm "$PACKAGE_JSON" "$SPEC_FILE"