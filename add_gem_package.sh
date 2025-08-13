8c74443e7ee0f494ce4b1fdd118a25d36aa86053
Frank Wall <fw@moov.de>
2025-08-13 10:43:00 +0200

    Apply suggestion from code review
    Co-authored-by: Evgeni Golov <evgeni@golov.de>

--- add_gem_package.sh
+++ add_gem_package.sh
@@ -23,7 +23,7 @@ generate_gem_package() {
 
 	if [[ $TEMPLATE_NAME == foreman_plugin ]]; then
 		UNPACKED_GEM_DIR=$(mktemp -d)
-		gem unpack --target "$UNPACKED_GEM_DIR" *.gem
+		gem unpack --target "$UNPACKED_GEM_DIR" ./*.gem
 		PLUGIN_LIB="${UNPACKED_GEM_DIR}"/${GEM_NAME}-*/lib
 		REQUIRES_FOREMAN=$(grep --extended-regexp --recursive --no-filename 'requires_foreman\s' $PLUGIN_LIB | sed -E 's/[^0-9.]//g')
 		if [[ -n $REQUIRES_FOREMAN ]]; then
