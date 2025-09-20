theforeman/foreman-packaging

commit 5c254bf99b4308ecd9c5bb3e6b790571626df68e

Build foreman-proxy for SCL

packages/foreman/rubygem-ffi/rubygem-ffi.spec
@@ -1,97 +1,104 @@
-# This package contains macros that provide functionality relating to
-# Software Collections. These macros are not used in default
-# Fedora builds, and should not be blindly copied or enabled.
-# Specifically, the "scl" macro must not be defined in official Fedora
-# builds. For more information, see:
-# http://docs.fedoraproject.org/en-US/Fedora_Contributor_Documentation
-# /1/html/Software_Collections_Guide/index.html
-
+# template: scl
 %{?scl:%scl_package rubygem-%{gem_name}}
 %{!?scl:%global pkg_name %{name}}
 
 %global gem_name ffi
-%{!?enable_test: %global enable_test 0}
 
-Name:           %{?scl_prefix}rubygem-%{gem_name}
-Version:        1.4.0
-Release:        8%{?dist}
-Summary:        FFI Extensions for Ruby
-Group:          Development/Languages
+Name: %{?scl_prefix}rubygem-%{gem_name}
+Version: 1.11.3
+Release: 1%{?dist}
+Summary: Ruby FFI
+Group: Development/Languages
+License: BSD-3-Clause
+URL: https://github.com/ffi/ffi/wiki
+Source0: https://rubygems.org/gems/%{gem_name}-%{version}.gem
 
-License:        LGPLv3
-URL:            http://wiki.github.com/ffi/ffi
-Source0:        https://rubygems.org/gems/%{gem_name}-%{version}.gem
-
-BuildRequires:  %{?scl_prefix_ruby}ruby-devel
-BuildRequires:  %{?scl_prefix_ruby}rubygems-devel
-BuildRequires:	libffi-devel
-%if 0%{enable_test} > 0
-BuildRequires:	%{?scl_prefix_ror}rubygem(rspec)
-%endif
-
-Requires: %{?scl_prefix_ruby}rubygems
+# start specfile generated dependencies
 Requires: %{?scl_prefix_ruby}ruby(release)
-
-Provides:       %{?scl_prefix}rubygem(%{gem_name}) = %{version}
-%{?scl:Obsoletes: ruby193-rubygem-%{gem_name}}
+Requires: %{?scl_prefix_ruby}ruby >= 2.0
+Requires: %{?scl_prefix_ruby}ruby(rubygems)
+BuildRequires: %{?scl_prefix_ruby}ruby(release)
+BuildRequires: %{?scl_prefix_ruby}ruby-devel >= 2.0
+BuildRequires: %{?scl_prefix_ruby}rubygems-devel
+Provides: %{?scl_prefix}rubygem(%{gem_name}) = %{version}
+# end specfile generated dependencies
 
 %description
-Ruby-FFI is a ruby extension for programmatically loading dynamic
-libraries, binding functions within them, and calling those functions
-from Ruby code. Moreover, a Ruby-FFI extension works without changes
-on Ruby and JRuby. Discover why should you write your next extension
-using Ruby-FFI here[http://wiki.github.com/ffi/ffi/why-use-ffi].
+Ruby FFI library.
+
+
+%package doc
+Summary: Documentation for %{pkg_name}
+Group: Documentation
+Requires: %{?scl_prefix}%{pkg_name} = %{version}-%{release}
+BuildArch: noarch
+
+%description doc
+Documentation for %{pkg_name}.
 
 %prep
-%setup -n %{pkg_name}-%{version} -q -c -T
-%{?scl:scl enable %{scl} - <<EOF}
-%gem_install -n %{SOURCE0}
+%{?scl:scl enable %{scl} - << \EOF}
+gem unpack %{SOURCE0}
+%{?scl:EOF}
+
+%setup -q -D -T -n  %{gem_name}-%{version}
+
+%{?scl:scl enable %{scl} - << \EOF}
+gem spec %{SOURCE0} -l --ruby > %{gem_name}.gemspec
 %{?scl:EOF}
 
 %build
+# Create the gem as gem install only works on a gem file
+%{?scl:scl enable %{scl} - << \EOF}
+gem build %{gem_name}.gemspec
+%{?scl:EOF}
+
+# %%gem_install compiles any C extensions and installs the gem into ./%%gem_dir
+# by default, so that we can move it into the buildroot in %%install
+%{?scl:scl enable %{scl} - << \EOF}
+%gem_install
+%{?scl:EOF}
 
 %install
 mkdir -p %{buildroot}%{gem_dir}
-cp -pa .%{gem_dir}/* \
+cp -a .%{gem_dir}/* \
         %{buildroot}%{gem_dir}/
 
 mkdir -p %{buildroot}%{gem_extdir_mri}
-cp -a ./%{gem_extdir_mri}/* %{buildroot}%{gem_extdir_mri}/
+cp -a .%{gem_extdir_mri}/{gem.build_complete,*.so} %{buildroot}%{gem_extdir_mri}/
 
-pushd %{buildroot}
-rm -f .%{gem_extdir_mri}/{gem_make.out,mkmf.log}
-popd
-
-%if 0%{enable_test} > 0
-%check
-pushd .%{gem_instdir}
-make -f libtest/GNUmakefile
-rspec spec
-popd
-%endif
+# Prevent dangling symlink in -debuginfo (rhbz#878863).
+rm -rf %{buildroot}%{gem_instdir}/ext/
 
 %files
-%doc %{gem_instdir}/COPYING
-%doc %{gem_instdir}/COPYING.LESSER
-%doc %{gem_instdir}/README.md
-%doc %{gem_instdir}/History.txt
-%doc %{gem_instdir}/LICENSE
-%doc %{gem_docdir}
-
 %dir %{gem_instdir}
-%{gem_instdir}/Rakefile
-%{gem_instdir}/gen
 %exclude %{gem_instdir}/ext
-%exclude %{gem_instdir}/libtest
-%{gem_instdir}/ffi.gemspec
+%{gem_extdir_mri}
+%exclude %{gem_instdir}/.appveyor.yml
+%exclude %{gem_instdir}/.gitignore
+%exclude %{gem_instdir}/.gitmodules
+%exclude %{gem_instdir}/.travis.yml
+%exclude %{gem_instdir}/.yardopts
+%license %{gem_instdir}/COPYING
+%license %{gem_instdir}/LICENSE
+%license %{gem_instdir}/LICENSE.SPECS
 %{gem_libdir}
-%{gem_instdir}/spec
-%{gem_extdir_mri}/
+%exclude %{gem_instdir}/samples
 %exclude %{gem_cache}
 %{gem_spec}
 
+%files doc
+%doc %{gem_docdir}
+%doc %{gem_instdir}/CHANGELOG.md
+%{gem_instdir}/Gemfile
+%doc %{gem_instdir}/README.md
+%{gem_instdir}/Rakefile
+%{gem_instdir}/ffi.gemspec
 
 %changelog
+* Tue Oct 01 2019 Eric D. Helms <ericdhelms@gmail.com> - 1.11.3-1
+- Update and rebuild into SCL
+
 * Fri Sep 07 2018 Eric D. Helms <ericdhelms@gmail.com> - 1.4.0-8
 - Rebuild for Rails 5.2 and Ruby 2.5
 

comps/comps-foreman-plugins-rhel7.xml
@@ -120,7 +120,6 @@
       <packagereq type="default">tfm-rubygem-foreman_xen</packagereq>
       <packagereq type="default">tfm-rubygem-git</packagereq>
       <packagereq type="default">tfm-rubygem-graphite-api</packagereq>
-      <packagereq type="default">tfm-rubygem-gssapi</packagereq>
       <packagereq type="default">tfm-rubygem-hammer_cli_foreman_admin</packagereq>
       <packagereq type="default">tfm-rubygem-hammer_cli_foreman_ansible</packagereq>
       <packagereq type="default">tfm-rubygem-hammer_cli_foreman_azure_rm</packagereq>
@@ -274,7 +273,6 @@
       <packagereq type="default">tfm-rubygem-foreman_wreckingball-doc</packagereq>
       <packagereq type="default">tfm-rubygem-foreman_xen-doc</packagereq>
       <packagereq type="default">tfm-rubygem-graphite-api-doc</packagereq>
-      <packagereq type="default">tfm-rubygem-gssapi-doc</packagereq>
       <packagereq type="default">tfm-rubygem-hammer_cli_foreman_admin-doc</packagereq>
       <packagereq type="default">tfm-rubygem-hammer_cli_foreman_ansible-doc</packagereq>
       <packagereq type="default">tfm-rubygem-hammer_cli_foreman_azure_rm-doc</packagereq>

comps/comps-foreman-rhel7.xml
@@ -154,6 +154,7 @@
       <packagereq type="default">tfm-rubygem-bootstrap-sass</packagereq>
       <packagereq type="default">tfm-rubygem-bundler_ext</packagereq>
       <packagereq type="default">tfm-rubygem-clamp</packagereq>
+      <packagereq type="default">tfm-rubygem-concurrent-ruby</packagereq>
       <packagereq type="default">tfm-rubygem-concurrent-ruby-edge</packagereq>
       <packagereq type="default">tfm-rubygem-connection_pool</packagereq>
       <packagereq type="default">tfm-rubygem-css_parser</packagereq>
@@ -169,6 +170,7 @@
       <packagereq type="default">tfm-rubygem-facter</packagereq>
       <packagereq type="default">tfm-rubygem-faraday</packagereq>
       <packagereq type="default">tfm-rubygem-fast_gettext</packagereq>
+      <packagereq type="default">tfm-rubygem-ffi</packagereq>
       <packagereq type="default">tfm-rubygem-fog-aws</packagereq>
       <packagereq type="default">tfm-rubygem-fog-core</packagereq>
       <packagereq type="default">tfm-rubygem-fog-digitalocean</packagereq>
@@ -194,6 +196,7 @@
       <packagereq type="default">tfm-rubygem-graphql</packagereq>
       <packagereq type="default">tfm-rubygem-graphql-batch</packagereq>
       <packagereq type="default">tfm-rubygem-gridster-rails</packagereq>
+      <packagereq type="default">tfm-rubygem-gssapi</packagereq>
       <packagereq type="default">tfm-rubygem-hammer_cli</packagereq>
       <packagereq type="default">tfm-rubygem-hammer_cli_foreman</packagereq>
       <packagereq type="default">tfm-rubygem-hashie</packagereq>
@@ -245,6 +248,7 @@
       <packagereq type="default">tfm-rubygem-rack-jsonp</packagereq>
       <packagereq type="default">tfm-rubygem-rails-i18n</packagereq>
       <packagereq type="default">tfm-rubygem-rake-compiler</packagereq>
+      <packagereq type="default">tfm-rubygem-rb-inotify</packagereq>
       <packagereq type="default">tfm-rubygem-rbovirt</packagereq>
       <packagereq type="default">tfm-rubygem-rbvmomi</packagereq>
       <packagereq type="default">tfm-rubygem-record_tag_helper</packagereq>
@@ -256,10 +260,12 @@
       <packagereq type="default">tfm-rubygem-rkerberos</packagereq>
       <packagereq type="default">tfm-rubygem-roadie</packagereq>
       <packagereq type="default">tfm-rubygem-roadie-rails</packagereq>
+      <packagereq type="default">tfm-rubygem-rsec</packagereq>
       <packagereq type="default">tfm-rubygem-ruby-libvirt</packagereq>
       <packagereq type="default">tfm-rubygem-ruby2ruby</packagereq>
       <packagereq type="default">tfm-rubygem-ruby_parser</packagereq>
       <packagereq type="default">tfm-rubygem-rubyforge</packagereq>
+      <packagereq type="default">tfm-rubygem-rubyipmi</packagereq>
       <packagereq type="default">tfm-rubygem-safemode</packagereq>
       <packagereq type="default">tfm-rubygem-scoped_search</packagereq>
       <packagereq type="default">tfm-rubygem-secure_headers</packagereq>
@@ -298,7 +304,7 @@
       <packagereq type="default">foreman-installer-katello</packagereq>
       <packagereq type="default">katello-certs-tools</packagereq>
       <packagereq type="default">katello-selinux</packagereq>
-      <packagereq type="default">keycloak-httpd-client-install</packagereq>	      
+      <packagereq type="default">keycloak-httpd-client-install</packagereq>
       <packagereq type="default">mod_passenger</packagereq>
       <packagereq type="default">pcp-mmvstatsd</packagereq>
       <packagereq type="default">puppet-agent-oauth</packagereq>
@@ -308,7 +314,6 @@
       <packagereq type="default">python2-keycloak-httpd-client-install</packagereq>
       <packagereq type="default">rubygem-bundler_ext</packagereq>
       <packagereq type="default">rubygem-clamp</packagereq>
-      <packagereq type="default">rubygem-concurrent-ruby</packagereq>
       <packagereq type="default">rubygem-foreman_maintain</packagereq>
       <packagereq type="default">rubygem-hashie</packagereq>
       <packagereq type="default">rubygem-highline</packagereq>
@@ -326,9 +331,6 @@
       <packagereq type="default">rubygem-passenger-native</packagereq>
       <packagereq type="default">rubygem-passenger-native-libs</packagereq>
       <packagereq type="default">rubygem-powerbar</packagereq>
-      <packagereq type="default">rubygem-rb-inotify</packagereq>
-      <packagereq type="default">rubygem-rsec</packagereq>
-      <packagereq type="default">rubygem-rubyipmi</packagereq>
       <!--
         Do not edit this section manually and use ./comps_doc.sh script
       -->
@@ -351,9 +353,6 @@
       <packagereq type="default">rubygem-multi_json-doc</packagereq>
       <packagereq type="default">rubygem-passenger-doc</packagereq>
       <packagereq type="default">rubygem-powerbar-doc</packagereq>
-      <packagereq type="default">rubygem-rb-inotify-doc</packagereq>
-      <packagereq type="default">rubygem-rsec-doc</packagereq>
-      <packagereq type="default">rubygem-rubyipmi-doc</packagereq>
       <packagereq type="default">tfm-rubygem-activerecord-session_store-doc</packagereq>
       <packagereq type="default">tfm-rubygem-addressable-doc</packagereq>
       <packagereq type="default">tfm-rubygem-algebrick-doc</packagereq>
@@ -368,6 +367,7 @@
       <packagereq type="default">tfm-rubygem-bootstrap-sass-doc</packagereq>
       <packagereq type="default">tfm-rubygem-bundler_ext-doc</packagereq>
       <packagereq type="default">tfm-rubygem-clamp-doc</packagereq>
+      <packagereq type="default">tfm-rubygem-concurrent-ruby-doc</packagereq>
       <packagereq type="default">tfm-rubygem-concurrent-ruby-edge-doc</packagereq>
       <packagereq type="default">tfm-rubygem-connection_pool-doc</packagereq>
       <packagereq type="default">tfm-rubygem-css_parser-doc</packagereq>
@@ -408,6 +408,7 @@
       <packagereq type="default">tfm-rubygem-graphql-batch-doc</packagereq>
       <packagereq type="default">tfm-rubygem-graphql-doc</packagereq>
       <packagereq type="default">tfm-rubygem-gridster-rails-doc</packagereq>
+      <packagereq type="default">tfm-rubygem-gssapi-doc</packagereq>
       <packagereq type="default">tfm-rubygem-hammer_cli-doc</packagereq>
       <packagereq type="default">tfm-rubygem-hammer_cli_foreman-doc</packagereq>
       <packagereq type="default">tfm-rubygem-hashie-doc</packagereq>
@@ -457,6 +458,7 @@
       <packagereq type="default">tfm-rubygem-rack-jsonp-doc</packagereq>
       <packagereq type="default">tfm-rubygem-rails-i18n-doc</packagereq>
       <packagereq type="default">tfm-rubygem-rake-compiler-doc</packagereq>
+      <packagereq type="default">tfm-rubygem-rb-inotify-doc</packagereq>
       <packagereq type="default">tfm-rubygem-rbovirt-doc</packagereq>
       <packagereq type="default">tfm-rubygem-rbvmomi-doc</packagereq>
       <packagereq type="default">tfm-rubygem-record_tag_helper-doc</packagereq>
@@ -468,9 +470,11 @@
       <packagereq type="default">tfm-rubygem-rkerberos-doc</packagereq>
       <packagereq type="default">tfm-rubygem-roadie-doc</packagereq>
       <packagereq type="default">tfm-rubygem-roadie-rails-doc</packagereq>
+      <packagereq type="default">tfm-rubygem-rsec-doc</packagereq>
       <packagereq type="default">tfm-rubygem-ruby2ruby-doc</packagereq>
       <packagereq type="default">tfm-rubygem-ruby-libvirt-doc</packagereq>
       <packagereq type="default">tfm-rubygem-ruby_parser-doc</packagereq>
+      <packagereq type="default">tfm-rubygem-rubyipmi-doc</packagereq>
       <packagereq type="default">tfm-rubygem-safemode-doc</packagereq>
       <packagereq type="default">tfm-rubygem-scoped_search-doc</packagereq>
       <packagereq type="default">tfm-rubygem-secure_headers-doc</packagereq>

package_manifest.yaml
@@ -440,14 +440,6 @@ foreman_nonscl_packages:
         - "jenkins_job=foreman-installer-develop-source-release"
         - "jenkins_url=https://ci.theforeman.org"
       build_package_tito_builder: "tito.builder.FetchBuilder"
-    foreman-proxy:
-      releasers:
-        - "{{ nightly_releaser }}"
-      build_package_tito_releaser_args: "{{ nightly_package_tito_releaser_args }}"
-      nightly_package_tito_releaser_args:
-        - "jenkins_job=smart-proxy-develop-source-release"
-        - "jenkins_url=https://ci.theforeman.org"
-      build_package_tito_builder: "tito.builder.FetchBuilder"
     foreman-release:
       diff_package_tags:
         - foreman-nightly-nonscl-rhel7
@@ -487,19 +479,20 @@ foreman_proxy_packages:
     package_base_dir: 'packages/foreman/'
     diff_package_skip: false
     diff_package_tags:
-      - foreman-nightly-nonscl-rhel7
+      - foreman-nightly-rhel7
     releasers:
       - koji-foreman
     nightly_releaser: koji-foreman-jenkins
     repoclosure_config: repoclosure/yum_el7.conf
     repoclosure_lookaside_repos:
       rhel7:
-        - el7-foreman-nightly
         - el7-base
         - el7-updates
         - el7-epel
         - el7-extras
-        - el7-puppet-6
+        - el7-scl
+        - el7-foreman-nightly
+        - el7-foreman-rails-nightly
   hosts:
     foreman-proxy:
       releasers:

packages/foreman/foreman-proxy/foreman-proxy.spec
@@ -6,7 +6,7 @@
 %global scl_ruby_bin /usr/bin/%{?scl:%{scl_prefix}}ruby
 %global scl_rake /usr/bin/%{?scl:%{scl_prefix}}rake
 
-%global release 1
+%global release 2
 %global prereleasesource develop
 %global prerelease %{?prereleasesource}
 
@@ -43,8 +43,8 @@ Requires:       %{?scl_prefix}rubygem(bundler_ext)
 Requires:       %{?scl_prefix}rubygem(rb-inotify)
 Requires:       %{?scl_prefix}rubygem(rsec)
 Requires:       %{?scl_prefix}rubygem(jwt)
-Requires:       %{?scl_prefix}rubygem(concurrent-ruby) >= 1.0
-Requires:       %{?scl_prefix}rubygem(concurrent-ruby) < 2.0
+Requires:       %{?scl_prefix_ror}rubygem(concurrent-ruby) >= 1.0
+Requires:       %{?scl_prefix_ror}rubygem(concurrent-ruby) < 2.0
 Requires:       %{?scl_prefix}rubygem(logging) >= 1.8.0
 Requires:       %{?scl_prefix}rubygem(logging) < 3.0.0
 Requires:       sudo
@@ -216,6 +216,9 @@ fi
 
 
 %changelog
+* Wed Jan 08 2020 Eric D. Helms <ericdhelms@gmail.com> - 2.0.0-0.2.develop
+- Update and rebuild into SCL
+
 * Mon Jan 06 2020 Tomer Brisker <tbrisker@gmail.com> - 2.0.0-0.1.develop
 - Bump version to 2.0-develop
 

packages/foreman/rubygem-ffi/ffi-1.11.3.gem
@@ -1,0 +1,1 @@
+../../../.git/annex/objects/8g/f1/SHA256E-s889856--6da2eb3c4867e64df28d3e0b1008422dfacda7c046f9a8f3c56c52505b195e81.3.gem/SHA256E-s889856--6da2eb3c4867e64df28d3e0b1008422dfacda7c046f9a8f3c56c52505b195e81.3.gem

packages/foreman/rubygem-gssapi/rubygem-gssapi.spec
@@ -5,7 +5,7 @@
 
 Name: %{?scl_prefix}rubygem-%{gem_name}
 Version: 1.2.0
-Release: 5%{?dist}
+Release: 6%{?dist}
 Summary: A FFI wrapper around the system GSSAPI library
 Group: Development/Languages
 License: MIT
@@ -86,6 +86,9 @@ rm -f %{buildroot}%{gem_instdir}/%{gem_name}.gemspec
 %doc %{gem_instdir}/preamble
 
 %changelog
+* Tue Oct 01 2019 Eric D. Helms <ericdhelms@gmail.com> - 1.2.0-6
+- Update and rebuild into SCL
+
 * Fri Sep 07 2018 Eric D. Helms <ericdhelms@gmail.com> - 1.2.0-5
 - Rebuild for Rails 5.2 and Ruby 2.5
 

commit 1180f7707f35dfa8f4c613ca0f30ee9052410fd2

Use system libffi
Fixes: 5c254bf99b4308ecd9c5bb3e6b790571626df68e

packages/foreman/rubygem-ffi/rubygem-ffi.spec
@@ -4,7 +4,7 @@
 
 Name: rubygem-%{gem_name}
 Version: 1.16.3
-Release: 1%{?dist}
+Release: 2%{?dist}
 Summary: Ruby FFI
 License: BSD-3-Clause
 URL: https://github.com/ffi/ffi/wiki
@@ -18,6 +18,8 @@ BuildRequires: rubygems-devel
 # https://fedoraproject.org/wiki/Packaging:C_and_C++#BuildRequires_and_Requires
 BuildRequires: gcc
 # end specfile generated dependencies
+
+BuildRequires: libffi-devel
 
 %description
 Ruby FFI library.
@@ -85,6 +87,9 @@ rm -rf gem_ext_test
 %exclude %{gem_instdir}/ffi.gemspec
 
 %changelog
+* Sat Sep 20 2025 Evgeni Golov - 1.16.3-2
+- Use system libffi
+
 * Mon Nov 06 2023 Foreman Packaging Automation <packaging@theforeman.org> 1.16.3-1
 - Update to 1.16.3
 