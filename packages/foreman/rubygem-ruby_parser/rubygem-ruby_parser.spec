theforeman/foreman-packaging

commit ec2eb970dca9add615ced37935252abe727caa51

Update rubygem-ruby_parser to 3.21.0

packages/foreman/rubygem-ruby_parser/rubygem-ruby_parser.spec
@@ -2,7 +2,7 @@
 %global gem_name ruby_parser
 
 Name: rubygem-%{gem_name}
-Version: 3.20.3
+Version: 3.21.0
 Release: 1%{?dist}
 Summary: A ruby parser written in pure ruby
 License: MIT
@@ -17,6 +17,12 @@ BuildRequires: ruby < 4
 BuildRequires: rubygems-devel
 BuildArch: noarch
 # end specfile generated dependencies
+
+# Prefer to consume racc as a default gem
+Requires: ruby-default-gems < 3.3
+BuildRequires: ruby-default-gems < 3.3
+Requires: (bundled(rubygem-racc) >= 1.4 with bundled(rubygem-racc) < 2)
+BuildRequires: (bundled(rubygem-racc) >= 1.4 with bundled(rubygem-racc) < 2)
 
 %description
 ruby_parser (RP) is a ruby parser written in pure ruby (utilizing
@@ -35,6 +41,10 @@ Documentation for %{name}.
 
 %prep
 %setup -q -n  %{gem_name}-%{version}
+
+# rubygem-racc is bundled into ruby-libs package and
+# auto-generated dependencies will break dependency resolution
+%gemspec_remove_dep -g racc "~> 1.5"
 
 %build
 # Create the gem as gem install only works on a gem file
@@ -78,6 +88,9 @@ find %{buildroot}%{gem_instdir}/bin -type f | xargs chmod a+x
 %{gem_instdir}/test
 
 %changelog
+* Wed Jan 24 2024 Foreman Packaging Automation <packaging@theforeman.org> - 3.21.0-1
+- Update to 3.21.0
+
 * Wed Jul 19 2023 Foreman Packaging Automation <packaging@theforeman.org> 3.20.3-1
 - Update to 3.20.3
 

packages/foreman/rubygem-ruby_parser/ruby_parser-3.20.3.gem
@@ -1,1 +1,0 @@
-../../../.git/annex/objects/8M/qp/SHA256E-s854528--8d2289a695dc81ffddcdd5a56e80c9a109806bc0d0b1239a1c852b0c71251c49.3.gem/SHA256E-s854528--8d2289a695dc81ffddcdd5a56e80c9a109806bc0d0b1239a1c852b0c71251c49.3.gem

packages/foreman/rubygem-ruby_parser/ruby_parser-3.21.0.gem
@@ -1,0 +1,1 @@
+../../../.git/annex/objects/VK/5Z/SHA256E-s765952--3842893d2f4602dcd93c0a79d77f9ce8e1ce197d41ac533d8e25c684f8f1c56b.0.gem/SHA256E-s765952--3842893d2f4602dcd93c0a79d77f9ce8e1ce197d41ac533d8e25c684f8f1c56b.0.gem

commit 37f3dbcbcf1c5549b554baf4bbafb08820e7315a

Drop EL8 requirement workaround
On EL9 and EL10 bundled(rubygem-racc) is provided so the additional
explicit requirement on ruby-default-gems isn't needed. Removing it
allows using Ruby 3.3 or greater.

Fixes: ec2eb970dca9add615ced37935252abe727caa51 ("Update rubygem-ruby_parser to 3.21.0")

packages/foreman/rubygem-ruby_parser/rubygem-ruby_parser.spec
@@ -3,7 +3,7 @@
 
 Name: rubygem-%{gem_name}
 Version: 3.21.1
-Release: 1%{?dist}
+Release: 2%{?dist}
 Summary: A ruby parser written in pure ruby
 License: MIT
 URL: https://github.com/seattlerb/ruby_parser
@@ -19,8 +19,6 @@ BuildArch: noarch
 # end specfile generated dependencies
 
 # Prefer to consume racc as a default gem
-Requires: ruby-default-gems < 3.3
-BuildRequires: ruby-default-gems < 3.3
 Requires: (bundled(rubygem-racc) >= 1.4 with bundled(rubygem-racc) < 2)
 BuildRequires: (bundled(rubygem-racc) >= 1.4 with bundled(rubygem-racc) < 2)
 
@@ -88,6 +86,9 @@ find %{buildroot}%{gem_instdir}/bin -type f | xargs chmod a+x
 %{gem_instdir}/test
 
 %changelog
+* Fri Dec 19 2025 Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl> - 3.21.1-2
+- Drop EL8 requirement workaround
+
 * Sun Jul 14 2024 Foreman Packaging Automation <packaging@theforeman.org> - 3.21.1-1
 - Update to 3.21.1
 